6451afa8b9376340c1969f33a99ea7068917595b
Justin Kuczynski <justinak@gmail.com>
2009-09-24 18:50:20 +0000

    auto-create output directory, ignore invisible (.*) files, and by default (no -e flag), use the alpha diversity file with the smallest number of seqs/sample as the example file

--- qiime/collate_alpha.py
+++ qiime/collate_alpha.py
@@ -34,9 +34,26 @@ from qiime.format import format_otu_table, format_matrix
 from qiime.util import FunctionWithParams
 
 def main(input_dir, output_dir, example_filepath=None):
+    if not os.path.exists(options.output_path):
+        os.mkdir(options.output_path)
     file_names = os.listdir(input_dir)
-    if example_filepath is None:
-        example_filepath = os.path.join(input_dir,file_names[0])
+    
+    # remove hidden
+    hidden_fnames = []
+    for fname in file_names:
+        if fname.startswith('.'):
+            hidden_fnames.append(fname)
+    for hidden in hidden_fnames:
+        file_names.remove(hidden)
+     
+    if example_filepath is None:    
+        # table row is base_name, seqs_per_sam, iters, ext
+        file_name_table = map(parse_rarefaction_fname, file_names)
+        # sort on seqs/sam
+        sorted_fname_table = sorted(file_name_table, key=operator.itemgetter(1))
+        # now map back to file name
+        example_fname = file_names[file_name_table.index(sorted_fname_table[0])]
+        example_filepath = os.path.join(input_dir,example_fname)
     f = open(example_filepath, 'r')
     all_metrics, all_samples, example_data = parse_matrix(f)
     num_cols = len(all_samples)
@@ -48,6 +65,8 @@ def main(input_dir, output_dir, example_filepath=None):
     for metric in all_metrics:
         metric_file_data = []
         for fname in file_names:
+            # f_ here refers to the input file currently being processed
+            # to distinguish from the output file we're building
             f = open(os.path.join(input_dir,fname), 'r')
             f_metrics, f_samples, f_data = \
                 parse_matrix(f)
@@ -65,7 +84,10 @@ def main(input_dir, output_dir, example_filepath=None):
                     output_row[all_samples.index(sample)+1] = \
                         str(f_data[f_row,f_col])
                 except IndexError:
-                    print "warning, didn't find sample in example file"
+                    print("warning, didn't find sample in example file."+\
+                     "exiting", sample, fname, metric)
+                    raise # re-raise error
+                    
 
             output_row.insert(1, seqs)
             output_row.insert(2, iter)
@@ -82,17 +104,26 @@ def main(input_dir, output_dir, example_filepath=None):
 
 def make_cmd_parser():
     """returns command-line options"""
-    usage = """python collate_alpha.py -i TEST/rare_chao1_PD -o TEST/rare_collated -e TEST/rare_chao1_PD/alpha_rarefaction_200_0.txt 
+    usage = """python %prog -i input_path -o output_path
+    
+example:
+python collate_alpha.py -i TEST/rare_chao1_PD -o TEST/rare_collated -e TEST/rare_chao1_PD/alpha_rarefaction_200_0.txt 
 this creates the files TEST/rare_collated/chao1.txt (and PD_whole_tree.txt)
 each is a matrix of rarefaction by sample.
 
+input directory should have only otu tables
+output dir should be empty or nonexistant
+example file is optional.  if you have a set of rarefied otu talbes,
+make sure the example file contains every sample present in the otu talbes.
+typically choose the file with the fewest sequences per sample, to avoid
+files with sparse samples omitted.  This is the default behavior.
 """
 
     parser = OptionParser(usage=usage)
     parser.add_option('-i', '--in_path', dest='input_path', default=None,
         help='input path (a directory)')
     parser.add_option('-o', '--out_path', dest='output_path', default=None,
-        help='output path (a directory)')
+        help='output path (a directory).  will be created if needed')
     parser.add_option('-e', '--example_path', dest='example_path', 
         default=None,
         help='example alpha_diversity analysis file, containing all samples'+\
