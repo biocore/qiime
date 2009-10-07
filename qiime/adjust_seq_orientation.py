6029089a0b7dc37ee3fa861c765eca682fa33284
Greg Caporaso <gregcaporaso@gmail.com>
2009-10-07 23:31:33 +0000

    Added script to take a set of fasta sequences and write a new fasta file containing the reverse complement of each sequence. Eventually we will expand this script to detect sequences which need to be reverse complemented (e.g., by blasting them against a template database to find if the forward or reverse complement aligns) and adjust accordingly.

---
+++ qiime/adjust_seq_orientation.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# File created on 07 Oct 2009.
+from __future__ import division
+
+__author__ = "Greg Caporaso"
+__copyright__ = "Copyright 2009, Qiime"
+__credits__ = ["Greg Caporaso"]
+__license__ = "GPL"
+__version__ = "0.1"
+__maintainer__ = "Greg Caporaso"
+__email__ = "gregcaporaso@gmail.com"
+__status__ = "Prototype"
+
+
+from optparse import OptionParser
+from os.path import split, splitext
+from cogent.parse.fasta import MinimalFastaParser
+from cogent.util.misc import revComp
+
+usage_str = """usage: %prog [options] {-i INPUT_FASTA_FP}
+
+[] indicates optional input (order unimportant)
+{} indicates required input (order unimportant)
+
+Example usage:
+ Write the reverse complement of all seqs in seqs.fasta (-i) to
+  seqs_rc.fasta (default, change output_fp with -o). Each sequence
+  description line will have ' RC' appended to the end of it (default,
+  leave sequence description lines untouched by passing -r):
+ 
+ python ~/repo/Qiime/qiime/adjust_seq_orientation.py -i seqs.fasta
+"""
+
+def parse_command_line_parameters():
+    """ Parses command line arguments """
+    usage = usage_str
+    version = 'Version: %prog ' + __version__
+    parser = OptionParser(usage=usage, version=version)
+
+    # A binary 'verbose' flag
+    parser.add_option('-v','--verbose',action='store_true',\
+        dest='verbose',help='Print information during execution -- '+\
+        'useful for debugging [default: %default]')
+
+    parser.add_option('-i','--input_fasta_fp',\
+        help='the input fasta file [REQUIRED]')
+
+    parser.add_option('-o','--output_fp',\
+        help='the input fasta file [default: generated from input_fasta_fp]')
+
+    parser.add_option('-r','--retain_seq_id',action='store_true',\
+        help='leave seq description lines untouched'+\
+        ' [default: append " RC" to seq description lines]')
+
+    # Set default values here if they should be other than None
+    parser.set_defaults(verbose=False,retain_seq_id=False)
+
+    required_options = ['input_fasta_fp']
+
+    opts,args = parser.parse_args()
+    
+    for option in required_options:
+        if eval('opts.%s' % option) == None:
+            parser.error('Required option --%s omitted.' % option) 
+
+    return opts,args
+
+def null_seq_desc_mapper(s):
+    return s
+
+def append_rc(s):
+    return s + ' RC'
+
+def rc_fasta_lines(fasta_lines,seq_desc_mapper=append_rc):
+    """ 
+    """
+    for seq_id, seq in MinimalFastaParser(fasta_lines):
+        seq_id = seq_desc_mapper(seq_id)
+        seq = revComp(seq)
+        yield seq_id, seq
+    return
+        
+def rc_fasta_file(fasta_fp,output_fp,seq_id_mapper=append_rc):
+    """
+    """
+    input_f = open(fasta_fp)
+    output_f = open(output_fp,'w')
+    
+    for s in rc_fasta_lines(input_f,seq_id_mapper):
+        output_f.write('>%s\n%s\n' % s)
+
+    input_f.close()
+    output_f.close()
+
+
+if __name__ == "__main__":
+    opts,args = parse_command_line_parameters()
+    verbose = opts.verbose
+    
+    input_fasta_fp = opts.input_fasta_fp
+    output_fp = opts.output_fp
+    retain_seq_id = opts.retain_seq_id
+    
+    if retain_seq_id:
+        seq_desc_mapper = null_seq_desc_mapper
+    else:
+        seq_desc_mapper = append_rc
+    
+    if not output_fp:
+        input_file_basename, input_file_ext = \
+         splitext(split(input_fasta_fp)[1])
+        output_fp = '%s_rc%s' % (input_file_basename,input_file_ext)
+        
+    rc_fasta_file(input_fasta_fp,output_fp,seq_desc_mapper)
---
+++ tests/test_adjust_seq_orientation.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# File created on 07 Oct 2009.
+from __future__ import division
+
+__author__ = "Greg Caporaso"
+__copyright__ = "Copyright 2009, Qiime"
+__credits__ = ["Greg Caporaso"]
+__license__ = "GPL"
+__version__ = "0.1"
+__maintainer__ = "Greg Caporaso"
+__email__ = "gregcaporaso@gmail.com"
+__status__ = "Prototype"
+
+
+from unittest import TestCase, main
+from cogent.parse.fasta import MinimalFastaParser
+from qiime.adjust_seq_orientation import rc_fasta_lines, null_seq_desc_mapper,\
+    append_rc
+    
+class AdjustSeqOrientationTests(TestCase):
+    """ """
+    
+    def setUp(self):
+        """ """
+        self.fasta_lines1 = fasta_lines1.split('\n')
+        self.fasta_lines1_exp = list(MinimalFastaParser(\
+            fasta_lines1_exp.split('\n')))
+        self.fasta_lines1_exp_null_desc_mapper = list(MinimalFastaParser(\
+            fasta_lines1_exp_null_desc_mapper.split('\n')))
+            
+    def test_rc_fasta_lines(self):
+        """rc_fasta_lines: functions as expected w/ seq_id mapping 
+        """
+        self.assertEqual(list(rc_fasta_lines(self.fasta_lines1,append_rc)),\
+                         self.fasta_lines1_exp)
+            
+    def test_rc_fasta_lines_leave_seq_desc(self):
+        """rc_fasta_lines: functions as expected w/o seq_id mapping 
+        """
+        self.assertEqual(list(rc_fasta_lines(self.fasta_lines1,null_seq_desc_mapper)),\
+                         self.fasta_lines1_exp_null_desc_mapper)
+
+
+fasta_lines1 = """>s1 some description
+AAATGGCGCGCG
+>s2
+TTATATCCGC
+"""
+
+fasta_lines1_exp = """>s1 some description RC
+CGCGCGCCATTT
+>s2 RC
+GCGGATATAA
+"""
+
+fasta_lines1_exp_null_desc_mapper = """>s1 some description
+CGCGCGCCATTT
+>s2
+GCGGATATAA
+"""
+
+
+
+if __name__ == "__main__":
+    main()
