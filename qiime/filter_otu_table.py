e3c73d14e6d590aad61a274f259f4fe95e0185bf
Jesse Stombaugh <jistombaugh@gmail.com>
2009-11-03 19:47:28 +0000

    Updated the filtering, so the taxonomy is not required when filtering.  Also wrote some unit-tests for the script.

--- qiime/filter_otu_table.py
+++ qiime/filter_otu_table.py
@@ -6,7 +6,7 @@ If OTU table has taxonomy assigned, can also use taxonomy to filter.
 """
 __author__ = "Rob Knight"
 __copyright__ = "Copyright 2009, the PyCogent Project" #consider project name
-__credits__ = ["Rob Knight"] #remember to add yourself
+__credits__ = ["Rob Knight and Jesse Stombaugh"] #remember to add yourself
 __license__ = "GPL"
 __version__ = "0.1"
 __maintainer__ = "Rob Knight"
@@ -14,27 +14,11 @@ __email__ = "rob@spot.colorado.edu"
 __status__ = "Prototype"
 
 from qiime.parse import parse_otus
-from sys import argv, stdout
+from sys import argv
 from string import strip
 from numpy import array
 from optparse import OptionParser
 
-def make_cmd_parser():
-    """Returns the command-line options."""
-    parser = OptionParser()
-    parser.add_option('-c', '--min_count', dest='min_count', default=5,
-        type=int,
-        help='minimum number of sequences to leave OTU in file.')
-    parser.add_option('-s', '--min_samples', dest='min_samples', default=2,
-        type=int,
-        help='minimum number of samples to leave OTU in file.')
-    parser.add_option('-i', '--include_taxonomy', dest='include_taxonomy',
-        default='', help='list of taxonomy terms to include')
-    parser.add_option('-e', '--exclude_taxonomy', dest='exclude_taxonomy',
-        default='', help='list of taxonomy terms to exclude')
-    parser.add_option('-o', '--otu_filename', dest='otu_fname',
-        help='otu file name')
-    return parser.parse_args()
 
 def strip_quotes(s):
     """splits leading/trailing quotes from string s"""
@@ -51,29 +35,50 @@ def split_tax(tax):
     if len(fields) == 1:
         fields = fields[0].split(',')
     return map(strip_quotes, fields)
-    
 
-if __name__ == '__main__':
-    from sys import argv, stdout
-    opts, args = make_cmd_parser()
-    min_otu_count = opts.min_count
-    min_otu_samples = opts.min_samples
+def process_options(opts):
+    
+    filepath=opts.otu_fname
+    filename=filepath.strip().split('/')[-1]
+    filename=filename.split('.')[0]
+    
+    params={}
+    params['otu_file'] = opts.otu_fname
+    params['min_otu_count'] = opts.min_count
+    params['min_otu_samples'] = opts.min_samples
+    
     if opts.include_taxonomy:
-        tax = opts.include_taxonomy.split(';')
-        if len(tax) == 1:
-            included_taxa = set(map(strip, split_tax(opts.include_taxonomy)))
+        included_taxa = set(map(strip, split_tax(opts.include_taxonomy)))
     else:
         included_taxa = set()
+        
     if opts.exclude_taxonomy:
         excluded_taxa = set(map(strip, split_tax(opts.exclude_taxonomy)))
     else:
         excluded_taxa=set()
+        
+    params['included_taxa']=included_taxa
+    params['excluded_taxa']=excluded_taxa
     
-    otu_file = open(opts.otu_fname, 'U')
+    filtered_otu_fp = '%s/%s_filtered.txt' % (opts.dir_path,filename)
+                                    
+    params['filtered_otu_file']=filtered_otu_fp
+                                    
+    return params
 
+def _filter_table(params):
+
+    filtered_otu_fp=params['filtered_otu_file']
+    otu_file=open(params['otu_file'], 'U')
+    min_otu_count=params['min_otu_count']
+    min_otu_samples=params['min_otu_samples']
+    included_taxa=params['included_taxa']
+    excluded_taxa=params['excluded_taxa']
+    
+    filtered_table_path=open(filtered_otu_fp,'w')
     for line in otu_file:
         if line.startswith('#'):
-            stdout.write(line)
+            filtered_table_path.write(line)
         else:
             fields = line.split('\t')
             try:
@@ -83,10 +88,40 @@ if __name__ == '__main__':
                 vals = array(map(int, fields[1:-1]), dtype=int)
                 taxa = set(map(strip, split_tax(fields[-1])))
             if vals.sum() >= min_otu_count and \
-                (vals > 0).sum() > min_otu_samples:
+                (vals > 0).sum() >= min_otu_samples:
                 if not taxa:
-                    stdout.write(line)
+                    filtered_table_path.write(line)
                 else:
                     if taxa.intersection(included_taxa) and not \
                         taxa.intersection(excluded_taxa):
-                        stdout.write(line)
+                        filtered_table_path.write(line)
+                    elif not included_taxa and not excluded_taxa:
+                        filtered_table_path.write(line)
+
+def make_cmd_parser():
+    """Returns the command-line options."""
+    parser = OptionParser()
+    parser.add_option('-i', '--otu_filename', dest='otu_fname',
+        help='otu file name [REQUIRED]')
+    parser.add_option('-c', '--min_count', dest='min_count', default=1,
+        type=int,
+        help='minimum number of sequences to leave OTU in file [default=%default]')
+    parser.add_option('-s', '--min_samples', dest='min_samples', default=2,
+        type=int,
+        help='minimum number of samples to leave OTU in file [default=%default]')
+    parser.add_option('-t', '--include_taxonomy', dest='include_taxonomy',
+        default='', help='list of taxonomy terms to include [default=%default]')
+    parser.add_option('-e', '--exclude_taxonomy', dest='exclude_taxonomy',
+        default='', help='list of taxonomy terms to exclude [default=%default]')
+    parser.add_option('-o', '--dir_path',\
+        help='directory prefix for all analyses [default=%default]',default='./')
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    from sys import argv, stdout
+    opts, args = make_cmd_parser()
+
+    params=process_options(opts)
+    
+    _filter_table(params)
+    
