29c64250943bf5ecdc34545e3261a4fc65f961bc
Cathy Lozupone <lozupone@colorado.edu>
2009-11-09 11:06:26 +0000

    major changes including:
    > 1) Optionally supplying a category mapping file to which the taxon  
    > summary
    > information can be added. If not supplied keeps the original format  
    > 2) Added the option to report relative abundances rather than raw  
    > counts. Made
    > it default behavior to report relative abundances since I think that  
    > this is what
    > people more often want to evaluate.
    > 3) Added the option to use a delimitor in taxon strings other than a  
    > semi-colon. Uses the semi-colon by default.

--- qiime/summarize_taxa.py
+++ qiime/summarize_taxa.py
@@ -15,6 +15,7 @@ from sys import stdout, stderr
 from optparse import OptionParser
 from string import strip
 from numpy import array
+from otu_category_significance import convert_OTU_table_relative_abundance
 
 def parse_command_line_parameters():
     """ Parses command line arguments """
@@ -35,36 +36,114 @@ def parse_command_line_parameters():
           type='int',dest='level', default=2, 
           help='Level of taxonomy to use')
 
-    opts,args = parser.parse_args()
+    parser.add_option('-m','--category_mapping',action='store',\
+          type='string',dest='category_mapping', 
+          help='if supplied - the taxon information will be added ' +\
+             'to the category mapping file. This mapping file can ' +\
+             'be used to color PCoA plots by taxon abundance or to ' +\
+             'perform statistical tests of taxon/category associations.')
+   
+    parser.add_option('-d','--delimitor',action='store',\
+          type='string',dest='delimitor',default=';', 
+          help='Delimitor that separates taxonomy categories. default=;')
+    
+    parser.add_option('-r', '--relative_abundance', action='store',\
+        type='string', dest='relative_abundance', default='True', \
+        help='If True, reports the relative abundance of the lineage in ' +\
+            'each sample. If False, reports the raw counts. Default=True')
 
+    opts,args = parser.parse_args()
     return opts, args
+
+def make_new_summary_file(otu_table, level, delimitor, relative_abundance): 
+    """makes a summary file with taxa on rows and samples in columns
+    """
+    #Note: the OTU table is often too large to read into memory, hence
+    #working on file directly.
+    result = {}
+    output = []
+    if relative_abundance == 'True':
+        otu_table = convert_OTU_table_relative_abundance(otu_table)
+    for line in otu_table:
+        if line.startswith('#OTU ID'):
+            line = line.replace('#OTU ID', 'Taxon')
+            line = line.replace('\tConsensus Lineage', '')
+            output.append(line + '\n')
+        elif line.startswith('#'):
+            output.append(line + '\n')
+        else:
+            result = process_data_line(line, result, delimitor, level)
+    for key, val in sorted(result.items()):
+        output.append('\t'.join([key] + map(str, val))+'\n')
+    return output
+
+def add_summary_category_mapping(otu_table, category_mapping, \
+    level, delimitor, relative_abundance): 
+    """makes a summary file with taxa on rows and samples in columns
+    """
+    #Note: the OTU table is often too large to read into memory, hence
+    #working on file directly.
+    result = {}
+    output = []
+    if relative_abundance == 'True':
+        otu_table = convert_OTU_table_relative_abundance(otu_table)
+    for line in otu_table:
+        if line.startswith('#OTU ID'):
+            samples = line.strip().split('\t')[1:-1]
+        if not line.startswith('#'):
+            result = process_data_line(line, result, delimitor, level)
+    for line in category_mapping:
+        if line.startswith('#SampleID'):
+            header = line.strip().split('\t')
+            header.extend(sorted(result.keys()))
+            output.append('\t'.join(header) + '\n')
+        elif line.startswith('#'):
+            output.append(line + '\n')
+        else:
+            line = line.strip().split('\t')
+            sample_name = line[0]
+            index = samples.index(sample_name)
+            for key, val in sorted(result.items()):
+                line.append(str(val[index]))
+            output.append('\t'.join(line) + '\n')
+    return output
+
+def process_data_line(line, result, delimitor, level):
+    """
+    """
+    fields = line.split('\t')
+    vals = [float(i) for i in fields[1:-1]]
+    data = array(vals, dtype=float)
+    tax = map(strip, fields[-1].split(delimitor)[:level])
+    if len(tax) < level:
+        tax.append('Other')
+    tax_str = ';'.join(tax)
+    if tax_str in result:
+        result[tax_str] += data
+    else:
+        result[tax_str] = data
+    return result
+
 if __name__ == "__main__":
     opts,args = parse_command_line_parameters()
     output_fname = opts.out_fp
-    result = {}
-    #Note: the OTU table is often too large to read into memory, hence
-    #working on file directly.
+    otu_fp = opts.otu_fp
+    otu_table = open(otu_fp, 'U')
+    delimitor = opts.delimitor
+    category_mapping = opts.category_mapping
+    if category_mapping:
+        category_mapping = open(category_mapping)
+    level = opts.level 
+    relative_abundance=opts.relative_abundance
     if output_fname:
         outfile = open(output_fname, 'w')
     else:
         outfile = stdout
-    level = opts.level 
-    for line in open(opts.otu_fp, 'U'):
-        if line.startswith('#OTU ID'):
-            outfile.write(line.replace('#OTU ID', 'Taxon'))
-        elif line.startswith('#'):
-            outfile.write(line)
-        else:
-            fields = line.split('\t')
-            vals = [int(i) for i in fields[1:-1]]
-            data = array(vals, dtype=int)
-            tax = map(strip, fields[-1].split(';'))[:level]
-            if len(tax) < level:
-                tax.append('Other')
-            tax_str = ';'.join(tax)
-            if tax_str in result:
-                result[tax_str] += data
-            else:
-                result[tax_str] = data
-    for key, val in sorted(result.items()):
-        outfile.write('\t'.join([key] + map(str, val))+'\n')
+    if not category_mapping:
+        output = make_new_summary_file(otu_table, level, delimitor, \
+            relative_abundance) 
+    else:
+        output = add_summary_category_mapping(otu_table, category_mapping, \
+            level, delimitor, relative_abundance) 
+    outfile.write(''.join(output))
+
--- tests/test_summarize_taxa.py
+++ tests/test_summarize_taxa.py
@@ -13,7 +13,8 @@ __email__ = "rob@spot.colorado.edu"
 __status__ = "Prototype"
 
 from cogent.util.unit_test import TestCase, main
-from qiime.summarize_taxa import parse_command_line_parameters #no functions yet
+from qiime.summarize_taxa import parse_command_line_parameters, \
+    make_new_summary_file, add_summary_category_mapping
 
 class TopLevelTests(TestCase):
     """Tests of top-level functions"""
@@ -21,6 +22,44 @@ class TopLevelTests(TestCase):
     def test_import(self):
         """empty test just verifies import of module"""
         pass
+
+    def test_make_new_summary_file(self):
+        """make_new_summary_file works
+        """
+        otu_table="""#Full OTU Counts
+#OTU ID\ts1\ts2\ts3\ts4\tConsensus Lineage
+0\t1\t0\t2\t4\tRoot;Bacteria;Actinobacteria;Actinobacteria;Coriobacteridae;Coriobacteriales;Coriobacterineae;Coriobacteriaceae
+1\t1\t2\t0\t1\tRoot;Bacteria;Firmicutes;"Clostridia"
+2\t0\t1\t1\t0\tRoot;Bacteria;Firmicutes;"Clostridia"
+3\t1\t2\t1\t0\tRoot;Bacteria""".split('\n')
+        result = make_new_summary_file(otu_table, 3, ';', 'False')
+        self.assertEqual(result, ['#Full OTU Counts\n', 'Taxon\ts1\ts2\ts3\ts4\n', 'Root;Bacteria;Actinobacteria\t1.0\t0.0\t2.0\t4.0\n', 'Root;Bacteria;Firmicutes\t1.0\t3.0\t1.0\t1.0\n', 'Root;Bacteria;Other\t1.0\t2.0\t1.0\t0.0\n'])
+        #test that works with relative abundances
+        result = make_new_summary_file(otu_table, 3, ';', 'True')
+        self.assertEqual(result, ['#Full OTU Counts\n', 'Taxon\ts1\ts2\ts3\ts4\n', 'Root;Bacteria;Actinobacteria\t0.333333333333\t0.0\t0.5\t0.8\n', 'Root;Bacteria;Firmicutes\t0.333333333333\t0.6\t0.25\t0.2\n', 'Root;Bacteria;Other\t0.333333333333\t0.4\t0.25\t0.0\n'])
+
+    def test_add_summary_category_mapping(self):
+        """make_new_summary_file works
+        """
+        otu_table="""#Full OTU Counts
+#OTU ID\ts1\ts2\ts3\ts4\tConsensus Lineage
+0\t1\t0\t2\t4\tRoot;Bacteria;Actinobacteria;Actinobacteria;Coriobacteridae;Coriobacteriales;Coriobacterineae;Coriobacteriaceae
+1\t1\t2\t0\t1\tRoot;Bacteria;Firmicutes;"Clostridia"
+2\t0\t1\t1\t0\tRoot;Bacteria;Firmicutes;"Clostridia"
+3\t1\t2\t1\t0\tRoot;Bacteria""".split('\n')
+        cat_mapping="""#SampleID\tBarcodeSequence\tTreatment\tDescription
+#Test mapping file
+s1\tAAAA\tControl\tControl mouse, I.D. 354
+s2\tGGGG\tControl\tControl mouse, I.D. 355
+s3\tCCCC\tExp\tDisease mouse, I.D. 356
+s4\tTTTT\tExp\tDisease mouse, I.D. 357""".split('\n')
+        result = add_summary_category_mapping(otu_table, cat_mapping, 3, ';',
+            'False')
+        self.assertEqual(result, ['#SampleID\tBarcodeSequence\tTreatment\tDescription\tRoot;Bacteria;Actinobacteria\tRoot;Bacteria;Firmicutes\tRoot;Bacteria;Other\n', '#Test mapping file\n', 's1\tAAAA\tControl\tControl mouse, I.D. 354\t1.0\t1.0\t1.0\n', 's2\tGGGG\tControl\tControl mouse, I.D. 355\t0.0\t3.0\t2.0\n', 's3\tCCCC\tExp\tDisease mouse, I.D. 356\t2.0\t1.0\t1.0\n', 's4\tTTTT\tExp\tDisease mouse, I.D. 357\t4.0\t1.0\t0.0\n'])
+        result = add_summary_category_mapping(otu_table, cat_mapping, 3, ';',
+            'True')
+        self.assertEqual(result, ['#SampleID\tBarcodeSequence\tTreatment\tDescription\tRoot;Bacteria;Actinobacteria\tRoot;Bacteria;Firmicutes\tRoot;Bacteria;Other\n', '#Test mapping file\n', 's1\tAAAA\tControl\tControl mouse, I.D. 354\t0.333333333333\t0.333333333333\t0.333333333333\n', 's2\tGGGG\tControl\tControl mouse, I.D. 355\t0.0\t0.6\t0.4\n', 's3\tCCCC\tExp\tDisease mouse, I.D. 356\t0.5\t0.25\t0.25\n', 's4\tTTTT\tExp\tDisease mouse, I.D. 357\t0.8\t0.2\t0.0\n'])
+
 
 #run unit tests if run from command-line
 if __name__ == '__main__':
