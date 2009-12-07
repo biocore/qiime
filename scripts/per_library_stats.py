10a43e4fb7e9245bb9fe38c17dddf51119e01939
Greg Caporaso <gregcaporaso@gmail.com>
2009-12-07 15:24:35 +0000

    Updated this script to use the OTU table as input, per Justin's suggestion.

--- scripts/per_library_stats.py
+++ scripts/per_library_stats.py
@@ -14,19 +14,19 @@ __status__ = "Prototype"
 from numpy import min, max, median, mean
 from cogent.parse.fasta import MinimalFastaParser
 from optparse import OptionParser
+from qiime.parse import parse_otus
 
-usage_str = """usage: %prog [options] 
+usage_str = """usage: %prog [options] {-i INPUT_OTU_TABLE}
 
 [] indicates optional input (order unimportant)
 {} indicates required input (order unimportant)
 
-Given fasta file with seq identifiers of the form <libraryid>_<seqid>
- compute and print the (min, max, median, mean) number of seqs per
- library.
+Given an otu table compute and print the (min, max, median, mean) 
+ number of seqs per library.
 
 Example usage:
 
- python Qiime/scripts/per_library_stats.py -i in.fasta 
+ python Qiime/scripts/per_library_stats.py -i otu_table.txt
 
 
 """
@@ -42,14 +42,14 @@ def parse_command_line_parameters():
         dest='verbose',help='Print information during execution -- '+\
         'useful for debugging [default: %default]')
 
-    parser.add_option('-i','--input_fasta_file',\
-         help='the input fasta file [REQUIRED]')
+    parser.add_option('-i','--input_otu_table',\
+         help='the input otu table [REQUIRED]')
 
     # Set default values here if they should be other than None
     parser.set_defaults(verbose=False)
 
     opts,args = parser.parse_args()
-    required_options = ['input_fasta_file']
+    required_options = ['input_otu_table']
     
     for option in required_options:
         if eval('opts.%s' % option) == None:
@@ -57,25 +57,31 @@ def parse_command_line_parameters():
 
     return opts,args
 
-def compute_stats(fasta_f,lib_seq_delimited='_'):
-    """ """
-    d = {}
-    for seq_id, seq in MinimalFastaParser(fasta_f):
-        lib_id = seq_id.split(lib_seq_delimited)[0]
-        try:
-            d[lib_id] +=1
-        except KeyError:
-            d[lib_id] = 1
-            
-    counts = d.values()
+# def compute_stats(fasta_f,lib_seq_delimited='_'):
+#     """ """
+#     d = {}
+#     for seq_id, seq in MinimalFastaParser(fasta_f):
+#         lib_id = seq_id.split(lib_seq_delimited)[0]
+#         try:
+#             d[lib_id] +=1
+#         except KeyError:
+#             d[lib_id] = 1
+#             
+#     counts = d.values()
+#     return min(counts), max(counts), median(counts), mean(counts)
+
+def compute_stats(otu_f):
+    counts = []
+    otu_table = parse_otus(otu_f)[2]
+    for i in range(otu_table.shape[1]):
+        counts.append(sum(otu_table[:,i]))
+        
     return min(counts), max(counts), median(counts), mean(counts)
-
-
 
 if __name__ == "__main__":
     opts,args = parse_command_line_parameters()
     verbose = opts.verbose
     
-    print compute_stats(open(opts.input_fasta_file))
+    print compute_stats(open(opts.input_otu_table,'U'))
     
     
