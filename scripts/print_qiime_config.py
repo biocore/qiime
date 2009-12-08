39f2feb15557cf5f2871289a28a8f58f45460ffe
Jens Reeder <jens.reeder@gmail.com>
2009-12-08 18:07:04 +0000

    Add simple util that prints the values of the qiime config.

---
+++ scripts/print_qiime_config.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python 
+
+"""A simple scripts that prints out the qiime config settings."""
+
+from optparse import OptionParser
+from qiime.util import load_qiime_config
+
+__author__ = "Jens Reeder"
+__copyright__ = "Copyright 2009, Qiime"
+__credits__ = ["Jens Reeder"]
+__license__ = "GPL"
+__version__ = "0.1"
+__maintainer__ = "Jens Reeder"
+__email__ = "gregcaporaso@gmail.com"
+__status__ = "Prototype"
+
+def parse_command_line_parameters(commandline_args=None):
+    """returns command-line options"""
+    from sys import argv
+
+    usage = """print_qiime_config.py
+
+Prints out the values of all qimme variables stored in the qiime_config file."""
+
+    version = '%prog ' + str(__version__)
+    parser = OptionParser(usage=usage, version=version)
+    opts,args = parser.parse_args(commandline_args)
+    
+    return opts,args
+
+def main(commandline_args=None):
+
+    opts, args = parse_command_line_parameters(commandline_args)
+    qiime_config = load_qiime_config()
+    for key,value in  qiime_config.items():
+        print "%20s:\t%s"%(key,value)
+
+if __name__ == "__main__":
+
+    main()
