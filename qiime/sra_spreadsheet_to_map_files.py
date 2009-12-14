fa76274b4284fc68f297e8e9810dd4e092546c38
Rob Knight <rob.knight@colorado.edu>
2009-12-14 06:05:49 +0000

    Now splits by combination of study and run prefix to allow barcode reuse.

--- qiime/sra_spreadsheet_to_map_files.py
+++ qiime/sra_spreadsheet_to_map_files.py
@@ -5,22 +5,22 @@ from os.path import split, join
 from collections import defaultdict
 """This script reads the SRA submission spreadsheet, makes QIIME map files.
 
-Produces one map file per EXPERIMENT -- note that this will include extra
-stuff not actually needed by QIIME. Intention is just to pull out the info
-needed for split_libaries and downstream analysis. Does not currently combine
-this with the data in the per-sample mapping file, but this is planned for
-the future.
+Produces one map file per (STUDY, RUN_PREFIX) combination. Note that the 
+output will include extra stuff not actually needed by QIIME. Intention is 
+just to pull out the info needed for split_libaries and downstream analysis. 
+Does not currently combine this with the data in the per-sample mapping file, 
+but this is planned for the future.
 """
 
 def strip_quotes(s):
     """strips whitespace and terminal quotes from a string."""
     return s.strip().strip('"')
 
-def collect_study_groups(fields, col_index=0):
+def collect_study_groups(fields, study_index=0, run_prefix_index=1):
     """Returns dict of {study_group:[fields]}"""
     result = defaultdict(list)
     for f in fields:
-        result[f[col_index]].append(f)
+        result[(f[col_index], f[run_prefix_index])].append(f)
     return result
 
 def remap_lines(col_names, lines):
@@ -40,10 +40,10 @@ def write_map_files(infile):
     map_header, map_lines = map_lines[0], map_lines[1:]
     col_names = map(strip_quotes, map_header)
     study_groups = collect_study_groups(map_lines, 
-        col_names.index('STUDY_REF'))
+        col_names.index('STUDY_REF'), col_names.index('RUN_PREFIX'))
     basedir, filename = split(argv[1])
     for name, lines in study_groups.items():
-        outfile = open(join(basedir, name)+'.map', 'w')
+        outfile = open(join(basedir, '_'.join(name))+'.map', 'w')
         for line in remap_lines(col_names, lines):
             outfile.write('\t'.join(line) + '\n')
 
