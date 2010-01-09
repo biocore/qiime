339326a446eba5511f13ea5d21575b849373b1d4
Justin Kuczynski <justinak@gmail.com>
2010-01-09 04:31:29 +0000

    now outputs 3 files, one with internal nodes named with bootstrap support vals.  other less major changes too

--- qiime/tree_compare.py
+++ qiime/tree_compare.py
@@ -3,12 +3,14 @@ from __future__ import division
 from optparse import OptionParser
 from cogent.core.tree import PhyloNode
 from cogent.parse.tree import DndParser
+import qiime.parse
 import os.path
 import sys
+from warnings import warn
 
 __author__ = "Justin Kuczynski"
-__copyright__ = "Copyright 2009, the PyCogent Project" #consider project name
-__credits__ = ["Justin Kuczynski"] #remember to add yourself
+__copyright__ = "Copyright 2009, the PyCogent Project"
+__credits__ = ["Justin Kuczynski"]
 __license__ = "GPL"
 __version__ = "0.1"
 __maintainer__ = "Justin Kuczynski"
@@ -19,8 +21,7 @@ __status__ = "Prototype"
 nodes.  main() is called when run from command line, primary function of this
 module is bootstrap_support()
 
-command line usage example:
-python tree_compare.py -m TEST/sample_cluster.tre -s TEST/rare_unifrac_upgma -o TEST/unifrac_jackknife
+see command line usage example called with -h
 """
 
 
@@ -28,13 +29,30 @@ def main(options):
     tree_file_names = os.listdir(options.support_dir)
     tree_file_names = [fname for fname in tree_file_names if not \
         fname.startswith('.')]
+    try:
+        #try to warn user if using multiple types of trees
+        base_names = [qiime.parse.parse_rarefaction_fname(fname)[0]
+            for fname in tree_file_names]
+        if len(set(base_names)) > 1:
+            warnstr = """
+warning: support trees are named differently, please be sure you're not 
+comparing trees generated in different manners, unless you're quite sure 
+that's what you intend to do.  types: """ + str(set(base_names))
+            warn(warnstr)
+    except:
+        pass
 
     support_trees = []
     for fname in tree_file_names:
-        f = open(os.path.join(options.support_dir, fname))
-        tree = DndParser(f, PhyloNode)
-        support_trees.append(tree)
-        f.close()
+        try:
+            f = open(os.path.join(options.support_dir, fname))
+            tree = DndParser(f, PhyloNode)
+            tree.filepath = fname
+            support_trees.append(tree)
+            f.close()
+        except IOError, err:
+            sys.stderr.write('error loading support tree ' + fname + '\n')
+            exit(1)
     master_tree = DndParser(open(options.master_tree), PhyloNode)
 
     # get support of each node in master
@@ -48,10 +66,18 @@ def main(options):
     f.close()
     
     f = open(os.path.join(options.output_dir, 'jackknife_support.txt'), 'w')
+    f.write('total support trees considered: ' + str(len(support_trees)) + '\n')
     for key, val in bootstraps.items():
         f.write("\t".join([str(key),str(val)]) + "\n")
     f.close()
     
+    # we ruin master_tree here, this better be the end
+    f = open(os.path.join(options.output_dir, 'jackknife_named_nodes.tre'), 'w')
+    for name, support_val in bootstraps.items():
+        node = new_master.getNodeMatchingName(name)
+        node.Name = str(support_val)
+    f.write(new_master.getNewick(with_distances=True))
+    f.close()
 
 
 def bootstrap_support(master_tree, trees):
@@ -99,11 +125,14 @@ def compare(master, subsampled_tree):
     """ compares master tree to subsampled_tree tree, modifies master in place
     """
     if set(master.getTipNames()) != set(subsampled_tree.getTipNames()):
-        raise ValueError(\
-            "different number of tips in subsampled_tree and master.\n"+\
-            "typically some sapmples with few sequences were skipped in"+\
-            " the support trees.  remove offending samples from the master"+\
-            " tree, and try again")
+        raise ValueError("""
+didn't like subsampled tree """ + subsampled_tree.filepath + """
+different number of tips in subsampled_tree and master.\n
+typically some samples with few sequences were skipped in
+the support trees.  be sure to keep all samples when doing rarefaction of
+otu tables (see --small_included option), or
+remove offending samples from the master tree, and try again)
+""")
     # list of lists.  each elem is list of tip names for a node
     subsampled_tree_nodes_names = [node.getTipNames() for node in \
         subsampled_tree.iterNontips(include_self=True)]
@@ -119,9 +148,11 @@ usage_str = """usage: %prog [options] {-i INPUT_PATH -o OUTPUT_DIR -m MASTER_TRE
 {} indicates required input (order unimportant)
 
 Example usage:
-python %prog -m TEST/sample_cluster.tre -s TEST/rare_unifrac_upgma/ -o TEST/unifrac_jackknife/
-this makes the folder TEST/unifrac_jackknife.  In that is the master tree,
-with internal nodes named, and a separate bootstrap/jackknife support file
+python %prog -m sample_cluster.tre -s rare_unifrac_upgma/ -o unifrac_jackknife/
+this makes the folder unifrac_jackknife.  In that is the master tree,
+with internal nodes named uniquely, a separate bootstrap/jackknife support file,
+and a jackknife_named_nodes.tre tree, for use with e.g.: figtree
+output jackknife support values 
 
 master tree must have the same tips as support trees.  if your support trees
 omit some tips (e.g.: samples with few sequences),
