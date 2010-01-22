6a823913f4e2b5edb4971f9a1d524b9dc4112287
Greg Caporaso <gregcaporaso@gmail.com>
2010-01-22 20:40:57 +0000

    Updated qiime_config and filter_alignment.py to handle a template_alignment_lanemask_fp value t be defined by the user.

--- qiime/filter_alignment.py
+++ qiime/filter_alignment.py
@@ -8,6 +8,7 @@ from sys import stdout
 from string import lowercase
 from os.path import split, exists, splitext
 from os import mkdir, remove
+from qiime.util import qiime_config
 
 __author__ = "Dan Knights"
 __copyright__ = "Copyright 2009, the PyCogent Project"
@@ -34,21 +35,21 @@ Description:
 Example usage:
 
  # Get detailed usage information
- python minimal_filter_alignment.py -h
+ python filter_alignment.py -h
 
  # filter 1.fasta using the lanemask in lm.txt, but filtering no gaps (b/c
  # --allowed_gap_frac=1.0, meaning positions can be up to 100% gap); output
  # written to ./1_pfiltered.fasta
- python minimal_filter_alignment.py -i 1.fasta -g 1.0 -m lm.txt
+ python filter_alignment.py -i 1.fasta -g 1.0 -m lm.txt
  
  # filter 1.fasta using the lanemask in lm.txt and filter positions which are
  # 100% gap (default -g behavior); output written to ./1_pfiltered.fasta
- python minimal_filter_alignment.py -i 1.fasta -o ./ -m lm.txt
+ python filter_alignment.py -i 1.fasta -o ./ -m lm.txt
  
  # filter 1.fasta positions which are 100% gap (default -g behavior) but no lane mask
  # filtering (because no lane mask file provided with -l); output written to 
  # ./1_pfiltered.fasta
- python minimal_filter_alignment.py -i 1.fasta -o ./"""
+ python filter_alignment.py -i 1.fasta -o ./"""
 
 
 def mask_to_positions(maskstring):
@@ -181,8 +182,13 @@ def parse_command_line_parameters():
            type='string',help='the output directory '+\
            '[default: %default]',default='.')
     parser.add_option('-m','--lane_mask_fp',action='store',\
-            type='string',help='path to lanemask file '+\
-            '[default: %default]')
+            type='string',
+            default=qiime_config['template_alignment_lanemask_fp'],
+            help='path to lanemask file [default: %default]')
+    parser.add_option('-s','--suppress_lane_mask_filter',action='store_true',\
+            help='suppress lane mask filtering (necessary to turn off '+\
+             'lane-mask-based filtering when a qiime_config default is  '+\
+             'provided for --lane_mask_fp) [default: %default]',default=False)
     parser.add_option('-g','--allowed_gap_frac',action='store',\
             type='float',help='gap filter threshold, ' +\
             'filters positions which are gaps in > allowed_gap_frac '+\
@@ -222,7 +228,7 @@ if __name__ == "__main__":
     
     # read the lane_mask, if one was provided
     if opts.verbose: print "Reading lane mask..."
-    if opts.lane_mask_fp:
+    if opts.lane_mask_fp and not opts.suppress_lane_mask_filter:
         lane_mask = open(opts.lane_mask_fp).read().strip()
     else:
         lane_mask = None
