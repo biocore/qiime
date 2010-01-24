c1b0ff56900c0e9270085c1c00c95762a579792c
Justin Kuczynski <justinak@gmail.com>
2010-01-24 18:36:59 +0000

    added minimal tests for make_bootstrapped_tree and principal_coordinates

---
+++ tests/test_make_bootstrapped_tree.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+__author__ = "Justin Kuczynski"
+__copyright__ = "Copyright 2009, Qiime"
+__credits__ = ["justin kuczynski"]
+__license__ = "GPL"
+__version__ = "0.1"
+__maintainer__ = "Justin Kuczynski"
+__email__ = "justinak@gmail.com"
+__status__ = "Prototype"
+
+from qiime.make_bootstrapped_tree import write_pdf_bootstrap_tree
+from cogent.util.unit_test import TestCase, main
+from cogent.core.tree import PhyloNode
+from cogent.parse.tree import DndParser
+from cogent.app.util import get_tmp_filename
+import os
+
+
+def remove_files(list_of_filepaths,error_on_missing=True):
+    missing = []
+    for fp in list_of_filepaths:
+        try:
+            os.remove(fp)
+        except OSError:
+            missing.append(fp)
+
+    if error_on_missing and missing:
+        raise OSError, "Some filepaths were not accessible: %s"\
+            % '\t'.join(missing)
+
+class FunctionTests(TestCase):
+    """Tests of top-level functions"""
+
+    def setUp(self):
+        self._paths_to_clean_up = []
+    def test_write_pdf_bootstrap_tree(self):
+        """ write_pdf_bootstrap_tree should throw no errors"""
+        
+        tree = DndParser(
+            "((tax7:0.1,tax3:0.2)node0:.98,tax8:.3, tax4:.3)node1:.4",
+            PhyloNode)
+        bootstraps = {'node0':.7,'node1':.4}
+        f = get_tmp_filename(prefix='bootstrap_tests_',suffix='.pdf')
+        self._paths_to_clean_up.append(f)
+        write_pdf_bootstrap_tree(tree, f, bootstraps)
+        assert(os.path.exists(f))
+
+    def tearDown(self):
+        remove_files(self._paths_to_clean_up)
+        
+
+#run tests if called from command line
+if __name__ == '__main__':
+    main()
---
+++ tests/test_principal_coordinates.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+
+__author__ = "Justin Kuzynski"
+__copyright__ = "Copyright 2009, Qiime"
+__credits__ = ["Justin Kuczynski"]
+__license__ = "GPL"
+__version__ = "0.1"
+__maintainer__ = "Justin Kuczynski"
+__email__ = "justinak@gmail.com"
+__status__ = "Prototype"
+
+from qiime.principal_coordinates import pcoa
+from cogent.util.unit_test import TestCase, main
+
+
+class FunctionTests(TestCase):
+    """Tests of top-level functions"""
+
+    def setUp(self):
+        self.distmtx_txt = """\tsam1\tsam2\tsam3
+sam1\t0.00\t.18\t.44
+sam2\t0.18\t0\t.66
+sam3\t.44\t.66\t0""".split('\n')
+
+    def test_pcoa(self):
+        """ pcoa should throw no errors"""
+        res = pcoa(self.distmtx_txt)
+        assert res # formatting tested elsewhere
+
+
+#run tests if called from command line
+if __name__ == '__main__':
+    main()
