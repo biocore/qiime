8e8cd45b7405502834e4ce5a3bb4abebfb2cfde6
Rob Knight <rob.knight@colorado.edu>
2010-01-25 07:15:56 +0000

    Added test stub for make_per_library_sff.

---
+++ tests/test_make_per_library_sff.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from cogent.util.unit_test import TestCase, main
+from qiime.make_per_library_sff import (make_option_parser) 
+"""Tests of the trim_sff_primers.py file.
+
+Note: this is presently just a stub that tests import as everything is 
+in the main block: this needs to be refactored. Integration tests have
+been performed in the hand study submission.
+"""
+__author__ = "Rob Knight"
+__copyright__ = "Copyright 2009, the PyCogent Project"
+#remember to add yourself if you make changes
+__credits__ = ["Rob Knight"]
+__license__ = "GPL"
+__version__ = "0.1"
+__maintainer__ = "Rob Knight"
+__email__ = "rob@spot.colorado.edu"
+__status__ = "Prototype"
+
+class TopLevelTests(TestCase):
+    """Top-level tests of functions in make_per_library_sff"""
+    pass
+
+if __name__ == '__main__':
+    main()
