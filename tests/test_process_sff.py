b336b1cebf2080506fb2663da4a91f28d55f34ca
Kyle Bittinger <kylebittinger@gmail.com>
2010-02-03 14:38:14 +0000

    Removed external file dependency (with exception of binary test.sff) for test_process_sff.py

--- tests/test_process_sff.py
+++ tests/test_process_sff.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
-from os import remove, getcwd
+import shutil
+import tempfile
 from cogent.util.unit_test import TestCase, main
 from cogent.app.util import ApplicationNotFoundError
 from cogent.util.misc import app_path
@@ -24,46 +25,68 @@ class TopLevelTests(TestCase):
     """Top-level tests of functions in process_sff"""
     
     def setUp(self):
-        """ """
-        qiime_dir = get_qiime_project_dir()
-        self.sra_test_files_dir = os.path.join(
-            qiime_dir, 'tests', 'sra_test_files')
+        """Create temporary directory of SFF files"""
+        if not app_path('sffinfo'):
+            raise ApplicationNotFoundError(
+                "Can't find sffinfo. Is it installed? Is it in your $PATH?")
+
+        sff_original_fp = os.path.join(
+            get_qiime_project_dir(), 'tests', 'sra_test_files', 'test.sff')
+
+        # copy sff file to working directory
+        self.sff_dir = tempfile.mkdtemp()
+        self.sff_fp = os.path.join(self.sff_dir, 'test.sff')
+        shutil.copy(sff_original_fp, self.sff_fp)
+
+    def tearDown(self):
+        shutil.rmtree(self.sff_dir)
 
     def test_make_fna(self):
         """test_make_fna should make fasta file as expected"""
-        if not app_path('sffinfo'):
-            raise ApplicationNotFoundError, \
-             "Can't find sffinfo. Is it installed? Is it in your $PATH?"
-        make_fna('%s/test.sff' % self.sra_test_files_dir)
-        result = open('%s/test.fna' % self.sra_test_files_dir).read()
-        self.assertEqual(result, \
-         '>FA6P1OK01CGMHQ length=48 xy=0892_1356 region=1 run=R_2008_05_28_17_11_38_\nATCTGAGCTGGGTCATAGCTGCCTCCGTAGGAGGTGCCTCCCTACGGC\n')
-        remove('%s/test.fna' % self.sra_test_files_dir)
+        make_fna(self.sff_fp)
+        expected_fp = os.path.join(self.sff_dir, 'test.fna')
+        observed = open(expected_fp).read()
+        expected = (
+            '>FA6P1OK01CGMHQ length=48 xy=0892_1356 region=1 run=R_2008_05_28_17_11_38_\n'
+            'ATCTGAGCTGGGTCATAGCTGCCTCCGTAGGAGGTGCCTCCCTACGGC\n'
+            )
+        self.assertEqual(observed, expected)
 
     def test_make_qual(self):
         """test_make_qual should make qual file as expected"""
-        if not app_path('sffinfo'):
-            raise ApplicationNotFoundError, \
-             "Can't find sffinfo. Is it installed? Is it in your $PATH?"
-        make_qual('%s/test.sff' % self.sra_test_files_dir)
-        result = open('%s/test.qual'% self.sra_test_files_dir).read()
-        self.assertEqual(result,\
-        '>FA6P1OK01CGMHQ length=48 xy=0892_1356 region=1 run=R_2008_05_28_17_11_38_\n32 32 32 32 32 32 32 25 25 21 21 21 28 32 32 31 30 30 32 32 32 33 31 25 18 18 20 18 32 30 28 23 22 22 24 28 18 19 18 16 16 16 17 18 13 17 27 21\n')
-        remove('%s/test.qual' % self.sra_test_files_dir)
+        make_qual(self.sff_fp)
+        expected_fp = os.path.join(self.sff_dir, 'test.qual')
+        observed = open(expected_fp).read()
+        expected = (
+            '>FA6P1OK01CGMHQ length=48 xy=0892_1356 region=1 run=R_2008_05_28_17_11_38_\n'
+            '32 32 32 32 32 32 32 25 25 21 21 21 28 32 32 31 30 30 32 32 32 33 31 25 18 18 '
+            '20 18 32 30 28 23 22 22 24 28 18 19 18 16 16 16 17 18 13 17 27 21\n'
+            )
+        self.assertEqual(observed, expected)
 
     def test_prep_sffs_in_dir(self):
         """test_prep_sffs_in_dir should make fasta/qual from sffs."""
-        if not app_path('sffinfo'):
-            raise ApplicationNotFoundError, \
-             "Can't find sffinfo. Is it installed? Is it in your $PATH?"
-        prep_sffs_in_dir(self.sra_test_files_dir)
-        result = open('%s/test.qual' % self.sra_test_files_dir).read()
-        self.assertEqual(result, \
-        '>FA6P1OK01CGMHQ length=48 xy=0892_1356 region=1 run=R_2008_05_28_17_11_38_\n32 32 32 32 32 32 32 25 25 21 21 21 28 32 32 31 30 30 32 32 32 33 31 25 18 18 20 18 32 30 28 23 22 22 24 28 18 19 18 16 16 16 17 18 13 17 27 21\n')
-        remove('%s/test.qual' % self.sra_test_files_dir)
-        result = open('%s/test.fna' % self.sra_test_files_dir).read()
-        self.assertEqual(result, '>FA6P1OK01CGMHQ length=48 xy=0892_1356 region=1 run=R_2008_05_28_17_11_38_\nATCTGAGCTGGGTCATAGCTGCCTCCGTAGGAGGTGCCTCCCTACGGC\n')
-        remove('%s/test.fna' % self.sra_test_files_dir)
+        prep_sffs_in_dir(self.sff_dir)
+
+        # Check fna file
+        expected_fp = os.path.join(self.sff_dir, 'test.fna')
+        observed = open(expected_fp).read()
+        expected = (
+            '>FA6P1OK01CGMHQ length=48 xy=0892_1356 region=1 run=R_2008_05_28_17_11_38_\n'
+            'ATCTGAGCTGGGTCATAGCTGCCTCCGTAGGAGGTGCCTCCCTACGGC\n'
+            )
+        self.assertEqual(observed, expected)
+
+        # Check qual file
+        expected_fp = os.path.join(self.sff_dir, 'test.qual')
+        observed = open(expected_fp).read()
+        expected = (
+            '>FA6P1OK01CGMHQ length=48 xy=0892_1356 region=1 run=R_2008_05_28_17_11_38_\n'
+            '32 32 32 32 32 32 32 25 25 21 21 21 28 32 32 31 30 30 32 32 32 33 31 25 18 18 '
+            '20 18 32 30 28 23 22 22 24 28 18 19 18 16 16 16 17 18 13 17 27 21\n'
+            )
+        self.assertEqual(observed, expected)
+
 
 if __name__ == '__main__':
     main()
