400e00ef8e02bb6653de930dbb9b61e7e3d0ae7e
Rob Knight <rob.knight@colorado.edu>
2010-02-09 20:15:33 +0000

    Converted make_fastq to new script interface.

--- qiime/make_fastq.py
+++ qiime/make_fastq.py
@@ -10,7 +10,7 @@ __email__ = "rob@spot.colorado.edu"
 __status__ = "Pre-release"
 
 
-"""Generates fastq file for SRA submission from paired fasta and qual files.
+"""Generates fastq file for ERA submission from paired fasta and qual files.
 
 The fastq format for each record is as follows:
 @seq_id [and optional description]
@@ -18,16 +18,14 @@ seq as bases
 +[optionally with repeat of seq_id and repeat line]
 qual scores as string of chr(33+qual)
 
-The SRA currently requires a separate fastq file for each library, split by
+The ERA currently requires a separate fastq file for each library, split by
 library id. This code takes the output from split_libraries.py and the
 corresponding qual files, pulls the qual info by id, and writes everything
 either to one file or to per-library files.
 """
-from qiime.split_libraries import qual_scores
 from cogent.parse.fasta import MinimalFastaParser
 from os import mkdir
 from collections import defaultdict
-from optparse import OptionParser
 
 def make_fastq_rec(header, seq, qual, offset=33):
     """Makes fasta string given header, seq, qual scores as integers"""
@@ -92,44 +90,4 @@ def make_fastq_multi(in_fasta, quals, out_fp, \
         outfile = open(out_fp+'/'+lib+'.fastq', 'w')
         outfile.write('\n'.join(recs))
         outfile.close()
-
-def parse_command_line_parameters():
-    """ Parses command line arguments """
-    usage =\
-     'usage: %prog [options] -f input_fasta_filepath -q input_qual_filepath'
-    version = 'Version: %prog ' +  __version__
-    parser = OptionParser(usage=usage, version=version)
-
-    parser.add_option('-f', '--fasta', dest='fasta_fp',
-        help='name of fasta output of split_libraries.py [Required]')
-
-    parser.add_option('-q', '--qual', dest='qual_fps',
-        help='names of qual files, comma-delimited [Required]')
-
-    parser.add_option('-o','--result_fp',action='store',\
-          type='string',dest='result_fp',help='Path to store '+\
-          'results [default: <input_sequences_filename>.fastq]')
-
-    parser.add_option('-s', '--split', dest='split', action='store_true',
-        help='make separate file for each library [default:False]', 
-        default=False)
-          
-    parser.set_defaults(result_fp=None)
-
-    opts,args = parser.parse_args()
-    return opts,args
-
-if __name__ == "__main__":
-    opts,args = parse_command_line_parameters()
-    #verbose = opts.verbose
-    in_fasta = open(opts.fasta_fp, 'U')
-    quals = qual_scores([open(f, 'U') for f in opts.qual_fps.split(',')])
-    if not opts.result_fp:
-        opts.result_fp = opts.fasta_fp + '.fastq'
-
-    if opts.split:
-        make_fastq_multi(in_fasta, quals, opts.result_fp)
-    else:
-        make_fastq_single(in_fasta, quals, opts.result_fp)
-
 
