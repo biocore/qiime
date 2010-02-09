44abc7cc08fcf42bf330a09b2cac91de4806c663
Jens Reeder <jens.reeder@gmail.com>
2010-02-09 21:27:30 +0000

    move main and option parsers to scripts folder.

--- qiime/make_phylogeny.py
+++ qiime/make_phylogeny.py
@@ -10,20 +10,16 @@ __email__ = "justinak@gmail.com"
 __status__ = "Pre-release"
 
 
-"""Contains code for aligning sequences, using several techniques.
+"""Contains code for making a phylogeny from aligned sequences, using several techniques.
 
 This module has the responsibility for taking a set of sequences and
 returning an alignment. Mostly, it will be thin wrappers for code 
 already in cogent.app.*, to which wrappers for e.g. NAST need to be
 added..
 """
-from os import remove
-from os.path import splitext
-from commands import getoutput
-from optparse import OptionParser
+
 from cogent import LoadSeqs, DNA
 from cogent.parse.fasta import MinimalFastaParser
-from cogent.app.util import get_tmp_filename
 from qiime.util import FunctionWithParams
 #app controllers that implement align_unaligned_seqs
 import cogent.app.muscle
@@ -104,94 +100,13 @@ class CogentTreeBuilder(TreeBuilder):
     def __call__(self, result_path=None, log_path=None, *args, **kwargs):
         """Calls superclass method to align seqs"""
         return FunctionWithParams.__call__(self, result_path=result_path,
-            log_path=log_path, *args, **kwargs)
+                                           log_path=log_path, *args, **kwargs)
 
-
-usage_str = """usage: %prog [options] {-i INPUT_ALIGNMENT_FILEPATH}
-
-[] indicates optional input (order unimportant) 
-{} indicates required input (order unimportant) 
-
-Example usage:
-
-python make_phylogeny.py -i align/rep_set_aligned.fa -o align/rep_set_aligned.tre -l align/rep_set_tree.log 
-"""
-def parse_command_line_parameters():
-    """ Parses command line arguments """
-    version = 'Version: %prog ' +  __version__
-    parser = OptionParser(usage=usage_str, version=version)
-
-    parser.add_option('-i','--input_fp',action='store',\
-          type='string',dest='input_fp',help='Path to read '+\
-          'input alignment [REQUIRED]')
-
-    parser.add_option('-t','--tree_method',action='store',\
-          type='string',dest='tree_method',help='Method for tree building'+\
-          ' [default: %default]')
-          
-    parser.add_option('-o','--result_fp',action='store',\
-          type='string',dest='result_fp',help='Path to store '+\
-          'result file [default: <input_sequences_filename>.tre]')
-          
-    parser.add_option('-l','--log_fp',action='store',\
-          type='string',dest='log_fp',help='Path to store '+\
-          'log file [default: No log file created.]')
-
-    parser.set_defaults(tree_method='fasttree',result_fp=None)
-
-    opts,args = parser.parse_args()
-
-    required_options = ['input_fp']
-    for option in required_options:
-        if eval('opts.%s' % option) == None:
-            parser.error('Required option --%s omitted.' % option) 
-
-    if not (opts.tree_method in tree_method_constructors or
-            opts.tree_method in tree_module_names):
-        parser.error(\
-         'Invalid alignment method: %s.\nValid choices are: %s'\
-         % (opts.tree_method,\
-            ' '.join(tree_method_constructors.keys() +
-                tree_module_names.keys())))
-            
-    return opts,args
 
 tree_method_constructors = {}  #currently unused
 tree_module_names = {'muscle':cogent.app.muscle, 
-    'clustalw':cogent.app.clustalw, 'mafft':cogent.app.mafft,
+    'clustalw':cogent.app.clustalw,
+    #'mafft':cogent.app.mafft,   # current version of Mafft does not support tree building
     'fasttree':cogent.app.fasttree,'fasttree_v1':cogent.app.fasttree_v1,
     'raxml':cogent.app.raxml, 'clearcut':cogent.app.clearcut,
     }
-
-if __name__ == "__main__":
-    opts,args = parse_command_line_parameters()
-    #verbose = opts.verbose
- 
-    try:
-        tree_builder_constructor =\
-        tree_method_constructors[opts.tree_method]
-        tree_builder_type = 'Constructor'
-        params = {}
-        tree_builder = tree_builder_constructor(params)
-    except KeyError:
-        tree_builder = CogentTreeBuilder({
-        'Module':tree_module_names[opts.tree_method],
-        'Method':opts.tree_method
-        })
-        tree_builder_type = 'Cogent'
-     
-    input_seqs_filepath = opts.input_fp
-    result_path = opts.result_fp
-    if not result_path: # empty or None
-        fpath, ext = splitext(input_seqs_filepath) # fpath omits extension
-        result_path = fpath + ".tre"
-     
-    log_path = opts.log_fp
-
-    if tree_builder_type=='Constructor':
-        tree_builder(input_seqs_filepath,\
-        result_path=result_path,log_path=log_path,failure_path=failure_path)
-    elif tree_builder_type=='Cogent':
-        tree_builder(result_path, aln_path=input_seqs_filepath,
-            log_path=log_path)
-
---
+++ scripts/make_phylogeny.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# File created on 09 Feb 2010
+from __future__ import division
+
+__author__ = "Justin Kuczynski, Jens Reeder"
+__copyright__ = "Copyright 2010, The QIIME project"
+__credits__ = ["Justin Kuczynski"]
+__license__ = "GPL"
+__version__ = "1.0-dev"
+__maintainer__ = "Justin Kuczynski"
+__email__ = "justinak@gmail.com"
+__status__ = "Pre-release"
+ 
+from os.path import splitext
+from optparse import make_option
+
+from qiime.util import parse_command_line_parameters
+
+from qiime.make_phylogeny import tree_module_names, tree_method_constructors,\
+    CogentTreeBuilder
+
+script_description = """Build a phylogenetic tree from aligned sequences, using one of several techniques."""
+
+script_usage = """
+ Build phylogenetic tree from sequences in  align/rep_set_aligned.fa
+ writing tree to align/rep_set_aligned.tre using FastTree (default):
+    make_phylogeny.py -i align/rep_set_aligned.fa -o align/rep_set_aligned.tre -l align/rep_set_tree.log
+"""
+
+required_options = [\
+  make_option('-i','--input_fp',action='store',\
+          type='string',dest='input_fp',help='Path to read '+\
+          'input alignment')
+]
+
+optional_options = [\
+    make_option('-t','--tree_method',action='store',\
+          type='string',dest='tree_method',help='Method for tree building. Valid choices are: '+\
+          ', '.join(tree_module_names.keys())+\
+          ' [default: %default]', default='fasttree'),
+          
+    make_option('-o','--result_fp',action='store',\
+          type='string',dest='result_fp',help='Path to store '+\
+          'result file [default: <input_sequences_filename>.tre]'),
+          
+    make_option('-l','--log_fp',action='store',\
+          type='string',dest='log_fp',help='Path to store '+\
+          'log file [default: No log file created.]')
+]
+
+def main():
+    option_parser, opts, args = parse_command_line_parameters(
+      script_description=script_description,
+      script_usage=script_usage,
+      version=__version__,
+      required_options=required_options,
+      optional_options=optional_options)
+
+    if not (opts.tree_method in tree_method_constructors or
+            opts.tree_method in tree_module_names):
+        option_parser.error(\
+         'Invalid alignment method: %s.\nValid choices are: %s'\
+         % (opts.tree_method,\
+            ' '.join(tree_method_constructors.keys() +
+                tree_module_names.keys())))
+    
+    #verbose = opts.verbose
+    try:
+        tree_builder_constructor =\
+            tree_method_constructors[opts.tree_method]
+        tree_builder_type = 'Constructor'
+        params = {}
+        tree_builder = tree_builder_constructor(params)
+    except KeyError:
+        tree_builder = CogentTreeBuilder({
+                'Module':tree_module_names[opts.tree_method],
+                'Method':opts.tree_method
+                })
+        tree_builder_type = 'Cogent'
+     
+    input_seqs_filepath = opts.input_fp
+    result_path = opts.result_fp
+    if not result_path: # empty or None
+        fpath, ext = splitext(input_seqs_filepath) # fpath omits extension
+        result_path = fpath + ".tre"
+     
+    log_path = opts.log_fp
+
+    if tree_builder_type=='Constructor':
+        tree_builder(input_seqs_filepath,\
+        result_path=result_path,log_path=log_path,failure_path=failure_path)
+    elif tree_builder_type=='Cogent':
+        tree_builder(result_path, aln_path=input_seqs_filepath,
+            log_path=log_path)
+
+
+if __name__ == "__main__":
+    main()
