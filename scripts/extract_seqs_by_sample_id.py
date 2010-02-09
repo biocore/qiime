b57ea7e1bbf1f4435819292f0554071bfe76eef6
Meg Pirrung <meganap@gmail.com>
2010-02-09 21:39:25 +0000

    Updated extract_seqs_by_sample_id.py script

--- scripts/extract_seqs_by_sample_id.py
+++ scripts/extract_seqs_by_sample_id.py
@@ -12,60 +12,56 @@ __email__ = "gregcaporaso@gmail.com"
 __status__ = "Pre-release"
 
 
-from optparse import OptionParser
+from qiime.util import parse_command_line_parameters
+from optparse import make_option
 from qiime.util import extract_seqs_by_sample_id
 from cogent.parse.fasta import MinimalFastaParser
 
-usage_str = """usage: %prog [options] {-i INPUT_FASTA_FP -o OUTPUT_FASTA_FP -s SAMPLE_IDS}
+script_description = """This script creates a fasta file which will contain \
+only sequences that ARE associated with the supplied sampleIDs(-s), OR all sequences \
+that are NOT associated with the supplied sampleIDs(-n)"""
 
-[] indicates optional input (order unimportant)
-{} indicates required input (order unimportant)
+script_usage = """Usage: %prog [options] {-i INPUT_FASTA_FP -o OUTPUT_FASTA_FP -s SAMPLE_IDS}
 
 Example usage:
 
- Create the file outseqs.fasta (-o), which will be a subset of inseqs.fasta 
-  (-i) containing only the sequences associated with sample ids S2, S3, 
-  S4 (-s). (As always, sample IDs are case-sensitive.)
-  python extract_seqs_by_sample_id.py -i inseqs.fasta -o outseqs.fasta -s S2,S3,S4
-  
- Create the file outseqs.fasta (-o), which will be a subset of inseqs.fasta 
-  (-i) containing only the sequences  THAT ARE NOT (-n) associated with sample 
-  ids S2, S3, S4 (-s). (As always, sample IDs are case-sensitive.)
-  python extract_seqs_by_sample_id.py -i inseqs.fasta -o outseqs.fasta -s S2,S3,S4 -n
+Create the file outseqs.fasta (-o), which will be a subset of inseqs.fasta \
+(-i) containing only the sequences THAT ARE associated with sample ids S2, S3, \
+S4 (-s). (As always, sample IDs are case-sensitive.)
+
+python extract_seqs_by_sample_id.py -i inseqs.fasta -o outseqs.fasta -s S2,S3,S4
+
+Create the file outseqs.fasta (-o), which will be a subset of inseqs.fasta \
+(-i) containing only the sequences  THAT ARE NOT (-n) associated with sample \
+ids S2, S3, S4 (-s). (As always, sample IDs are case-sensitive.)
+
+python extract_seqs_by_sample_id.py -i inseqs.fasta -o outseqs.fasta -s S2,S3,S4 -n
 
 """
 
-def parse_command_line_parameters():
-    """ Parses command line arguments """
-    usage = usage_str
-    version = 'Version: %prog ' + __version__
-    parser = OptionParser(usage=usage, version=version)
+required_options = [\
+make_option('-i','--input_fasta_fp',\
+help='the input fasta file'),\
+make_option('-s','--sample_ids',\
+help="comma-separated sample_ids to include in output fasta file"+\
+"(or exclude if -n=True)"),\
+make_option('-o','--output_fasta_fp',\
+help='the output fasta file')
+]
 
-    parser.add_option('-i','--input_fasta_fp',\
-        help='the input fasta file [REQUIRED]')
-    parser.add_option('-s','--sample_ids',\
-        help="comma-separated sample_ids to include in output fasta file"+\
-             " (or exclude if -n=True) [REQUIRED]")
-    parser.add_option('-o','--output_fasta_fp',\
-        help='the output fasta file [REQUIRED]')
-    parser.add_option('-n','--negate',action='store_true',\
-        help='negate the sample ID list (i.e., output sample '+\
-             'ids not passed via -s) [default: %default]')
-    
-    # Set default values here if they should be other than None
-    parser.set_defaults(verbose=False,negate=False)
+optional_options = [\
+make_option('-n','--negate',action='store_true',default='false',\
+help='negate the sample ID list (i.e., output sample '+\
+'ids not passed via -s) [default: %default]')
+]
 
-    opts,args = parser.parse_args()
-    required_options = ['input_fasta_fp','output_fasta_fp','sample_ids']
-    
-    for option in required_options:
-        if eval('opts.%s' % option) == None:
-            parser.error('Required option --%s omitted.' % option) 
-
-    return opts,args
-
-if __name__ == "__main__":
-    opts,args = parse_command_line_parameters()
+def main():
+    option_parser, opts, args = parse_command_line_parameters(
+      script_description=script_description,
+      script_usage=script_usage,
+      version=__version__,
+      required_options=required_options,
+      optional_options=optional_options)
     
     sample_ids = opts.sample_ids.split(',')
     negate = opts.negate
@@ -73,17 +69,20 @@ if __name__ == "__main__":
     try:
         seqs = MinimalFastaParser(open(opts.input_fasta_fp))
     except IOError:
-        print "Cannot open %s. Does it exist? Do you have read access?" %\
-         opts.input_fasta_fp
+        option_parser.error('Cannot open %s. Does it exist? Do you have read access?'%\
+         opts.input_fasta_fp)
         exit(1)
         
     try:
         output_fasta_f = open(opts.output_fasta_fp,'w')
     except IOError:
-        print "Cannot open %s. Does path exist? Do you have write access?" %\
-         opts.output_fasta_fp
+        option_parser.error("Cannot open %s. Does path exist? Do you have write access?" %\
+         opts.output_fasta_fp)
         exit(1)
     
     for r in extract_seqs_by_sample_id(seqs,sample_ids,negate):
         output_fasta_f.write('>%s\n%s\n' % r)
     output_fasta_f.close()
+
+if __name__ == "__main__":
+    main()
