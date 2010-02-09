c331367ae90a6b2c6456ef3ad497d9c996e00678
Julia Goodrich <jkg59@cornell.edu>
2010-02-09 19:57:46 +0000

    Changed maintainer

--- qiime/summarize_otu_by_cat.py
+++ qiime/summarize_otu_by_cat.py
@@ -6,7 +6,7 @@ __copyright__ = "Copyright 2010, The QIIME Project"
 __credits__ = ["Julia Goodrich"] #remember to add yourself
 __license__ = "GPL"
 __version__ = "1.0-dev"
-__maintainer__ = "Rob Knight"
+__maintainer__ = "Julia Goodrich"
 __email__ = "julia.goodrich@colorado.edu"
 __status__ = "Pre-release"
 
