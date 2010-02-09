cc35762b97efd3a97ed322a3b240dfd6d7ee8343
Meg Pirrung <meganap@gmail.com>
2010-02-09 21:18:08 +0000

    Updated blast_wrapper.py script

--- scripts/blast_wrapper.py
+++ scripts/blast_wrapper.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # File created on 20 Dec 2009.
 from __future__ import division
-from optparse import OptionParser
-from cogent.parse.fasta import MinimalFastaParser
-from qiime.util import qiime_blast_seqs
 
 __author__ = "Greg Caporaso"
 __copyright__ = "Copyright 2010, The QIIME Project"
@@ -14,57 +11,51 @@ __maintainer__ = "Greg Caporaso"
 __email__ = "gregcaporaso@gmail.com"
 __status__ = "Pre-release"
 
-usage_str = """usage: %prog [options] -i INPUT_FASTA_FP -r REFSEQS_FP
+from qiime.util import parse_command_line_parameters
+from optparse import make_option
+from cogent.parse.fasta import MinimalFastaParser
+from qiime.util import qiime_blast_seqs
 
-This script is a functionality-limited interface to the 
- qiime.util.qiime_blast_seqs function, primarily useful for testing purposes.
- Once that function has been integrated into qiime as the primary blast interface
- it will move to PyCogent. An expanded version of this command line interface may
- replace the script functionality of cogent.app.blast at that point.
+script_description = """This script is a functionality-limited interface to the \
+qiime.util.qiime_blast_seqs function, primarily useful for testing purposes. \
+Once that function has been integrated into qiime as the primary blast interface \
+it will move to PyCogent. An expanded version of this command line interface may \
+replace the script functionality of cogent.app.blast at that point."""
 
-[] indicates optional input (order unimportant)
-{} indicates required input (order unimportant)
+script_usage = """Usage: %prog [options] -i INPUT_FASTA_FP -r REFSEQS_FP
 
 Example usage:
-Blast all sequences in inseqs.fasta (-i) against a BLAST db constructed 
- from refseqs.fasta (-r).
+Blast all sequences in inseqs.fasta (-i) against a BLAST db constructed \
+from refseqs.fasta (-r).
 
- python blast_wrapper.py -i inseqs.fasta -r refseqs.fasta
+python blast_wrapper.py -i inseqs.fasta -r refseqs.fasta
 """
 
-def parse_command_line_parameters():
-    """ Parses command line arguments """
-    usage = usage_str
-    version = 'Version: %prog ' + __version__
-    parser = OptionParser(usage=usage, version=version)
+required_options = [\
+make_option('-i','--input_fasta_fp',\
+    help='paths to sequences to blast as a fasta file'),\
+make_option('-r','--refseqs_fp',\
+    help='path to blast database as a fasta file')
+]
 
-    parser.add_option('-i','--input_fasta_fp',\
-        help='paths to sequences to blast as a fasta file [REQUIRED]')
-    parser.add_option('-r','--refseqs_fp',\
-        help='path to blast database as a fasta file [REQUIRED]')
-    parser.add_option('-n','--num_seqs_per_blast_run',type='int',\
-        help='number of sequences passed to each blast call '+\
-        '- useful for very large sequence collections [default: %default]')
+optional_options = [\
+make_option('-n','--num_seqs_per_blast_run', type='int', default='1000', \
+help = 'number of sequences passed to each blast call '+\
+"- useful for very large sequence collections [default: %default]")
+]
 
-    parser.set_defaults(verbose=False,num_seqs_per_blast_run=1000)
-
-    opts,args = parser.parse_args()
-    required_options = ['input_fasta_fp','refseqs_fp']
-    
-    for option in required_options:
-        if eval('opts.%s' % option) == None:
-            parser.error('Required option --%s omitted.' % option) 
-
-    return opts,args
-
-if __name__ == "__main__":
-    opts,args = parse_command_line_parameters()
-    verbose = opts.verbose
+def main():
+    option_parser, options, args = parse_command_line_parameters(
+      script_description=script_description,
+      script_usage=script_usage,
+      version=__version__,
+      required_options=required_options,
+      optional_options=optional_options)
     
     blast_results = qiime_blast_seqs(\
-     seqs=MinimalFastaParser(open(opts.input_fasta_fp)),\
-     refseqs_fp=opts.refseqs_fp,\
-     seqs_per_blast_run=opts.num_seqs_per_blast_run)
+     seqs=MinimalFastaParser(open(options.input_fasta_fp)),\
+     refseqs_fp=options.refseqs_fp,\
+     seqs_per_blast_run=options.num_seqs_per_blast_run)
      
     for query_id, blast_result in blast_results.items():
         first_blast_result = blast_result[0][0]
@@ -73,4 +64,7 @@ if __name__ == "__main__":
          first_blast_result['SUBJECT ID'],
          first_blast_result['E-VALUE'],
          first_blast_result['% IDENTITY'])
+
+if __name__ == "__main__":
+    main()
     
