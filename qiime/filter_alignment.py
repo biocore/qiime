9789e76aec26d0693fc9983bb4ea3dc0b84d6755
Greg Caporaso <gregcaporaso@gmail.com>
2010-02-16 19:53:56 +0000

    Removed command line interface code (appears to have been accidentally left in following the code sprint) and wrapped lines at 80 characters.

--- qiime/filter_alignment.py
+++ qiime/filter_alignment.py
@@ -3,12 +3,10 @@ from numpy import nonzero, array, fromstring, repeat, bitwise_or, uint8, zeros
 from random import sample
 from cogent.parse.fasta import MinimalFastaParser
 from cogent.core.alignment import eps
-from optparse import OptionParser
 from sys import stdout
 from string import lowercase
 from os.path import split, exists, splitext
 from os import mkdir, remove
-from qiime.util import load_qiime_config
 
 __author__ = "Dan Knights"
 __copyright__ = "Copyright 2010, The QIIME Project"
@@ -22,35 +20,6 @@ __status__ = "Pre-release"
 """Contains code for filtering alignments before building trees from them
 """
 
-usage_string = """usage: %prog [options] {-i INPUT_FASTA_FILE}
-
-Description:
- %prog is intended to be applied as a pre-filter for building trees from 
- alignments. It removes positions which are highly variable based on a 
- lane mask file (a string of 1s and 0s which is the length of the alignment
- and where 1s indicate positions to keep; e.g., lanemask_in_1s_and_0s.txt from
- greengenes), and positions which are 100% gap characters. Uses a minimal
- fasta parser, safe to use on large sequence collections.
-
-Example usage:
-
- # Get detailed usage information
- python filter_alignment.py -h
-
- # filter 1.fasta using the lanemask in lm.txt, but filtering no gaps (b/c
- # --allowed_gap_frac=1.0, meaning positions can be up to 100% gap); output
- # written to ./1_pfiltered.fasta
- python filter_alignment.py -i 1.fasta -g 1.0 -m lm.txt
- 
- # filter 1.fasta using the lanemask in lm.txt and filter positions which are
- # 100% gap (default -g behavior); output written to ./1_pfiltered.fasta
- python filter_alignment.py -i 1.fasta -o ./ -m lm.txt
- 
- # filter 1.fasta positions which are 100% gap (default -g behavior) but no lane mask
- # filtering (because no lane mask file provided with -l); output written to 
- # ./1_pfiltered.fasta
- python filter_alignment.py -i 1.fasta -o ./"""
-
 
 def mask_to_positions(maskstring):
     """Converts lanemask binary string to array of valid indices."""
@@ -61,19 +30,25 @@ def get_masked_string(s, p):
     return (fromstring(s,dtype=uint8))[p].tostring()
 
 def find_gaps(s, gapcode=45):
-    """Returns index array indicating locations of gaps ('-') in string s"""
+    """Returns index array indicating locations of gaps ('-') in string s
+    """
     return nonzero(fromstring(s,dtype=uint8) == gapcode)
 
 def apply_lane_mask(fastalines, lane_mask, verbose=False):
-    """ Applies lanemask to fasta-formatted data, yielding filtered seqs."""
-    return apply_lane_mask_and_gap_filter(fastalines, lane_mask, allowed_gap_frac=1, verbose=False)
+    """ Applies lanemask to fasta-formatted data, yielding filtered seqs.
+    """
+    return apply_lane_mask_and_gap_filter(fastalines, lane_mask,\
+     allowed_gap_frac=1, verbose=False)
 
 def apply_gap_filter(fastalines, allowed_gap_frac=1-eps, verbose=False):
-    """ Applies gap filter to fasta-formatted data, yielding filtered seqs."""
-    return apply_lane_mask_and_gap_filter(fastalines, None, allowed_gap_frac=allowed_gap_frac, verbose=False)
+    """ Applies gap filter to fasta-formatted data, yielding filtered seqs.
+    """
+    return apply_lane_mask_and_gap_filter(fastalines, None, \
+     allowed_gap_frac=allowed_gap_frac, verbose=False)
 
-def apply_lane_mask_and_gap_filter(fastalines, lane_mask, allowed_gap_frac=1-eps, verbose=False):
-    """Applies lanemask and gap filter to fasta file with minimal parser, yielding filtered seqs.
+def apply_lane_mask_and_gap_filter(fastalines, lane_mask,\
+    allowed_gap_frac=1-eps, verbose=False):
+    """Applies lanemask and gap filter to fasta file, yielding filtered seqs.
     """
     
     if lane_mask:
@@ -94,7 +69,8 @@ def apply_lane_mask_and_gap_filter(fastalines, lane_mask, allowed_gap_frac=1-eps
     try:
         tmpfile = open(tmpfilename,'w')
     except IOError:
-        raise IOError, "Can't open temporary file for writing: %s" % tmpfilename
+        raise IOError, "Can't open temporary file for writing: %s" %\
+          tmpfilename
 
     # the number of gaps seen in each position (length may be unknown here)
     gapcounts = None
@@ -168,43 +144,3 @@ def status(message,dest=stdout,overwrite=True, max_len=100):
     if not overwrite:
         dest.write('\n')
     dest.flush()
-
-qiime_config = load_qiime_config()
-
-def parse_command_line_parameters():
-    """ Parses command line arguments """
-    usage = usage_string
-    version = 'Version: %prog 0.1'
-    parser = OptionParser(usage=usage, version=version)
-
-    parser.add_option('-i','--input_fasta_file',action='store',\
-           type='string',help='the input directory '+\
-           '[REQUIRED]')
-    parser.add_option('-o','--output_dir',action='store',\
-           type='string',help='the output directory '+\
-           '[default: %default]',default='.')
-    parser.add_option('-m','--lane_mask_fp',action='store',\
-            type='string',
-            default=qiime_config['template_alignment_lanemask_fp'],
-            help='path to lanemask file [default: %default]')
-    parser.add_option('-s','--suppress_lane_mask_filter',action='store_true',\
-            help='suppress lane mask filtering (necessary to turn off '+\
-             'lane-mask-based filtering when a qiime_config default is  '+\
-             'provided for --lane_mask_fp) [default: %default]',default=False)
-    parser.add_option('-g','--allowed_gap_frac',action='store',\
-            type='float',help='gap filter threshold, ' +\
-            'filters positions which are gaps in > allowed_gap_frac '+\
-            'of the sequences [default: %default]',
-            default=1.-eps)
-    parser.add_option('--verbose',action='store_true',\
-            help='print all information [default: %default]')
-
-    opts,args = parser.parse_args()
-
-    required_options = ['input_fasta_file']
-    
-    for option in required_options:
-        if eval('opts.%s' % option) == None:
-            parser.error('Required option --%s omitted.' % option) 
-
-    return opts,args
