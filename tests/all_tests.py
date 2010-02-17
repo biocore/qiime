4f56dd8980ba02667a8f3be21f0bbc3732617c60
Greg Caporaso <gregcaporaso@gmail.com>
2010-02-17 20:22:13 +0000

    Added more detailed reporting to provide users with information about unit tests which fail due to missing applications, versus unit tests that fail for other reasons. Also added basic testing of scripts.

--- tests/all_tests.py
+++ tests/all_tests.py
@@ -3,7 +3,8 @@
 """
 from os import walk, environ
 from subprocess import Popen, PIPE, STDOUT
-from os.path import join
+from os.path import join, abspath, dirname, split
+from glob import glob
 import re
 
 __author__ = "Rob Knight"
@@ -15,31 +16,77 @@ __maintainer__ = "Rob Knight"
 __email__ = "rob@spot.colorado.edu"
 __status__ = "Pre-release"
 
-good_pattern = re.compile('OK\s*$') 
-start_dir = '.'
+# Note on getting qiime_dir: that this is not the standard way of
+# getting the qiime directory in Qiime. Users should always call
+# qiime.util.get_qiime_project_dir(), but since this script is 
+# designed to test QIIME, including the qiime.util.get_qiime_project_dir
+# function, it is not appropriate to call that function here.
+qiime_dir = abspath(join(dirname(__file__),'..'))
+test_dir = join(qiime_dir,'tests')
+scripts_dir = join(qiime_dir,'scripts')
+
+unittest_good_pattern = re.compile('OK\s*$')
+application_not_found_pattern = re.compile('ApplicationNotFoundError')
 python_name = 'python'
-
 bad_tests = []
-filenames = []
+missing_application_tests = []
 
-for root, dirs, files in walk(start_dir):
+# Run through all of QIIME's unit tests, and keep track of any files which
+# fail unit tests.
+
+unittest_names = []
+
+for root, dirs, files in walk(test_dir):
     for name in files:
         if name.startswith('test_') and name.endswith('.py'):
-            filenames.append(join(root,name))
-filenames.sort()
+            unittest_names.append(join(root,name))
 
-for filename in filenames:
-    print "Testing %s:\n" % filename
-    command = '%s %s -v' % (python_name, filename)
-    #result = popen3()[2].read()
-    #result = popen3('%s %s -v' % (python_name, filename))[2].read()
+unittest_names.sort()
+
+for unittest_name in unittest_names:
+    print "Testing %s:\n" % unittest_name
+    command = '%s %s -v' % (python_name, unittest_name)
     result = Popen(command,shell=True,universal_newlines=True,\
                    stdout=PIPE,stderr=STDOUT).stdout.read()
     print result
-    if not good_pattern.search(result):
-        bad_tests.append(filename)
+    if not unittest_good_pattern.search(result):
+        if application_not_found_pattern.search(result):
+            missing_application_tests.append(unittest_name)
+        else:
+            bad_tests.append(unittest_name)
+
+
+# Run through all of QIIME's scripts, and pass -h to each one. If the
+# resulting stdout does not being with the Usage text, that is an 
+# indicator of something being wrong with the script. Issues that would
+# cause that are bad import statements in the script, SyntaxErrors, or 
+# other failures prior to running qiime.util.parse_command_line_parameters.
+
+script_names = []
+script_names = glob('%s/*py' % scripts_dir)
+script_names.sort()
+bad_scripts = []
+
+for script_name in script_names:
+    script_good_pattern = re.compile('^Usage: %s' % split(script_name)[1])
+    print "Testing %s." % script_name
+    command = '%s %s -h' % (python_name, script_name)
+    result = Popen(command,shell=True,universal_newlines=True,\
+                   stdout=PIPE,stderr=STDOUT).stdout.read()
+    if not script_good_pattern.search(result):
+        bad_scripts.append(script_name)
 
 if bad_tests:
-    print "Failed the following tests:\n%s" % '\n'.join(bad_tests)
-else:
-    print "All tests passed successfully."
+    print "\nFailed the following unit tests.\n%s" % '\n'.join(bad_tests)
+    
+if missing_application_tests:
+    print "\nFailed the following unit tests, in part or whole due "+\
+    "to missing external applications.\nDepending on the QIIME features "+\
+    "you plan to use, this may not be critical.\n%s"\
+     % '\n'.join(missing_application_tests)
+     
+if bad_scripts:
+    print "\nFailed the following script tests.\n%s" % '\n'.join(bad_scripts)
+     
+if not (bad_tests or missing_application_tests or bad_scripts):
+    print "\nAll tests passed successfully."
