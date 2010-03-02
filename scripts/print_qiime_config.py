a3f78966ab6f6ed662b097291b916b299b94cc92
Dan Knights <danknights@gmail.com>
2010-03-02 20:05:07 +0000

    now adheres to new documentation guidelines

--- scripts/print_qiime_config.py
+++ scripts/print_qiime_config.py
@@ -4,7 +4,7 @@ from __future__ import division
 
 __author__ = "Jens Reeder"
 __copyright__ = "Copyright 2010, The QIIME project"
-__credits__ = ["Jens Reeder"]
+__credits__ = ["Jens Reeder","Dan Knights"]
 __license__ = "GPL"
 __version__ = "1.0-dev"
 __maintainer__ = "Jens Reeder"
@@ -14,24 +14,18 @@ __status__ = "Pre-release"
 from qiime.util import load_qiime_config, parse_command_line_parameters
 from optparse import make_option
 
-script_description = \
-"""A simple scripts that prints out the qiime config settings."""
-
-script_usage = """To print qiime config settings:
-print_qiime_config """
-
-required_options = []
-
-optional_options = []
+script_info = {}
+script_info['brief_description']= """Print out the qiime config settings."""
+script_info['script_description'] = """A simple scripts that prints out the qiime config settings."""
+script_info['script_usage']=[]
+script_info['script_usage'].append(("Example 1","""Print qiime config settings:""","""python print_qiime_config.py"""))
+script_info['required_options'] = []
+script_info['optional_options'] = []
+script_info['version'] = __version__
+script_info['help_on_no_arguments'] = False
 
 def main():
-    option_parser, opts, args = parse_command_line_parameters(
-      script_description=script_description,
-      script_usage=script_usage,
-      version=__version__,
-      required_options=required_options,
-      optional_options=optional_options,
-      help_on_no_arguments=False)
+    option_parser, opts, args = parse_command_line_parameters(**script_info)
 
     qiime_config = load_qiime_config()
     for key,value in  qiime_config.items():
