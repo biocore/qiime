294f06a2c769173e54610edf6e32a65d3724fe05
Tony Walters <william.a.walters@gmail.com>
2010-03-02 20:03:52 +0000

    Updated scripts/extracts_seqs_by_sample_id.py to conform to new documentation guildlines.

--- scripts/extract_seqs_by_sample_id.py
+++ scripts/extract_seqs_by_sample_id.py
@@ -12,60 +12,45 @@ __email__ = "gregcaporaso@gmail.com"
 __status__ = "Pre-release"
 
 
-from qiime.util import parse_command_line_parameters
+from qiime.util import parse_command_line_parameters, get_options_lookup
 from optparse import make_option
 from qiime.util import extract_seqs_by_sample_id
 from cogent.parse.fasta import MinimalFastaParser
 
-script_description = """This script creates a fasta file which will contain \
-only sequences that ARE associated with the supplied sampleIDs(-s), OR all sequences \
-that are NOT associated with the supplied sampleIDs(-n)"""
-
-script_usage = """Usage: %prog [options] {-i INPUT_FASTA_FP -o OUTPUT_FASTA_FP -s SAMPLE_IDS}
-
-Example usage:
-
-Create the file outseqs.fasta (-o), which will be a subset of inseqs.fasta \
-(-i) containing only the sequences THAT ARE associated with sample ids S2, S3, \
-S4 (-s). (As always, sample IDs are case-sensitive.)
-
-python extract_seqs_by_sample_id.py -i inseqs.fasta -o outseqs.fasta -s S2,S3,S4
-
-Create the file outseqs.fasta (-o), which will be a subset of inseqs.fasta \
-(-i) containing only the sequences  THAT ARE NOT (-n) associated with sample \
-ids S2, S3, S4 (-s). (As always, sample IDs are case-sensitive.)
-
-python extract_seqs_by_sample_id.py -i inseqs.fasta -o outseqs.fasta -s S2,S3,S4 -n
-
-"""
-
-required_options = [\
-make_option('-i','--input_fasta_fp',\
-help='the input fasta file'),\
+#extract_seqs_by_sample_id.py
+options_lookup = get_options_lookup()
+script_info={}
+script_info['brief_description']="""Extract sequences based on the SampleID"""
+script_info['script_description']="""This script creates a fasta file which will contain only sequences that ARE associated with the supplied sampleIDs(-s), OR all sequences that are NOT associated with the supplied sampleIDs (-n)"""
+script_info['script_usage']=[]
+script_info['script_usage'].append(("""Examples:""","""Create the file outseqs.fasta (-o), which will be a subset of inseqs.fasta (-i) containing only the sequences THAT ARE associated with sample ids S2, S3, \
+S4 (-s). As always, sample IDs are case-sensitive:""","""extract_seqs_by_sample_id.py -i inseqs.fasta -o outseqs.fasta -s S2,S3,S4"""))
+script_info['script_usage'].append(("""""","""Create the file outseqs.fasta (-o), which will be a subset of inseqs.fasta (-i) containing only the sequences  THAT ARE NOT (-n) associated with sample ids S2, S3, S4 (-s). As always, sample IDs are case-sensitive:""","""extract_seqs_by_sample_id.py -i inseqs.fasta -o outseqs.fasta -s S2,S3,S4 -n"""))
+script_info['output_description']="""The script produces a fasta file containing containing only the specified SampleIDs."""
+script_info['required_options']=[\
+options_lookup['fasta_as_primary_input'],\
 make_option('-s','--sample_ids',\
 help="comma-separated sample_ids to include in output fasta file"+\
 "(or exclude if -n=True)"),\
 make_option('-o','--output_fasta_fp',\
-help='the output fasta file')
-]
-
-optional_options = [\
+help='the output fasta file')]
+script_info['optional_options']=[\
 make_option('-n','--negate',action='store_true',default='false',\
 help='negate the sample ID list (i.e., output sample '+\
-'ids not passed via -s) [default: %default]')
-]
+'ids not passed via -s) [default: %default]')]
+script_info['version'] = __version__
+
+
 
 def main():
-    option_parser, opts, args = parse_command_line_parameters(
-      script_description=script_description,
-      script_usage=script_usage,
-      version=__version__,
-      required_options=required_options,
-      optional_options=optional_options)
+    option_parser, opts, args = parse_command_line_parameters(**script_info)
     
     sample_ids = opts.sample_ids.split(',')
     negate = opts.negate
     
+    print opts
+    
+   
     try:
         seqs = MinimalFastaParser(open(opts.input_fasta_fp))
     except IOError:
