6d15f2ea98b1cf11c21fb2f70c9dac29c150cf79
Jesse Stombaugh <jistombaugh@gmail.com>
2010-03-02 19:14:54 +0000

    Updated usage descriptions and command-line options.

--- scripts/blast_wrapper.py
+++ scripts/blast_wrapper.py
@@ -4,53 +4,43 @@ from __future__ import division
 
 __author__ = "Greg Caporaso"
 __copyright__ = "Copyright 2010, The QIIME Project"
-__credits__ = ["Greg Caporaso"]
+__credits__ = ["Greg Caporaso", "Jesse Stombaugh"]
 __license__ = "GPL"
 __version__ = "1.0-dev"
 __maintainer__ = "Greg Caporaso"
 __email__ = "gregcaporaso@gmail.com"
 __status__ = "Pre-release"
 
-from qiime.util import parse_command_line_parameters
+from qiime.util import parse_command_line_parameters, get_options_lookup
 from optparse import make_option
 from cogent.parse.fasta import MinimalFastaParser
 from qiime.util import qiime_blast_seqs
 
-script_description = """This script is a functionality-limited interface to the \
-qiime.util.qiime_blast_seqs function, primarily useful for testing purposes. \
-Once that function has been integrated into qiime as the primary blast interface \
-it will move to PyCogent. An expanded version of this command line interface may \
-replace the script functionality of cogent.app.blast at that point."""
+options_lookup = get_options_lookup()
 
-script_usage = """Usage: %prog [options] {-i INPUT_FASTA_FP -r REFSEQS_FP}
-
-Example usage:
-Blast all sequences in inseqs.fasta (-i) against a BLAST db constructed \
-from refseqs.fasta (-r).
-
-python blast_wrapper.py -i inseqs.fasta -r refseqs.fasta
-"""
-
-required_options = [\
-make_option('-i','--input_fasta_fp',\
-    help='paths to sequences to blast as a fasta file'),\
+#blast_wrapper.py
+script_info={}
+script_info['brief_description']="""Blast Interface"""
+script_info['script_description']="""This script is a functionally-limited interface to the qiime.util.qiime_blast_seqs function, primarily useful for testing purposes. Once that function has been integrated into qiime as the primary blast interface it will move to PyCogent. An expanded version of this command line interface may replace the script functionality of cogent.app.blast at that point."""
+script_info['script_usage']=[]
+script_info['script_usage'].append(("""Example:""","""Blast all sequences in inseqs.fasta (-i) against a BLAST db constructed \
+from refseqs.fasta (-r).""","""%prog -i inseqs.fasta -r refseqs.fasta"""))
+script_info['output_description']="""This is a utility program, which returns BLAST results."""
+script_info['required_options']=[\
+options_lookup['fasta_as_primary_input'],
 make_option('-r','--refseqs_fp',\
     help='path to blast database as a fasta file')
 ]
-
-optional_options = [\
+script_info['optional_options']=[\
 make_option('-n','--num_seqs_per_blast_run', type='int', default='1000', \
 help = 'number of sequences passed to each blast call '+\
 "- useful for very large sequence collections [default: %default]")
 ]
 
+script_info['version'] = __version__
+
 def main():
-    option_parser, options, args = parse_command_line_parameters(
-      script_description=script_description,
-      script_usage=script_usage,
-      version=__version__,
-      required_options=required_options,
-      optional_options=optional_options)
+    option_parser, options, args = parse_command_line_parameters(**script_info)
     
     blast_results = qiime_blast_seqs(\
      seqs=MinimalFastaParser(open(options.input_fasta_fp)),\
