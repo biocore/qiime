a5e75265e8d4ad6a5a78dc51f475c87c129591fb
Daniel McDonald <wasade@gmail.com>
2010-03-02 17:30:20 +0000

    Code sprint 3/2/10, updated otu_table_filtered.txt

--- scripts/filter_otu_table.py
+++ scripts/filter_otu_table.py
@@ -12,35 +12,26 @@ __email__ = "rob@spot.colorado.edu"
 __status__ = "Pre-release"
  
 
-from qiime.util import parse_command_line_parameters
+from qiime.util import parse_command_line_parameters, get_options_lookup
 from qiime.filter_otu_table import _filter_table, split_tax
 from optparse import make_option
 from string import strip
 
-script_description = """Filters OTU table according to minimum OTU count and number of samples.
-If OTU table has taxonomy assigned, can also use taxonomy to filter."""
+options_lookup = get_options_lookup()
 
-script_usage = """Creates a new filtered OTU with the original filename + "_filtered.txt".
-To retain only OTUs with at least this many sequences:
-filter_otu_table -i otu_table.txt -c MIN_COUNT 
+#filter_otu_table.py
+script_info={}
+script_info['brief_description']="""Filters OTU table by minimum OTU count and number of samples or by taxonomy"""
+script_info['script_description']="""After the OTU has been generated, the user may want to filter the table based on the number of samples within each OTU or by the number of sequences per OTU. This step is generally done to reduce the noise within the OTU table and can also reduce the overall size of the table, which is essential when performing analyses on large datasets. Along with filtering based on samples or sequences, the user can include and exclude specific taxon groups."""
+script_info['script_usage']=[]
+script_info['script_usage'].append(("""Examples:""","""To filter the OTU table using the default parameters ("-c 1" and "-s 2"), then write the results to the current working directory, you can use the code as follows:""","""filter_otu_table.py -i otu_table.txt"""))
+script_info['script_usage'].append(("""""","""To filter by the number of samples (i.e., 5) within each OTU (keep only OTU's found in at least X number of samples), you can use the code as follows:""","""filter_otu_table.py -i otu_table.txt -s 5"""))
+script_info['script_usage'].append(("""""","""To filter by the number of sequences (i.e., 5) within each OTU (keep only OTU's with at least X sequences in the OTU), you can use the code as follows:""","""filter_otu_table.py -i otu_table.txt -c 5"""))
+script_info['script_usage'].append(("""""","""To include ("Bacteria") and exclude ("Proteobacteria") certain taxon groups (options -t / -e respectively), you can use the code as follows.  The include and exclude parameters must be used together:""","""filter_otu_table.py -i otu_table.txt -t Bacteria -e Proteobacteria"""))
+script_info['output_description']="""The result of filter_otu_table.py creates a new OTU table, where the filename uses the input OTU filename and appends "filtered.txt" to the end of the name."""
+script_info['required_options'] = [options_lookup['otu_table_as_primary_input']]
 
-To retain only OTUs found in at least this many samples:
-filter_otu_table -i otu_table.txt -s MIN_SAMPLES
-
-To retain only OTUs containing particular taxonomic terms in their lineage:
-filter_otu_table -i otu_table.txt -t "Actinobacteria,Bacteroidetes"
-
-To exclude OTUs containing particular taxonomic terms in their lineage, 
-but include all other Bacteria:
-filter_otu_table -i otu_table.txt -e "Actinobacteria,Bacteroidetes" -t "Bacteria"
-"""
-
-required_options = [\
-    make_option('-i', '--otu_filename', dest='otu_fname',
-                help='otu file name')\
-]
-
-optional_options = [\
+script_info['optional_options']=[\
     make_option('-c', '--min_count', default=1, type=int,
         help='retain OTUs with at least this many sequences [default=%default]'),\
     make_option('-s', '--min_samples', default=2, type=int,
@@ -52,25 +43,20 @@ optional_options = [\
     make_option('-o', '--dir_path', default='./',
         help='directory prefix for all analyses [default=%default]')
 ]
-
+script_info['version'] = __version__
 
 
 
 def main():
-    option_parser, opts, args = parse_command_line_parameters(
-      script_description=script_description,
-      script_usage=script_usage,
-      version=__version__,
-      required_options=required_options,
-      optional_options=optional_options)
+    option_parser, opts, args = parse_command_line_parameters(**script_info)
 
     # process options (was originally process_options())
-    filepath=opts.otu_fname
+    filepath=opts.otu_table_fp
     filename=filepath.strip().split('/')[-1]
     filename=filename.split('.')[0]
 
     params={}
-    params['otu_file'] = opts.otu_fname
+    params['otu_file'] = opts.otu_table_fp
     params['min_otu_count'] = opts.min_count
     params['min_otu_samples'] = opts.min_samples
 
