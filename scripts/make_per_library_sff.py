744a373ea00bfa663c8d7f5cf66dfa988b94dea3
Daniel McDonald <wasade@gmail.com>
2010-03-02 18:14:04 +0000

    Code sprint 3/2/10, updated make_per_library_sff.py

--- scripts/make_per_library_sff.py
+++ scripts/make_per_library_sff.py
@@ -4,7 +4,7 @@ from __future__ import division
 
 __author__ = "Rob Knight"
 __copyright__ = "Copyright 2010, The QIIME project"
-__credits__ = ["Rob Knight"]
+__credits__ = ["Rob Knight", "Daniel McDonald"]
 __license__ = "GPL"
 __version__ = "1.0-dev"
 __maintainer__ = "Rob Knight"
@@ -17,23 +17,22 @@ from optparse import make_option
 from os import walk, system
 from os.path import splitext, join
 
-script_description = """Makes per-library sff files from id lists."""
+#make_per_library_sff.py
+script_info={}
+script_info['brief_description']="""Make per-library sff files from id lists"""
+script_info['script_description']="""This script generates per-library sff files using the id lists."""
+script_info['script_usage']=[]
+script_info['script_usage'].append(("""Example:""","""Make per-library sff files using input.sff and a directory of libs where each file in the directory contains the id lists for each library:""","""make_per_library_sff.py -i input.sff -l libs"""))
+script_info['output_description']="""The result of this script generates sff files for each library."""
 
-script_usage = """python make_per_library_sff.py {-i input_sff -l lib_dir} [options]
-
-Example: Make per-library sff files using input.sff and a directory of libs where each file in the directory contains the id lists for each library.
-
-python make_per_library_sff.py -i input.sff -l libs
-"""
-
-required_options = [\
+script_info['required_options']=[\
     make_option("-i","--input_sff",dest='in_sff',\
         help="The path to an input sff file (or files: separate w/ comma, no spaces)"),
     make_option("-l","--libdir",dest='libdir',\
         help=""" The directory containing per-library id files""")
 ]
 
-optional_options = [\
+script_info['optional_options']=[\
     make_option("-p","--sfffile_path",dest='sfffile_path',\
         help=""" Path to sfffile binary [default: %default]""", 
         default='sfffile'),
@@ -41,16 +40,12 @@ optional_options = [\
         action='store_true',
         help="Print command-line for debugging [default: %default]")
 ]
+script_info['version'] = __version__
 
 cmd = "%s -i %s -o %s %s"
 
 def main():
-    option_parser, opts, args = parse_command_line_parameters(
-      script_description=script_description,
-      script_usage=script_usage,
-      version=__version__,
-      required_options=required_options,
-      optional_options=optional_options)
+    option_parser, opts, args = parse_command_line_parameters(**script_info)
 
     if opts.debug:
         print "Making debug output"
