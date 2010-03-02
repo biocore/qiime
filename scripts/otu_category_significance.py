6d1f8f85f495d59bccfd98f39138eabbf78255a0
Dan Knights <danknights@gmail.com>
2010-03-02 18:27:41 +0000

    now adheres to new documentation guidelines

--- scripts/otu_category_significance.py
+++ scripts/otu_category_significance.py
@@ -16,23 +16,43 @@ from qiime.otu_category_significance import test_wrapper
 from qiime.util import parse_command_line_parameters
 from optparse import make_option
 
-script_description = """Look for OTUs that are associated with a category."""
+script_info={}
+script_info['brief_description']="""OTU significance and co-occurence analysis"""
+script_info['script_description']="""The script otu_category_significance.py tests whether any of the OTUs in an OTU table are significantly associated with a category in the category mapping file. This code uses, ANOVA, the G test of independence, or Pearson correlation to find OTUs whose members are differentially represented across experimental treatments or measured variables. It can also be used with presence/absence or abundance data for a phylogenetic group (such as that determined with quantitative PCR) to determine if any OTUs co-occur with a taxon of interest."""
+script_info['script_usage']=[]
+script_info['script_usage'].append(("Example 1","""If the user would like to perform a G test on their OTU table using default parameters, while testing the category "Sex", they can run the following command:""","""otu_category_significance.py -i otu_table.txt -m Mapping_file.txt -s g_test -c Sex"""))
+script_info['script_usage'].append(("Example 2","""If the user would like to perform the same test using numerical qPCR data, where everything below a threshold value should be considered "absent" and everything above that value "present", the user will need to set the threshold by running the following command:""","""otu_category_significance.py -i otu_table.txt -m Mapping_file.txt -s g_test -c qPCR -t 0.16"""))
+script_info['script_usage'].append(("Example 3","""Alternatively, the user could run an ANOVA test on the same data by using the following command:""","""otu_category_significance.py -i otu_table.txt -m Mapping_file.txt -s ANOVA -c Sex"""))
+script_info['output_description']="""The G test results are output as tab delimited text, which can be examined in Excel. The output has the following columns:
 
-script_usage = """
-Look for OTUs that are associated with a category. Currently can do:
-1) perform g-test of independence to determine whether OTU presence
-absence is associated with a category in the mapping file. Can
-be used to look for co-occurrence using qPCR data or to look for 
-associations with a categorical environmental variable.
-2) perform ANOVA to determine whether OTU abundance is significantly
-different across a category. Again can be used with a category in the 
-mapping file that has categorical data.
-3) perform a Pearson correlation to determine whether OTU abundance is correlated with a category in the category mapping file. Can be used with a category that contains continuous data. 
+* OTU: The name of the OTU.
+* g_val: The raw test statistic.
+* g_prob: The probability that this OTU is non-randomly distributed across the categories.
+* Bonferroni_corrected: The probability after correction for multiple comparisons with the Bonferroni correction. In this correction, the p-value is multiplied by the number of comparisons performed (the number of OTUs remaining after applying the filter).
+* FDR_corrected: The probability after correction with the "false discovery rate" method. In this method, the raw p-values are ranked from low to high. Each p-value is multiplied by the number of comparisons divided by the rank. This correction is less conservative than the Bonferroni correction. The list of significant OTUs is expected to have the percent of false positives predicted by the p value.
+* Contingency table columns: The next columns give the information in the contingency table and will vary in number and name based on the number of categories and their names. The two numbers in brackets represent the number of samples that were observed in those categories and the number that would be expected if the OTU members were randomly distributed across samples in the different categories. These columns can be used to evaluate the nature of a non-random association (e.g. if that OTU is always present in a particular category or if it is never present).
+* Consensus lineage: The consensus lineage for that OTU will be listed in the last column if it was present in the input OTU table.
 
-python ~/repo/Qiime/qiime/otu_category_significance.py -i otu_table.txt, -m category_mapping.txt -s g_test -f 10 -c category name -o output_fp -t None
+The ANOVA results are output as tab delimited text that can be examined in Excel. The output has the following columns:
+
+* OTU: The name of the OTU.
+* prob: The raw probability from the ANOVA 
+* Bonferroni_corrected: The probability after correction for multiple comparisons with the Bonferroni correction. In this correction, the p-value is multiplied by the number of comparisons performed (the number of OTUs remaining after applying the filter). 
+* FDR_corrected: The probability after correction with the "false discovery rate" method. In this method, the raw p-values are ranked from low to high. Each p-value is multiplied by the number of comparisons divided by the rank. This correction is less conservative than the Bonferroni correction. The list of significant OTUs is expected to have the percent of false positives predicted by the p value.
+* Category Mean Columns: Contains one column for each category reporting the mean count of the OTU in that category.
+* Consensus lineage: The consensus lineage for that OTU will be listed in the last column if it was present in the input OTU table.
+
+The correlation test results are output as tab delimited text, which can be examined in Excel. The output has the following columns:
+
+* OTU: The name of the OTU.  
+* prob: The probability that the OTU relative abundance is correlated with the category values across samples. 
+* Bonferroni_corrected: The probability after correction for multiple comparisons with the Bonferroni correction. In this correction, the p-value is multiplied by the number of comparisons performed (the number of OTUs remaining after applying the filter). 
+* FDR_corrected: The probability after correction with the "false discovery rate" method. In this method, the raw p-values are ranked from low to high. Each p-value is multiplied by the number of comparisons divided by the rank. This correction is less conservative than the Bonferroni correction. The list of significant OTUs is expected to have the percent of false positives predicted by the p value.
+* r: Pearson's r. This value ranges from -1 to +1, with -1 indicating a perfect negative correlation, +1 indicating a perfect positive correlation, and 0 indicating no relationship.
+* Consensus lineage: The consensus lineage for that OTU will be listed in the last column if it was present in the input OTU table.
 """
 
-required_options = [\
+script_info['required_options']=[\
     make_option('-i','--otu_table_fp', dest='otu_table_fp',\
         help='path to the otu table'),\
     make_option('-m','--category_mapping_fp',\
@@ -42,7 +62,7 @@ required_options = [\
         help='name of category over which to run the analysis')
 ]
 
-optional_options = [\
+script_info['optional_options']=[\
     make_option('-s','--test', dest='test', default='g_test',\
         help='the type of statistical test to run. options are: ' +\
         'g_test: determines whether OTU presence/absence is associated ' +\
@@ -64,14 +84,10 @@ optional_options = [\
         'present or absent based on a threshold. Should be None for ' +\
         'categorical data or with the correlation test. default value is None')
 ]
+script_info['version'] = __version__
 
 def main():
-    option_parser, opts, args = parse_command_line_parameters(
-        script_description=script_description,
-        script_usage=script_usage,
-        version=__version__,
-        required_options=required_options,
-        optional_options=optional_options)
+    option_parser, opts, args = parse_command_line_parameters(**script_info)
 
     verbose = opts.verbose
 
