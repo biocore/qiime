694f6b60c2cba539609b6b62d95e12964fa65eb9
Jesse Stombaugh <jistombaugh@gmail.com>
2010-03-02 18:50:35 +0000

    Updated documentation and command-line options.

--- scripts/make_otu_heatmap_html.py
+++ scripts/make_otu_heatmap_html.py
@@ -14,7 +14,7 @@ __email__ = "jesse.stombaugh@colorado.edu"
 __status__ = "Pre-release"
 
 
-from qiime.util import parse_command_line_parameters
+from qiime.util import parse_command_line_parameters, get_options_lookup
 from optparse import make_option
 from qiime.make_otu_heatmap_html import generate_heatmap_plots,get_otu_counts
 from qiime.make_3d_plots import create_dir
@@ -22,61 +22,45 @@ import shutil
 import os
 from qiime.util import get_qiime_project_dir
 
+options_lookup = get_options_lookup()
 
-script_description = """This script takes an otu table and generates an OTU \
-count Heatmap. This Heatmap is a dynamic web application, which allows the user \
-to filter the table by counts, transpose the table and several mouseover \
-features."""
-
-script_usage = """
-Example 1: Create html file and javascript array from otu counts table:
-
-Usage: make_otu_heatmap_html.py -i otu_counts.txt
-
-Example 2: Create html file, then javascript array in specified directory:
-
-Usage: make_otu_heatmap_html.py -i otu_counts.txt -o ./test/
-
-Example 3: Create html file, then javascript array where the number of hits
-per otu are specified:
-
-Usage: make_otu_heatmap_html.py -i otu_counts.txt -n 50"""
-
-required_options = [\
- # Example required option
- #make_option('-i','--input_dir',help='the input directory'),\
- make_option('-i', '--otu_count_fname', help='This is the path to the OTU table \
-(i.e., the resulting OTU table from make_otu_table.py or filter_otu_table.py)')
+#make_otu_heatmap_html.py
+script_info={}
+script_info['brief_description']="""Make heatmap of OTU table"""
+script_info['script_description']="""Once the OTU table has been generated, the user can create an interactive OTU heatmap. This script parses the OTU count table and filters the table by counts per otu (user-specified), then converts the table into a javascript array, which can be loaded into a web application. The OTU heatmap displays raw OTU counts per sample, where the counts are colored based on the contribution of each OTU to the total OTU count present in that sample (blue: contributes low percentage of OTUs to sample; red: contributes high percentage of OTUs). This web application allows the user to filter the otu table by number of counts per otu. The user also has the ability to view the table based on taxonomy assignment. Additional features include: the ability to drag rows (up and down) by clicking and dragging on the row headers; and the ability to zoom in on parts of the heatmap by clicking on the counts within the heatmap."""
+script_info['script_usage']=[]
+script_info['script_usage'].append(("""Examples:""","""By using the default values ("-n 5), you can then use the code as follows:""","""%prog -i otu_table.txt"""))
+script_info['script_usage'].append(("""""","""If you would like to filter the OTU table by a different number of counts per OTU (i.e., 10), you can use the following code:""","""%prog -i otu_table.txt -n 10"""))
+script_info['script_usage'].append(("""""","""If you would like to specify a different output directory (i.e., "otu_heatmap"), you can use the following code:""","""%prog -i otu_table.txt -o otu_heatmap"""))
+script_info['output_description']="""The interactive heatmap is located in a randomly generated folder where the name of the folder starts with "otu_table". The resulting folder contains the interactive heatmap (html file) along with a javascript library folder. This web application has been tested in Mozilla Firefox and Safari. Safari is recommended for viewing the OTU Heatmap, since the HTML table generation is much faster."""
+script_info['required_options']=[\
+ options_lookup['otu_table_as_primary_input']
 ]
-
-optional_options = [\
- # Example optional option
- #make_option('-o','--output_dir',help='the output directory [default: %default]'),\
+script_info['optional_options']=[\
+options_lookup['output_dir'],
  make_option('-n', '--num_otu_hits', help='This is the minimum number of \
 Samples that an OTU is present in, for an OTU to be kept in the OTU table \
-[default: %default]',default=5),
- make_option('-o', '--dir_path',help='This is the location where the resulting \
-output should be written [default: %default]',default='')
+[default: %default]',default=5)
 ]
+
+script_info['version'] = __version__
+
+
+
 
 
 def main():
-    option_parser, opts, args = parse_command_line_parameters(
-      script_description=script_description,
-      script_usage=script_usage,
-      version=__version__,
-      required_options=required_options,
-      optional_options=optional_options)
+    option_parser, opts, args = parse_command_line_parameters(**script_info)
       
     data = {}
 
     #Open and get coord data
-    data['otu_counts'] = get_otu_counts(opts, data)
+    data['otu_counts'] = get_otu_counts(opts.otu_table_fp, data)
 
-    filepath=opts.otu_count_fname
+    filepath=opts.otu_table_fp
     filename=filepath.strip().split('/')[-1].split('.')[0]
 
-    dir_path = create_dir(opts.dir_path,'otu_heatmap_')
+    dir_path = create_dir(opts.output_dir,'otu_heatmap_')
 
     if dir_path and not dir_path.endswith('/'):
         dir_path=dir_path+'/'
