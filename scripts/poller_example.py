1848fc54aa902ba633c82c344cc9f8cf23dfa18e
Jesse Stombaugh <jistombaugh@gmail.com>
2010-03-02 20:40:27 +0000

    Updated usage options and description.

--- scripts/poller_example.py
+++ scripts/poller_example.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # File created on 08 Nov 2009.
 from __future__ import division
+from optparse import make_option
+from qiime.util import parse_command_line_parameters, get_options_lookup
 from os import system, makedirs
 from qiime.parallel.poller import remove_all
 from qiime.util import load_qiime_config
@@ -9,7 +11,7 @@ from optparse import OptionParser
 
 __author__ = "Greg Caporaso"
 __copyright__ = "Copyright 2010, The QIIME Project"
-__credits__ = ["Greg Caporaso"]
+__credits__ = ["Greg Caporaso","Jesse Stombaugh"]
 __license__ = "GPL"
 __version__ = "1.0-dev"
 __maintainer__ = "Greg Caporaso"
@@ -17,82 +19,42 @@ __email__ = "gregcaporaso@gmail.com"
 __status__ = "Pre-release"
 
 qiime_config = load_qiime_config()
+options_lookup = get_options_lookup()
 
-usage_str = """usage: %prog [options] {-d POLLED_DIR}
-
-[] indicates optional input (order unimportant)
-{} indicates required input (order unimportant)
-
-poller.py is designed for use with parallel jobs to wait for their
- completion, and subsequently process the results and clean up. This 
- script allows users to see it in action, and also to allow manual testing
- as this is a difficult process to unit test.
+script_info={}
+script_info['brief_description']="""Create python file""" 
+script_info['script_description']="""This script is designed for use with parallel jobs to wait for their completion, and subsequently process the results and clean up. This script allows users to see it in action, and also to allow manual testing as this is a difficult process to unit test.
  
-To test, call the example command below. The poller will begin running,
- at which time you can create the three polled files in POLLED_DIR. When
- all three are created, the poller will process the results, clean up, and exit.
+To test, call the example command below. The poller will begin running, at which time you can create the three polled files in POLLED_DIR. When all three are created, the poller will process the results, clean up, and exit.""" 
+script_info['script_usage']=[] 
+script_info['script_usage'].append(("""Example usage:""","""""","""%prog -d /Users/caporaso/poller_test/""")) 
+script_info['script_usage'].append(("""""","""The actual call to the polling command is printed for reference just prior to calling it. This illustrates how to pass both functions and filepaths to the poller. For an example where the default (non-verbose) check_run_complete_f, process_run_results_f, and clean_up_f are used, pass -c. Again, the polling command will be printed just prior to calling:""","""%prog -d /Users/caporaso/poller_test/ -c"""))
+script_info['output_description']="""The poller waits for three files to be created:
 
-Example usage:
- The poller waits for three files to be created:
-  <POLLED_DIR>/poller_test_0.txt
-  <POLLED_DIR>/poller_test_1.txt
-  <POLLED_DIR>/poller_test_2.txt
-  
-  <POLLED_DIR> is defined via -d.
-  
- Existence of these three files is checked every 5 seconds with
- verbose_check_run_complete_f. When all three exist verbose_process_run_results_f 
- is called, which cats all the files into a single file:
-  <POLLED_DIR>/poller_test_completed.txt
- Finally, verbose_clean_up_f is called which removes the original three 
- files the poller was waiting on.
-  
-  python qiime/scripts/poller_example.py -d /Users/caporaso/poller_test/
-  
- The actual call to the polling command is printed for reference just prior 
-  to calling it. This illustrates how to pass both functions and filepaths to
-  the poller. For an example where the default (non-verbose) check_run_complete_f,
-  process_run_results_f, and clean_up_f are used, pass -c. Again, the polling
-  command will be printed just prior to calling:
-  
-  python qiime/scripts/poller_example.py -d /Users/caporaso/poller_test/ -c
-"""
+ - <POLLED_DIR>/poller_test_0.txt
+ - <POLLED_DIR>/poller_test_1.txt
+ - <POLLED_DIR>/poller_test_2.txt
+ - <POLLED_DIR> is defined via -d.
 
-def parse_command_line_parameters():
-    """ Parses command line arguments """
-    usage = usage_str
-    version = 'Version: %prog ' + __version__
-    parser = OptionParser(usage=usage, version=version)
-    
-    parser.add_option('-d','--polled_dir',\
-           type='string',help='path to directory to poll '+\
-           ' [REQUIRED]')
-    
-    parser.add_option('-c','--suppress_custom_functions',\
+Existence of these three files is checked every 5 seconds with verbose_check_run_complete_f. When all three exist verbose_process_run_results_f 
+is called, which cats all the files into a single file: <POLLED_DIR>/poller_test_completed.txt. Finally, verbose_clean_up_f is called which removes the original three files the poller was waiting on."""
+
+script_info['required_options']=[\
+ make_option('-d','--polled_dir',\
+       type='string',help='path to directory to poll')
+] 
+
+script_info['optional_options']=[\
+ options_lookup['poller_fp'],
+ options_lookup['python_exe_fp'],
+ make_option('-c','--suppress_custom_functions',\
            action='store_true',help='use the default functions for '+\
            'checking run completion, processing results, and '+\
-           'cleaning up (these are quiet) [defualt: %default]')
-    
-    parser.add_option('-P','--poller_fp',action='store',\
-           type='string',help='full path to '+\
-           'Qiime/scripts/poller.py [default: %default]')
+           'cleaning up (these are quiet) [default: %default]',default=False)
+]
 
-    parser.add_option('-Y','--python_exe_fp',
-           help='full path to python executable [default: %default]',\
-           default=qiime_config['python_exe_fp'])
+script_info['version'] = __version__
 
-    # Set default values here if they should be other than None
-    parser.set_defaults(poller_fp=qiime_config['poller_fp'],\
-     suppress_custom_functions=False)
-
-    opts,args = parser.parse_args()
-    required_options = ['polled_dir']
-    
-    for option in required_options:
-        if eval('opts.%s' % option) == None:
-            parser.error('Required option --%s omitted.' % option) 
-
-    return opts,args
 
 def write_poller_files(polled_dir):
     """ write files to support the poller
@@ -132,7 +94,7 @@ def write_poller_files(polled_dir):
     return check_run_complete_fp, process_run_results_fp, clean_up_fp
 
 if __name__ == "__main__":
-    opts,args = parse_command_line_parameters()
+    option_parser, opts, args = parse_command_line_parameters(**script_info)
     poller_fp = opts.poller_fp
     python_exe_fp = opts.python_exe_fp
     polled_dir = opts.polled_dir
