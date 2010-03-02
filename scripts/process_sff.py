8047f555be420e1a89687590bef177ba25595271
Kyle Bittinger <kylebittinger@gmail.com>
2010-03-02 18:55:32 +0000

    Updated docs for process_sff.py script.

--- scripts/process_sff.py
+++ scripts/process_sff.py
@@ -4,7 +4,7 @@ from __future__ import division
 
 __author__ = "Rob Knight"
 __copyright__ = "Copyright 2010, The QIIME project"
-__credits__ = ["Rob Knight"]
+__credits__ = ["Rob Knight", "Kyle Bittinger"]
 __license__ = "GPL"
 __version__ = "1.0-dev"
 __maintainer__ = "Rob Knight"
@@ -16,33 +16,23 @@ from qiime.util import parse_command_line_parameters
 from optparse import make_option
 from qiime.process_sff import prep_sffs_in_dir
 
-script_description = """Converts directory of sff files into fasta and qual files.
+script_info={}
+script_info['brief_description']="""Convert sff to FASTA and QUAL files"""
+script_info['script_description']="""This script converts a directory of sff files into FASTA and QUAL files.
 
-Requires that 454's off-instrument apps (sffinfo, sfffile) are on your path.
-"""
-
-script_usage = """Convert all the sffs in directory sffs to fasta and qual:
-
-process_sff -i sffs
-"""
-
-required_options = [\
- make_option('-i','--input_dir',help='the input directory of sffs')
-]
-
-optional_options = [\
-]
-
-
+This script requires that 454's off-instrument apps (sffinfo, sfffile) are in your path."""
+script_info['script_usage']=[]
+script_info['script_usage'].append(("""Simple example""","""Convert all the sffs in directory \"sffs/\" to fasta and qual.""","""process_sff.py -i sffs/"""))
+script_info['output_description']="""This script results in FASTA and QUAL formatted files."""
+script_info['required_options'] = [\
+    make_option('-i', '--input_dir', help='Input directory of sff files'),
+    ]
+script_info['optional_options']=[]
+script_info['version'] = __version__
 
 
 def main():
-    option_parser, opts, args = parse_command_line_parameters(
-      script_description=script_description,
-      script_usage=script_usage,
-      version=__version__,
-      required_options=required_options,
-      optional_options=optional_options)
+    option_parser, opts, args = parse_command_line_parameters(**script_info)
 
     prep_sffs_in_dir(opts.input_dir)
 
