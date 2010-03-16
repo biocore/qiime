7787d078673b798ef5dd0a55e4448798b32c8bb1
Greg Caporaso <gregcaporaso@gmail.com>
2010-03-16 22:46:36 +0000

    Updated merge_mapping_files to call qiime.parse.new_parse_map.

--- qiime/merge_mapping_files.py
+++ qiime/merge_mapping_files.py
@@ -12,7 +12,7 @@ __email__ = "gregcaporaso@gmail.com"
 __status__ = "Pre-release"
 
 from optparse import OptionParser
-from qiime.parse import parse_map
+from qiime.parse import new_parse_map, parse_map
     
 def merge_mapping_files(mapping_files,no_data_value='no_data'):
     """ Merge list of mapping files into a single mapping file 
@@ -27,29 +27,24 @@ def merge_mapping_files(mapping_files,no_data_value='no_data'):
     
     # iterate over mapping files, parsing each
     for mapping_file in mapping_files:
-        # the parse map doesn't function as it says it should, but 
-        # when that gets fixed, the following two lines should be 
-        # replaced with the comments lines just below them
-        data = parse_map(mapping_file,return_header=True,strip_quotes=False)
-        current_headers = data[0][0]
-        #data, current_headers = \
-        # parse_map(mapping_file,return_header=True,strip_quotes=False)
+        data, current_headers, current_comments = \
+           new_parse_map(mapping_file,strip_quotes=False)
         all_headers.update(dict.fromkeys(current_headers))
-        for d in data[0][1:]:
+        for d in data:
             current_values = {}
             for i,v in enumerate(d):
                 current_values[current_headers[i]] = v
             mapping_lines.append(current_values)
     
     # remove and place the fields whose order is important
-    del all_headers['#SampleID']
+    del all_headers['SampleID']
     del all_headers['BarcodeSequence']
     del all_headers['Description']
-    all_headers = ['#SampleID','BarcodeSequence'] \
+    all_headers = ['SampleID','BarcodeSequence'] \
      + list(all_headers) + ['Description']
     
     # generate the mapping file lines containing all fields
-    result.append('\t'.join(all_headers))
+    result.append('#' + '\t'.join(all_headers))
     for mapping_line in mapping_lines:
         result.append('\t'.join(\
          [mapping_line.get(h,no_data_value) for h in all_headers]))
