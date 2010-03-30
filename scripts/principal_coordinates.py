f93c68edb823c586f6f021b49780cfc912c831c0
Antonio Gonzalez <antgonza@gmail.com>
2010-03-30 23:21:37 +0000

    Updated an error in help

--- scripts/principal_coordinates.py
+++ scripts/principal_coordinates.py
@@ -25,7 +25,8 @@ script_info['script_usage']=[]
 script_info['script_usage'].append(("""Example""","""For this script, the user supplies a distance matrix (i.e., resulting file from beta_diversity.py), along with the output filename (e.g. beta_div_coords.txt), as follows:""","""principal_coordinates.py -i beta_div.txt -o beta_div_coords.txt"""))
 script_info['output_description']="""The resulting output file consists of each component (columns) along with the loading for each sample (rows). Pairs of components can then be graphed to view the relationships between samples. The bottom of the output file contains the eigenvalues and % variation explained for each component."""
 script_info['required_options']=[\
-   options_lookup['otu_table_as_primary_input'],\
+   make_option('-i', '--input_distance_matrix_fp',\
+     help='path to the input distance matrix file (i.e., the output from beta_diversity.py)'),\
    options_lookup['output_fp']\
 ]
 script_info['optional_options']=[]
@@ -34,7 +35,7 @@ script_info['version'] = __version__
 def main():
     option_parser, opts, args = parse_command_line_parameters(**script_info)
       
-    infilepath = opts.otu_table_fp
+    infilepath = opts.input_fistance_matrix_fp
     f = open(infilepath,'U')
     pcoa_res_string = pcoa(f)
     f.close()
