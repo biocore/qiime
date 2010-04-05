c15d6dac916a7e9347fccc6131855940e5338aac
Jens Reeder <jens.reeder@gmail.com>
2010-04-05 18:38:40 +0000

    add example for using denoiser in wirkflow script

--- scripts/pick_otus_through_otu_table.py
+++ scripts/pick_otus_through_otu_table.py
@@ -38,7 +38,7 @@ script_info['script_usage'] = []
 
 script_info['script_usage'].append(("""Simple example""","""The following command will start an analysis on inseq1.fasta (-i), which is a post-split_libraries fasta file. The sequence identifiers in this file should be of the form <sample_id>_<unique_seq_id>. The following steps, corresponding to the preliminary data preparation, are applied.
 
-1. Pick OTUs with cdhit at similarity of 0.97;
+1. Pick OTUs with uclust at similarity of 0.97;
 
 2. Pick a representative set with the most_abundant method;
 
@@ -56,7 +56,29 @@ All output files will be written to the directory specified by -o, and
 subdirectories as appropriate.
 ""","""pick_otus_through_otu_table.py -i inseqs1.fasta -o wf1/ -p custom_parameters.txt"""))
 
-script_info['output_description'] ="""This script will produce an OTU mapping file (pick_otus.py), a representative set of sequences (FASTA file from pick_rep_set.py), a sequence alignment file (FASTA file from align_seqs.py), taxonomy assignment file (from assign_taxonomy.py), a filtered sequence alignment (from filter_alignment.py), a phylogenetic tree (Newick file from make_phylogeny.py) and an OTU table (from make_otu_table.py)."""
+script_info['script_usage'].append(("""Simple example with denoising""","""This command will do the same steps as the previous example and additionally denoise the data set prior to OTU picking. Only flowgrams in the input sff.txt file that have a matching identifier in inseqs1.fasta are considered here, the rest is discarded.
+
+1. Denoise flowgrams in inseqs1.sff.txt;
+
+2. Pick OTUs with uclust at similarity of 0.97;
+
+3. Pick a representative set with the most_abundant method;
+
+4. Align the representative set with PyNAST (REQUIRED: SPECIFY TEMPLATE ALIGNMENT with align_seqs:template_fp in the parameters file);
+
+5. Assign taxonomy with RDP classifier;
+
+6. Filter the alignment prior to tree building - remove positions which are all gaps, and specified as 0 in the lanemask (REQUIRED: SPECIFY LANEMASK with filter_alignment:lane_mask_fp in the parameters file);
+
+7. Build a phylogenetic tree with FastTree;
+
+8. Build an OTU table.
+
+All output files will be written to the directory specified by -o, and 
+subdirectories as appropriate.
+""","""pick_otus_through_otu_table.py -f inseqs1.sff.txt -m metadata_mapping.txt -i inseqs1.fasta -o wf2/ -p custom_parameters.txt"""))
+
+script_info['output_description'] ="""This script will produce a set of cluster centroids (as a FASTA file) and a cluster mapping file (from denoise.py if sff.txt and mapping file were provided), an OTU mapping file (pick_otus.py), a representative set of sequences (FASTA file from pick_rep_set.py), a sequence alignment file (FASTA file from align_seqs.py), taxonomy assignment file (from assign_taxonomy.py), a filtered sequence alignment (from filter_alignment.py), a phylogenetic tree (Newick file from make_phylogeny.py) and an OTU table (from make_otu_table.py)."""
 
 script_info['required_options'] = [
     make_option('-i','--input_fp',
