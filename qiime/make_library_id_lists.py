61215fc9b0e64ae00a77c68880217cf713e0f74a
Greg Caporaso <gregcaporaso@gmail.com>
2010-05-18 16:52:19 +0000

    Removed function that was no longer being used.

--- qiime/make_library_id_lists.py
+++ qiime/make_library_id_lists.py
@@ -39,38 +39,3 @@ def get_first_id(lines):
         if line.startswith('>'):
             result.add(line[1:].split()[0])
     return result
-
-def make_option_parser():
-    """Generate a parser for command-line options"""
-    
-    usage = """\n\t python make_library_id_lists.py {-i input_fasta_fp -o
-    output_dir} [options]
-
-    [] indicates optional input (order unimportant)
-    {} indicates required input (order unimportant)
-    \nExample:\n\tpython make_library_id_lists.py -i
-    seqs.fna -o /Users/rob/results
-    
-    (For help run: \n\tpython make_library_id_lists.py --help)
-    """
-
-    parser=OptionParser(usage=usage)
-    parser.add_option("-i","--input_fasta",dest='in_fasta',default = None,\
-        help="The path to a FASTA file containing input sequences [REQUIRED]")
-    parser.add_option("-s", "--screened_rep_seqs",dest="screened_rep_seqs",
-        default=None,
-        help="The path to a FASTA file containing screened representative seqs" +
-        "[DEFAULT: %default]")
-    parser.add_option("-u", "--otus",dest="otus",
-        default=None,
-        help="The path to an OTU file mapping OTUs onto rep seqs" +
-        "[DEFAULT: %default]")
-    parser.add_option("-o","--outdir",dest='outdir',\
-        default = '.',\
-        help=""" The base directory to save results (one file per library).""") 
-    parser.add_option("-f", "--field",dest="field", type=int,\
-        default = 1,\
-        help="Index of space-delimited field to read id from [DEFAULT: %default]")
-    parser.add_option("--debug", dest="debug", action="store_true",
-    default=False, help="Show debug output.")
-    return parser
