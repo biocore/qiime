15c857acb8186dbba3e5b8ba8f275b7dda47a9fc
Greg Caporaso <gregcaporaso@gmail.com>
2010-11-22 23:28:31 +0000

    Added ability to pass --stepwords and --w to the uclust application controller. This has not been hooked up to the interface yet.

--- qiime/pick_otus.py
+++ qiime/pick_otus.py
@@ -28,7 +28,6 @@ from cogent.app.dotur import dotur_from_alignment
 from cogent.app.mothur import Mothur
 from cogent.app.formatdb import build_blast_db_from_fasta_path
 from cogent.app.blast import blast_seqs, Blastall, BlastResult
-from cogent.app.uclust import get_clusters_from_fasta_filepath
 from cogent.core.sequence import DnaSequence
 from cogent.util.misc import remove_files
 from cogent import LoadSeqs, DNA, Alignment
@@ -36,6 +35,7 @@ from cogent.util.trie import build_prefix_map
 from cogent.util.misc import flatten
 from qiime.util import FunctionWithParams, sort_fasta_by_abundance
 from qiime.parse import fields_to_dict
+from qiime.pycogent_backports.uclust import get_clusters_from_fasta_filepath
 
 class OtuPicker(FunctionWithParams):
     """An OtuPicker dereplicates a set of sequences at a given similarity.
