45d12ba5c8dd5b2c2718fe6468340187ea2d76bb
Greg Caporaso <gregcaporaso@gmail.com>
2011-05-30 04:50:47 +0000

    Added documentation to the merge_otu_tables.py script (previously had essentially no documentation).

--- scripts/merge_otu_tables.py
+++ scripts/merge_otu_tables.py
@@ -21,19 +21,21 @@ from qiime.format import format_otu_table
 options_lookup = get_options_lookup()
 
 script_info = {}
-script_info['brief_description'] = ""
-script_info['script_description'] = ""
-script_info['script_usage'] = [("","","")]
+script_info['brief_description'] = "Merge two or more OTU tables into a single OTU table."
+script_info['script_description'] = """This script merges two or more OTU tables into a single OTU table. This is useful, for example, when you've created several reference-based OTU tables for different analyses and need to combine them for a larger analysis. 
+
+Requirements: This process requires that the sample IDs are distinct across the different OTU tables. It is also very important that your OTUs are consistent across across the different OTU tables. For example, you cannot safely merge OTU tables from two independent de novo OTU picking runs. Finally, either all or none of the OTU tables can contain taxonomic information: you can't merge some OTU tables with taxonomic data and some without taxonomic data."""
+script_info['script_usage'] = [\
+ ("",
+  "Merge two OTU tables into a single OTU table",
+  "%prog -i otu_table1.txt,otu_table2.txt -o merged_otu_table.txt")]
 script_info['output_description']= ""
 script_info['required_options'] = [\
  # Example required option
- make_option('-i','--input_fps',help='the otu tables'),\
+ make_option('-i','--input_fps',help='the otu tables (comma-separated)'),\
  make_option('-o','--output_fp',help='the output otu table filepath'),\
 ]
-script_info['optional_options'] = [\
- # Example optional option
- #make_option('-o','--output_dir',help='the output directory [default: %default]'),\
-]
+script_info['optional_options'] = []
 script_info['version'] = __version__
 
 def main():
