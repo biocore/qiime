8f0b926edddd5569f32ab028e5a5a6d044fb1174
Will Van Treuren <William.Vantreuren@colorado.edu>
2011-06-01 19:05:26 +0000

    Added library code and tests for compare_alpha_diversity for use in two sample comparisons between different categories in an alpha rarefaction file.

---
+++ qiime/compare_alpha_diversity.py
@@ -0,0 +1,304 @@
+#!/usr/bin/env python
+# File created on 19 May 2011
+from __future__ import division
+
+__author__ = "William Van Treuren"
+__copyright__ = "Copyright 2011, The QIIME project"
+__credits__ = ["William Van Treuren, Greg Caporaso"]
+__license__ = "GPL"
+__version__ = "1.2.1-dev"
+__maintainer__ = "William Van Treuren"
+__email__ = "vantreur@colorado.edu"
+__status__ = "Development"
+
+from numpy import array, isnan
+from qiime.parse import parse_mapping_file_to_dict, parse_rarefaction
+from cogent.maths.stats.test import t_two_sample
+
+def make_value_pairs_from_category(mapping_data, category):
+    """creates all pairs of unique category values from mapping data
+    
+    input:
+        mapping_data - a nested dictionary that maps SampleIds to
+        descriptor categories (e.g. {'Id1': {'Weight':'Fat'}}
+    
+        category - a string which specifies a category found in the 
+        mapping data (e.g. 'Obese')
+    
+    output:
+        unique_pairs - a list of unique pairs of the values specified by
+        category
+        (e.g. [('Obese','Fat'),('Obese','notFat'),('Fat','notFat')]
+    
+    """
+    
+    #gets the keys of the mapping file dictionary. corresponds to the 
+    #names of the individuals in the mapping data file
+    
+    keys = mapping_data.keys()
+       
+    categories = []
+    for key in keys:
+        try:
+            categories.append(mapping_data[key][category])
+        except KeyError:
+            raise ValueError(('the specified category ({0}) was '+\
+                    'not found in the mapping file').format(category))
+    #strip duplicate values from this list
+    
+    unique_vals = []
+    for val in categories:
+        if unique_vals.count(val)==0:
+            unique_vals.append(val)
+            
+    #create and populate list of unique pairs of category values
+    
+    unique_pairs = []
+    for i in range(len(unique_vals)):
+        for j in unique_vals[i+1:]:
+            unique_pairs.append((unique_vals[i], j))
+    
+    return unique_pairs
+            
+def make_category_values_Id_dict(mapping_data, category):
+    """makes dict lists all SampleIds that have given category value
+    
+    input:
+        mapping_data - a nested dictionary that maps SampleID to
+        descriptor categories (e.g. {'Id1': {'Weight':'Fat}}
+    
+        category - a string which specifies a category found in the 
+        mapping data (e.g. 'Weight')
+    
+    output:
+        cat_val_Ids - a dictionary with category values as keys
+        and a list of SampleIds which have the specific category value
+        as values 
+        (e.g. {'Fat':['Id1','Id2'],'notFat':['Id3'],'Obese':['Id4']}).
+    
+    """
+    
+    keys = mapping_data.keys()
+    
+    # create and populate list of all the different values for the 
+    # category that was specified 
+    
+    categories = []
+    for key in keys:
+        categories.append(mapping_data[key][category])
+    
+    #strip duplicate values from this list
+    
+    unique_vals = []
+    for val in categories:
+        if unique_vals.count(val)==0:
+            unique_vals.append(val)
+    
+    #make a dictionary with keys that are the possible values of the 
+    #category that was specified.
+    
+    cat_val_Ids = {}
+    for val in unique_vals:
+        cat_val_Ids[val] = []
+    
+    #populate the cat_val dict with Id's which have proper category 
+    
+    for key in keys:
+        for val in cat_val_Ids.keys():
+            if mapping_data[key][category] == val:
+                cat_val_Ids[val].append(key)
+    
+    return cat_val_Ids
+        
+def map_category_value_pairs_to_Ids(value_pairs, cat_val_Ids):
+    """maps category value pairs to Id's which have that category value
+    
+    input:
+        value_pairs - a list of pairs of categories (e.g. 
+        [('Obese','Fat'),('Fat','notFat'),('Obese','notFat')]
+        
+        cat_val_Ids - a dictionary with category values as keys
+        and a list of SampleId's which have the specific category value 
+        as values
+        (e.g. {'Fat':['Id1','Id2'],'notFat':['Id3'],'Obese':['Id4']}).
+    
+    output:
+        mapped_pairs - the list of value_pairs with the values replaced
+        by the SampleIds which have the values specified in the pair e.g
+        [(['Id4'],['Id1','Id2']),(['Id1','Id2'],\
+        ['Id3]),(['Id4'],['Id3])] 
+    """
+    
+    mapped_pairs = []
+    
+    for pair in value_pairs:
+        mapped_pairs.append((cat_val_Ids[pair[0]],cat_val_Ids[pair[1]]))
+    
+    return mapped_pairs
+
+
+def make_SampleIds_rarefaction_columns_dict(rarefaction_list):
+    """maps SampleId to column in parsed rarefaction file output
+    
+    input:
+        rarefaction_list - ouput of parse_rarefaction.py. a nested list
+        of scores and SampleIds and other fields. 
+    
+    output:
+        map_from_Id_to_col - a dict which has as keys the SampleIds, and
+        as values the col they are in the in the parsed rarefaction list
+    """
+    
+    map_from_Id_to_col = {}
+    
+    # the first 3 entries in the rarefaction_list are not SampleIDs
+    # so we dump them
+    
+    Ids = rarefaction_list[0][3:]
+    
+    for Id in Ids:
+        map_from_Id_to_col[Id] = Ids.index(Id)
+    
+    return map_from_Id_to_col
+        
+    
+def extract_rarefaction_scores_at_depth(depth, rarefaction_list):
+    """makes rarefaction matrix with row=iteration and col=SampleId
+    
+    input:
+        depth - an integer which corresponds to the depth of the
+        rarefaction. also called the "sequences per sample" in the 
+        rarefaction file
+        
+        rarefaction_list - ouput of parse_rarefaction.py. a nested list
+        of scores and SampleIds and other fields. 
+    
+    output:
+        result - a matrix with rows=rarefaction scores at a given depth
+        and iteration, and cols=SampleIds.
+    
+    """
+    # make and populate an array that has as rows rarefaction values
+    # at the same depth and iteration and as cols SampleIds.
+    
+    score_matrix = []
+    
+    # the 4th element of rarefaction_list is a list of scores for each
+    # different SampleId
+    
+    for line in rarefaction_list[3]:
+        if line[0] == depth:
+            # the first two elements are just rarefaction depth and 
+            # iteration, throw these away
+            score_matrix.append(line[2:])
+    
+    # raise error if rarefaction depth not found in rarefaction file
+    if score_matrix == []:
+        raise ValueError(('Specified depth ({0}) was not found in'+\
+                    'the rarefaction file').format(depth))
+                        
+    
+    score_matrix_elements = []
+    
+    for line in score_matrix:
+        score_matrix_elements.append(line)
+    
+    result = array(score_matrix_elements) 
+    
+    # raise error if any rarefaction score at spec. depth is Nan
+    if isnan(result).any():
+        raise ValueError(('Specified depth ({0}) has NaNs for some'+\
+                            'rarefaction scores').format(depth))
+    
+    return result
+
+
+def convert_SampleIds_to_rarefaction_mtx(chosen_SampleIds,score_matrix,\
+                                        map_from_SampleIds_to_cols):
+    """converts list of SampleIDs to score mtx from rarefaction file
+    
+    input:
+        chosen_SampleIds - a list of SampleIds
+        
+        score_matrix - a matrix created by
+        extract_rarefaction_scores_at_depth which represents the
+        rarefaction scores for a given depth
+        
+        map_from_SampleIds_to_cols - a dict which maps a SampleId to 
+        the column its scores are in in the score matrix
+    
+    output:
+        reduced_scores_matrix - a matrix which is the input scores mtx
+        with only the cols that correspond to the chosen_SampleIds
+    
+    """
+    #create and populate a list that specifies the r_array columns which
+    #correspond to the name_list
+    
+    cols=[]
+    
+    for Id in chosen_SampleIds:
+        cols.append(map_from_SampleIds_to_cols[Id])
+        
+    # grab only the columns we need based on a passed list of names and
+    # a dictionary to convert between those names and the proper cols
+    reduced_scores_matrix = score_matrix.take(cols, axis=1)
+    
+    return reduced_scores_matrix
+    
+    
+
+def compare_alpha_diversities(rarefaction_lines, mapping_lines, 
+                              category, depth):
+    """compares alpha diversities
+    
+    inputs:
+        rarefaction_file - rarefaction file which gives scores for 
+        various rarefactions and depths
+        
+        mapping_file - file that has ID's and categories that the ID's
+        fall in
+        
+        category - the category to be compared, is a string
+        
+        depth - the depth of the rarefaction_file to use, is an integer
+    
+    outputs:
+        results - a nested dictionary which specifies the category as
+        the top level key, and as its value, dictionaries which give the
+        results of the t_two_sample test for all unique pairs of values
+        in the specified category
+    
+    """
+     
+    rarefaction_data = parse_rarefaction(rarefaction_lines)
+    mapping_data = parse_mapping_file_to_dict(mapping_lines)[0]
+    value_pairs = make_value_pairs_from_category(mapping_data, category)
+    
+    category_values_Ids = make_category_values_Id_dict(mapping_data, 
+                                                       category)
+    
+    SampleId_pairs = map_category_value_pairs_to_Ids(value_pairs,
+                                                    category_values_Ids)
+    
+    map_from_Id_to_col = make_SampleIds_rarefaction_columns_dict(
+                                                       rarefaction_data)
+    
+    reduced_rarefaction_mtx = extract_rarefaction_scores_at_depth(depth,
+                                                       rarefaction_data)
+    
+    results = {category:{}}
+    
+    for pair in range(len(SampleId_pairs)):
+        i=(convert_SampleIds_to_rarefaction_mtx(SampleId_pairs[pair][0],
+                           reduced_rarefaction_mtx, map_from_Id_to_col))
+        
+        j=(convert_SampleIds_to_rarefaction_mtx(SampleId_pairs[pair][1],
+                           reduced_rarefaction_mtx, map_from_Id_to_col))
+        
+        results[category][(str(value_pairs[pair][0]),
+                           str(value_pairs[pair][1]))] =\
+                          t_two_sample(i,j)
+    
+    return results
+    
---
+++ tests/test_compare_alpha_diversities.py
@@ -0,0 +1,228 @@
+#!/usr/bin/env python
+# File created on 19 May 2011
+
+
+from __future__ import division
+
+__author__ = "William Van Treuren"
+__copyright__ = "Copyright 2011, The QIIME project"
+__credits__ = ["William Van Treuren, Greg Caporaso"]
+__license__ = "GPL"
+__version__ = "1.2.1-dev"
+__maintainer__ = "William Van Treuren"
+__email__ = "vantreur@colorado.edu"
+__status__ = "Development"
+ 
+
+
+from cogent.util.unit_test import TestCase,main
+from qiime.parse import parse_mapping_file_to_dict, parse_rarefaction
+from numpy import array
+from qiime.compare_alpha_diversity import (compare_alpha_diversities,
+    extract_rarefaction_scores_at_depth,
+    make_SampleIds_rarefaction_columns_dict,
+    make_category_values_Id_dict,
+    make_value_pairs_from_category,
+    map_category_value_pairs_to_Ids,
+    convert_SampleIds_to_rarefaction_mtx)
+
+class TopLevelTests(TestCase):
+    """Tests of top level functions"""
+    
+    def setUp(self):
+        """define data for tests"""
+        self.rarefaction_file = \
+         ['\tsequences per sample\titeration\t123\t234\t345',
+          'rare10.txt\t10\t0\t1.99181\t5.42877\t2.13996',
+          'rare10.txt\t10\t1\t2.07163\t1.42877\t2.37055',
+          'rare310.txt\t310\t0\t8.83115\t6.42877\t11.00725',
+          'rare310.txt\t310\t1\t10.05242\t9.42877\t8.24474',
+          'rare810.txt\t810\t0\t12.03067\tn/a\t11.58928',
+          'rare910.txt\t910\t1\t12.9862\t2.42877\t11.58642']
+        
+        self.rarefaction_data = parse_rarefaction(self.rarefaction_file)
+        
+        self.mapping_file = \
+        ['#SampleID\tTreatment\tLinker'+\
+         'PrimerSequence\tDose\tTTD\tDescription',
+         '#Comment Line',
+         '123\tAAAA\tBBBB\tHigh\t31\tM_ID_123',
+         '234\tCCCC\tDDDD\tLow\t67\tM_ID_234',
+         '345\tAAAA\tFFFF\tMed\t21\tM_ID_345']
+        
+        self.mapping_data = \
+         parse_mapping_file_to_dict(self.mapping_file)[0]
+        self.value_pairs_Dose = \
+         [('Low','Med'),('Low','High'),('Med','High')]                         
+        self.value_pairs_TTD = \
+         [('67', '21'), ('67', '31'), ('21', '31')]
+        self.value_pairs_Treatment = \
+         [('CCCC', 'AAAA')]
+        self.cat_val_Dose = \
+         {'High': ['123'], 'Low': ['234'], 'Med': ['345']}
+        self.cat_val_TTD = \
+         {'21': ['345'], '31': ['123'], '67': ['234']}
+        self.cat_val_Treatment = \
+         {'AAAA': ['345', '123'], 'CCCC': ['234']}
+        self.Id_pairs_Dose = \
+         [(['234'], ['345']), (['234'], ['123']), (['345'], ['123'])]
+        self.Id_pairs_TTD = \
+         [(['234'], ['345']), (['234'], ['123']), (['345'], ['123'])]
+        
+        self.Id_pairs_Treatment = \
+         [(['234'], ['345', '123'])]
+        
+        self.rarefaction_cols_dict = \
+         {'123': 0, '234': 1, '345': 2}
+       
+        self.extracted_mtx_10 = \
+         array([[ 1.99181,  5.42877,  2.13996],
+               [ 2.07163,  1.42877,  2.37055]])
+        
+        self.extracted_mtx_310 = \
+         array([[  8.83115,   6.42877,  11.00725],
+               [ 10.05242,   9.42877,   8.24474]])
+        
+        self.extracted_mtx_910 = \
+         array([[ 12.9862 ,   2.42877,  11.58642]])
+        
+        self.sample_pair1 = \
+         (['234'], ['345', '123'])
+            
+        self.rarefaction_mtx_for_sample_pair1_0 = \
+         array([[ 5.42877],
+               [ 1.42877]])
+        
+        self.rarefaction_mtx_for_sample_pair1_1 = \
+         array([[ 2.13996,  1.99181],
+               [ 2.37055,  2.07163]])
+        
+        self.compared_alpha_diversities_TTD = \
+         {'TTD':{('21','31'):(1.8321466933860993,0.20839398129924847),
+         ('67', '21'): (0.58578495700890432, 0.61731739324369639),
+         ('67', '31'): (0.69838596448703294, 0.55721515283248324)}}
+        
+    def test_make_value_pairs_from_category(self):
+        """check value pairs returns correct unique pairs for categories
+        """
+        
+          
+        self.assertEqual(
+         self.value_pairs_Dose,
+         make_value_pairs_from_category(self.mapping_data, 'Dose'))
+        
+        self.assertEqual(
+            self.value_pairs_TTD, 
+            make_value_pairs_from_category(self.mapping_data, 'TTD'))
+        
+        self.assertEqual(
+            self.value_pairs_Treatment, 
+            make_value_pairs_from_category(self.mapping_data, 
+                                            'Treatment'))
+        
+        self.assertRaises(
+         ValueError,
+         make_value_pairs_from_category, self.mapping_data, 'WrongCat')
+                         
+    def test_make_category_values_Id_dict(self):
+        """check value pairs reference correct Id pairs"""
+             
+        self.assertEqual(
+            self.cat_val_Dose,
+            make_category_values_Id_dict(self.mapping_data, 'Dose'))
+        
+        self.assertEqual(
+            self.cat_val_TTD,
+            make_category_values_Id_dict(self.mapping_data, 'TTD'))
+        
+        self.assertEqual(
+            self.cat_val_Treatment,
+            make_category_values_Id_dict(self.mapping_data,
+                                         'Treatment'))
+    
+    def test_map_category_value_pairs_to_Ids(self):
+        """check value pairs converted to correct Ids"""
+                
+        self.assertEqual(
+            self.Id_pairs_Dose,
+            map_category_value_pairs_to_Ids(self.value_pairs_Dose,
+                                            self.cat_val_Dose))
+
+        self.assertEqual(
+            self.Id_pairs_TTD,
+            map_category_value_pairs_to_Ids(self.value_pairs_TTD,
+                                            self.cat_val_TTD))
+        
+        self.assertEqual(
+            self.Id_pairs_Treatment,
+            map_category_value_pairs_to_Ids(
+            self.value_pairs_Treatment,
+            self.cat_val_Treatment))
+    
+    def test_make_SampleIds_rarefaction_columns_dict(self):
+        """ """
+                
+        self.assertEqual(
+         self.rarefaction_cols_dict,
+         make_SampleIds_rarefaction_columns_dict(self.rarefaction_data))
+        
+    def test_extract_rarefaction_scores_at_depth(self):
+        """check correct errors raised for wrong depths and correct mtx
+        """
+           
+        self.assertEqual(
+         self.extracted_mtx_10,
+         extract_rarefaction_scores_at_depth(10,self.rarefaction_data))
+        
+        self.assertEqual(
+         self.extracted_mtx_310,
+         extract_rarefaction_scores_at_depth(310,self.rarefaction_data))
+        
+        self.assertEqual(
+         self.extracted_mtx_910,
+         extract_rarefaction_scores_at_depth(910,self.rarefaction_data))
+        
+        self.assertRaises(
+         ValueError,
+         extract_rarefaction_scores_at_depth,810,self.rarefaction_data)
+        
+        self.assertRaises(
+         ValueError,
+         extract_rarefaction_scores_at_depth,100,self.rarefaction_data)
+        
+    def test_convert_SampleIds_to_rarefaction_mtx(self):
+        """check correct reduced rarefaction scores mtx produced"""
+                 
+        self.rarefaction_mtx_for_sample_pair1_0 = \
+         array([[ 5.42877],
+               [ 1.42877]])
+        
+        self.rarefaction_mtx_for_sample_pair1_1 = \
+         array([[ 2.13996,  1.99181],
+               [ 2.37055,  2.07163]])
+        
+        self.assertEqual(
+            convert_SampleIds_to_rarefaction_mtx(
+                self.sample_pair1[0],
+                self.extracted_mtx_10,
+                self.rarefaction_cols_dict),
+            self.rarefaction_mtx_for_sample_pair1_0)
+        
+        self.assertEqual(
+            convert_SampleIds_to_rarefaction_mtx(
+                self.sample_pair1[1],
+                self.extracted_mtx_10,
+                self.rarefaction_cols_dict),
+            self.rarefaction_mtx_for_sample_pair1_1)
+        
+    def test_compare_alpha_diversity(self):
+        """test main function properly compares alpha diversities"""
+                
+        self.assertEqual(
+            self.compared_alpha_diversities_TTD,
+            compare_alpha_diversities(self.rarefaction_file,
+                                      self.mapping_file, 'TTD', 10))
+
+
+if __name__ == "__main__":
+    main()
