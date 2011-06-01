7007973aedbc677b7683575b18c9e9902cf55e07
Justin Kuczynski <justinak@gmail.com>
2011-06-01 22:51:22 +0000

    removed dependance on pycogent_backports/distance_transform.py

--- tests/test_beta_diversity.py
+++ tests/test_beta_diversity.py
@@ -18,7 +18,7 @@ from cogent.util.unit_test import TestCase, main
 from cogent.util.misc import remove_files
 from cogent.app.util import get_tmp_filename
 from cogent.core.tree import PhyloNode
-from qiime.pycogent_backports.distance_transform import dist_chisq
+from cogent.maths.distance_transform import dist_chisq
 from qiime.parse import parse_newick, parse_distmat, parse_matrix
 from qiime.format import format_otu_table
 from qiime.beta_diversity import BetaDiversityCalc, single_file_beta,\
