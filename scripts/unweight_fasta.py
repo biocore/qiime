8e5f62a096f81a8a5a010e7d4f2631d65950645a
Jose Carlos Clemente <jose.clemente@gmail.com>
2011-10-28 21:16:48 +0000

    Option types added

--- scripts/unweight_fasta.py
+++ scripts/unweight_fasta.py
@@ -35,8 +35,8 @@ AATGCTTGTCACATCGATGC
 AATGCTTGTCACATCGATGC''',"%prog -i input.fna -o output.fna -l goodsample")]
 script_info['output_description']= "a .fasta file"
 script_info['required_options'] = [
- make_option('-i','--input_fasta',help='the input fasta file'),
- make_option('-o','--output_file',help='the output fasta filepath'),
+ make_option('-i','--input_fasta',type='existing_filepath',help='the input fasta file'),
+ make_option('-o','--output_file',type='new_filepath',help='the output fasta filepath'),
  make_option('-l','--label',help='sequence label used for all records. fasta label lines will look like: >label_423'),
 ]
 script_info['optional_options'] = []
