9511638151f78f39325369cc1a983f16ccbcf54b
Daniel McDonald <wasade@gmail.com>
2011-12-22 06:44:37 +0000

    added credit mention

--- scripts/merge_otu_tables.py
+++ scripts/merge_otu_tables.py
@@ -4,7 +4,7 @@ from __future__ import division
 
 __author__ = "Greg Caporaso"
 __copyright__ = "Copyright 2011, The QIIME Project"
-__credits__ = ["Greg Caporaso"]
+__credits__ = ["Greg Caporaso", "Daniel McDonald"]
 __license__ = "GPL"
 __version__ = "1.4.0-dev"
 __maintainer__ = "Greg Caporaso"
