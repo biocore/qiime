896ca03d55654d7a902b6cd71fdb8d7dec356872
Greg Caporaso <gregcaporaso@gmail.com>
2012-03-14 23:24:45 +0000

    updated to refer to biom-formatted otu tables and use pycogent option types

--- scripts/multiple_rarefactions_even_depth.py
+++ scripts/multiple_rarefactions_even_depth.py
@@ -4,7 +4,7 @@ from __future__ import division
 
 __author__ = "Justin Kuczynski"
 __copyright__ = "Copyright 2011, The QIIME Project"
-__credits__ = ["Justin Kuczynski"]
+__credits__ = ["Justin Kuczynski", "Greg Caporaso"]
 __license__ = "GPL"
 __version__ = "1.4.0-dev"
 __maintainer__ = "Justin Kuczynski"
@@ -20,16 +20,19 @@ from qiime.rarefaction import RarefactionMaker
 script_info={}
 script_info['brief_description']="""Perform multiple rarefactions on a single otu table, at one depth of sequences/sample"""
 script_info['script_description']="""To perform bootstrap, jackknife, and rarefaction analyses, the otu table must be subsampled (rarefied).  This script rarefies, or subsamples, an OTU table.  This does not provide curves of diversity by number of sequences in a sample.  Rather it creates a subsampled OTU table by random sampling (without replacement) of the input OTU table.  Samples that have fewer sequences then the requested rarefaction depth are omitted from the ouput otu tables.  The pseudo-random number generator used for rarefaction by subsampling is NumPy's default - an implementation of the Mersenne twister PRNG."""
+
 script_info['script_usage']=[]
-script_info['script_usage'].append(("""Example:""","""subsample otu_table.txt at 400 seqs/sample (-d), 100 times (-n), write results to files (i.e. rarefaction_400_17.txt)""","""%prog -i otu_table.txt -o rarefaction_tables -d 400 -n 100"""))
-script_info['output_description']="""The results of this script consist of n subsampled OTU tables, written to the directory specified by -o. The file has the same otu table format as the input otu_table.txt. note: if the output files would be empty, no files are written"""
+
+script_info['script_usage'].append(("""Example:""","""subsample otu_table.txt at 100 seqs/sample (-d) 10 times (-n) and write results to files (e.g., rarefaction_400_0.biom) in 'rarefied_otu_tables/' (-o).""","""%prog -i otu_table.biom -o rarefied_otu_tables/ -d 100 -n 10"""))
+
+script_info['output_description']="""The results of this script consist of n subsampled OTU tables, written to the directory specified by -o. The file has the same otu table format as the input otu_table.biom. Note: if the output files would be empty, no files are written."""
 
 
 script_info['required_options']=[
-    make_option('-i', '--input_path',
+    make_option('-i', '--input_path',type='existing_filepath',
         help='input otu table filepath'),
 
-    make_option('-o', '--output_path',
+    make_option('-o', '--output_path',type='new_dirpath',
         help="write output rarefied otu tables files to this dir (makes dir if it doesn't exist)"),
 
     make_option('-d', '--depth', type=int,
