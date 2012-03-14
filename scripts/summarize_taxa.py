1cbc23b6bbe0a34b5a95f1d9f9cbd64d98289b88
Greg Caporaso <gregcaporaso@gmail.com>
2012-03-14 01:40:43 +0000

    fixed bug in writing to mapping file and added an example of that functionality to help text.

--- scripts/summarize_taxa.py
+++ scripts/summarize_taxa.py
@@ -13,9 +13,6 @@ __maintainer__ = "Daniel McDonald"
 __email__ = "wasade@gmail.com"
 __status__ = "Development"
  
-
-#from qiime.util import parse_command_line_parameters, \
-#        convert_otu_table_relative
 from qiime.util import parse_command_line_parameters
 from qiime.util import make_option,get_options_lookup,create_dir
 from qiime.summarize_taxa import make_summary, add_summary_mapping
@@ -29,11 +26,13 @@ from biom.parse import parse_biom_table
 options_lookup = get_options_lookup()
 
 script_info={}
-script_info['brief_description']="""Summarize Taxa"""
+script_info['brief_description']="""Summarize taxa and store results in a new table or appended to an existing mapping file."""
 script_info['script_description']="""The summarize_taxa.py script provides summary information of the representation of taxonomic groups within each sample. It takes an OTU table that contains taxonomic information as input. The taxonomic level for which the summary information is provided is designated with the -L option. The meaning of this level will depend on the format of the taxon strings that are returned from the taxonomy assignment step. The taxonomy strings that are most useful are those that standardize the taxonomic level with the depth in the taxonomic strings. For instance, for the RDP classifier taxonomy, Level 2 = Domain (e.g. Bacteria), 3 = Phylum (e.g. Firmicutes), 4 = Class (e.g. Clostridia), 5 = Order (e.g. Clostridiales), 6 = Family (e.g. Clostridiaceae), and 7 = Genus (e.g. Clostridium). By default, the relative abundance of each taxonomic group will be reported, but the raw counts can be returned if -a is passed."""
 script_info['script_usage']=[]
 
-script_info['script_usage'].append(("""Examples:""","""Summarize taxa based at taxonomic levels 2, 3, 4, 5, and 6, where the and write results are written to the directory "./tax" ""","""%prog -i otu_table.biom -o ./tax"""))
+script_info['script_usage'].append(("""Examples:""","""Summarize taxa based at taxonomic levels 2, 3, 4, 5, and 6, where the and write resulting taxa tables to the directory "./tax" ""","""%prog -i otu_table.biom -o ./tax"""))
+
+script_info['script_usage'].append(("""Examples:""","""Summarize taxa based at taxonomic levels 2, 3, 4, 5, and 6, where the and write resulting mapping files to the directory "./tax" ""","""%prog -i otu_table.biom -o tax_mapping/ -m Fasting_Map.txt"""))
 
 script_info['output_description']="""There are two possible output formats depending on whether or not a mapping file is provided with the -m option. If a mapping file is not provided, a table is returned where the taxonomic groups are each in a row and there is a column for each sample. If a mapping file is provided, the summary information will be appended to this file. Specifically, a new column will be made for each taxonomic group to which the relative abundances or raw counts will be added to the existing rows for each sample. The addition of the taxonomic information to the mapping file allows for taxonomic coloration of Principal coordinates plots in the 3d viewer. As described in the make_3d_plots.py section, principal coordinates plots can be dynamically colored based on any of the metadata columns in the mapping file. Dynamic coloration of the plots by the relative abundances of each taxonomic group can help to distinguish which taxonomic groups are driving the clustering patterns.
 """
@@ -151,8 +150,7 @@ def main():
             summary, tax_order = add_summary_mapping(otu_table, mapping,
                                                      int(level))
             write_add_taxa_summary_mapping(summary,tax_order,mapping,
-                                            header,output_fname,delimiter,
-                                            opts.transposed_output)
+                                            header,output_fname,delimiter)
         else:
             #define output filename
             output_fname = join(output_dir_path,basename+'_L%s.txt' % (level))
