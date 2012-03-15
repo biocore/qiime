d6a868ecf07f3b50eeb58b50e25205517798c809
Greg Caporaso <gregcaporaso@gmail.com>
2012-03-15 00:42:38 +0000

    updated to refer to biom-formatted otu tables and use pycogent option types

--- scripts/plot_rank_abundance_graph.py
+++ scripts/plot_rank_abundance_graph.py
@@ -28,13 +28,13 @@ script_info['brief_description'] = "plot rank-abundance curve"
 script_info['script_description'] = "Plot a set of rank-abundance graphs from an OTU table and a set of sample names. Multiple graphs will be plotted into the same figure, in order to allow for an easy comparison across samples."
 script_info['script_usage'] = [("Single graph example",
                                 "Plot the rank-abundance curve of one sample using a linear scale for the x_axis:",
-                                "plot_rank_abundance_graph.py -i otu_table.txt  -s 'Sample1' -x -v"),
+                                "%prog -i otu_table.biom  -s 'PC.354' -x -v -o single_plot/"),
                                ("multiple graph example",
                                 "Plot the rank-abundance curve of several sample:",
-                                "plot_rank_abundance_graph.py -i otu_table.txt  -s 'Sample1,Sample3,Sample5' -x  -v"),
+                                "%prog -i otu_table.biom  -s 'PC.354,PC.481,PC.636' -x -v -o multi_plot/"),
                                ("multiple graph example",
                                 "Plot the rank-abundance curve of all samples in an OTU table:",
-                                "plot_rank_abundance_graph.py -i otu_table.txt  -s '*' -x -f eps -v"),
+                                "%prog -i otu_table.biom  -s '*' -x -f eps -v -o all_plot/"),
                                ]
 
 script_info['output_description']= ""
@@ -42,7 +42,7 @@ script_info['output_description']= ""
 script_info['required_options'] = [
  options_lookup['otu_table_as_primary_input'],
  make_option('-s','--sample_name',help='name of the sample to plot. Use "*" to plot all.'),
- make_option('-o','--output_dir',help='name of output directory'),
+ make_option('-o','--output_dir',help='name of output directory',type='new_dirpath'),
  ]
 
 #could basically allow all of matplotlib format here
