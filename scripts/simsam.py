90d0ee09cc73c73107459598e6ffe69cf834eb07
Greg Caporaso <gregcaporaso@gmail.com>
2012-03-15 01:31:14 +0000

    updated to refer to biom-formatted otu tables and use pycogent option types

--- scripts/simsam.py
+++ scripts/simsam.py
@@ -21,12 +21,12 @@ from biom.parse import parse_biom_table
 script_info = {}
 script_info['brief_description'] = "Simulate samples for each sample in an OTU table, using a phylogenetic tree."
 script_info['script_description'] = ""
-script_info['script_usage'] = [("","Make 3 related sample for each sample in in_otu_table.txt.","%prog -i in_otu_table.txt -t rep_set.tre -o out_otu_table.txt -d .001 -n 3")]
+script_info['script_usage'] = [("","Make 3 related sample for each sample in otu_table.biom.","%prog -i otu_table.biom -t rep_set.tre -o otu_table.simsam.biom -d .001 -n 3")]
 script_info['output_description']= "an otu table, samples are named: 'original_sample_0, original_sample_1 ...'"
 script_info['required_options'] = [
- make_option('-i','--otu_table',help='the input otu table'),
- make_option('-t','--tree_file',help='tree file'),
- make_option('-o','--output_file',help='the output file'),
+ make_option('-i','--otu_table',help='the input otu table',type='existing_filepath'),
+ make_option('-t','--tree_file',help='tree file',type='existing_filepath'),
+ make_option('-o','--output_file',help='the output file',type='new_filepath'),
  make_option('-d','--dissim',help='dissimilarity between nodes up the tree',
     type=float),
  make_option('-n','--num',
