b548716746bc2bbe366c290784ccfd07dc38b725
Greg Caporaso <gregcaporaso@gmail.com>
2012-03-15 01:10:54 +0000

    updated to refer to biom-formatted otu tables and use pycogent option types

--- scripts/parallel_multiple_rarefactions.py
+++ scripts/parallel_multiple_rarefactions.py
@@ -30,15 +30,19 @@ options_lookup = get_options_lookup()
 script_info={}
 script_info['brief_description']="""Parallel multiple file rarefaction"""
 script_info['script_description']="""This script performs like the multiple_rarefactions.py script, but is intended to make use of multicore/multiprocessor environments to perform analyses in parallel."""
+
 script_info['script_usage']=[]
-script_info['script_usage'].append(("""Example""","""Build rarefied otu tables containing 100 (-m) to 2000 (-x) sequences in steps of 100 (-s) with 5 (-n) repetions per number of sequences, from /home/qiime_user/otu_table.txt (-i). Write the output files to the /home/qiime_user/rare directory (-o, will be created if it doesn't exist). The name of the output files will be of the form /home/qiime_user/rare/rarefaction_<num_seqs>_<reptition_number>.txt""","""%prog -o /home/qiime_user/rare -m 100 -x 2000 -s 100 -n 5 -i /home/qiime_user/otu_table.txt"""))
-script_info['script_usage'].append(("""Example 2""","""Build 8 rarefied otu tables each containing exactly 100 sequences per sample (even depth rarefaction).""","""%prog -o /home/qiime_user/rare -m 100 -x 100 -s 100 -n 8 -i /home/qiime_user/otu_table.txt"""))
+
+script_info['script_usage'].append(("""OTU tables of different depths""","""Build rarefied otu tables containing 10 (-m) to 140 (-x) sequences in steps of 10 (-s) with 2 (-n) repetions per number of sequences, from otu_table.biom (-i). Write the output files to the rarefied_otu_tables directory (-o, will be created if it doesn't exist). The name of the output files will be of the form rarefaction_<num_seqs>_<reptition_number>.biom""","""%prog -o $PWD/rarefied_otu_tables/ -m 10 -x 140 -s 10 -n 2 -i $PWD/otu_table.biom"""))
+
+script_info['script_usage'].append(("""OTU tables of the same depth""","""Build 8 rarefied otu tables each containing exactly 100 sequences per sample (even depth rarefaction).""","""%prog -o $PWD/even_otu_tables/ -m 100 -x 100 -n 8 -i $PWD/otu_table.biom"""))
+
 script_info['output_description']="""The result of parallel_multiple_rarefactions.py consists of a number of files, which depend on the minimum/maximum number of sequences per samples, steps and iterations. The files have the same otu table format as the input otu_table.txt, and are named in the following way: rarefaction_100_0.txt, where "100" corresponds to the sequences per sample and "0" for the iteration."""
 
 script_info['required_options'] = [\
- make_option('-i', '--input_path',
+ make_option('-i', '--input_path',type='existing_filepath',
         help='input filepath, (the otu table) [REQUIRED]'),\
- make_option('-o', '--output_path',
+ make_option('-o', '--output_path',type='new_dirpath',
         help="write output rarefied otu tables here makes dir if it doesn't exist [REQUIRED]"),\
  make_option('-m', '--min', type=int,help='min seqs/sample [REQUIRED]'),\
  make_option('-x', '--max', type=int,\
@@ -57,8 +61,8 @@ script_info['optional_options'] = [\
  make_option('--suppress_lineages_included', default=True, 
     action="store_false",dest='lineages_included',
     help='Exclude taxonomic (lineage) information for each OTU.'),
- make_option('-N','--single_rarefaction_fp',action='store',\
-           type='string',help='full path to scripts/single_rarefaction.py [default: %default]',\
+ make_option('-N','--single_rarefaction_fp',type='existing_filepath',\
+           help='full path to scripts/single_rarefaction.py [default: %default]',\
            default=join(get_qiime_scripts_dir(),'single_rarefaction.py')),\
  make_option('-s', '--step', type=int, default=1,\
                       help='levels: min, min+step... for level <= max [default: %default]'),\
