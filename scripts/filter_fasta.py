07f21bf4403bc4836df460fcec019293f4af986f
Greg Caporaso <gregcaporaso@gmail.com>
2012-03-30 01:44:11 +0000

    added ability to filter a fasta file based on a list of sample ids.

--- scripts/filter_fasta.py
+++ scripts/filter_fasta.py
@@ -44,6 +44,9 @@ script_info['script_usage'].append(("biom-based filtering", "Keep all sequences 
 
 script_info['script_usage'].append(("fastq filtering","Keep all sequences from a fastq file that are listed in a text file (note: file name must end with .fastq to support fastq filtering).",
  "%prog -f inseqs.fastq -o list_filtered_seqs.fastq -s seqs_to_keep.txt"))
+ 
+script_info['script_usage'].append(("sample id list filtering","Keep all sequences from a fasta file where the sample id portion of the sequence identifier is listed in a text file (sequence identifiers in fasta file must be in post-split libraries format: sampleID_seqID).",
+ "%prog -f sl_inseqs.fasta -o sample_id_list_filtered_seqs.fasta --sample_id_fp map.txt"))
 
 script_info['output_description']= ""
 script_info['required_options'] = [\
@@ -60,8 +63,10 @@ script_info['optional_options'] = [\
   help='A biom file where otu identifiers should be retained'),\
  make_option('-a','--subject_fasta_fp',type='existing_filepath',
   help='A fasta file where the seq ids should be retained.'),\
- make_option('-p','--seq_id_prefix',type='existing_filepath',
+ make_option('-p','--seq_id_prefix',
   help='keep seqs where seq_id starts with this prefix'),\
+ make_option('--sample_id_fp',type='existing_filepath',
+  help='keep seqs where seq_id starts with a sample id listed in this file'),\
  make_option('-n','--negate', help='discard passed seq ids rather than'
   ' keep passed seq ids [default: %default]', default=False, 
   action='store_true'),
@@ -98,6 +103,14 @@ def get_seqs_to_keep_lookup_from_prefix(fasta_f,prefix):
                     if seq_id.startswith(prefix)]
     return {}.fromkeys(seqs_to_keep)
 
+def get_seqs_to_keep_lookup_from_sample_ids(fasta_f,sample_ids):
+    sample_ids = set(sample_ids)
+    seqs_to_keep = set()
+    for seq_id, seq in MinimalFastaParser(fasta_f):
+        if seq_id.split('_')[0] in sample_ids:
+            seqs_to_keep.add(seq_id)
+    return {}.fromkeys(seqs_to_keep)
+
 def get_seqs_to_keep_lookup_from_mapping_file(fasta_f,mapping_f,valid_states):
     sample_ids = {}.fromkeys(\
      sample_ids_from_metadata_description(mapping_f,valid_states))
@@ -120,6 +133,7 @@ def main():
                           opts.subject_fasta_fp,
                           opts.seq_id_prefix,
                           opts.biom_fp,
+                          opts.sample_id_fp,
                           opts.mapping_fp and opts.valid_states])): 
         option_parser.error(error_msg)
 
@@ -148,6 +162,10 @@ def main():
     elif opts.biom_fp:
         seqs_to_keep_lookup = \
          get_seqs_to_keep_lookup_from_biom(open(opts.biom_fp,'U'))
+    elif opts.sample_id_fp:
+        sample_ids = set([e.strip().split()[0] for e in open(opts.sample_id_fp,'U')])
+        seqs_to_keep_lookup = get_seqs_to_keep_lookup_from_sample_ids(
+                         open(opts.input_fasta_fp),sample_ids)
     else:
         option_parser.error(error_msg)
     
