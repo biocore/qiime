5f7fb0595dbc58ef7929e5285ffbc93c922f8570
Tony Walters <william.a.walters@gmail.com>
2012-05-05 23:47:21 +0000

    Added files for add_qiime_labels to svn repository

---
+++ qiime/add_qiime_labels.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+from __future__ import division
+
+__author__ = "William Walters"
+__copyright__ = "Copyright 2011, The QIIME Project"
+__credits__ = ["William Walters"]
+__license__ = "GPL"
+__version__ = "1.4.0-dev"
+__maintainer__ = "William Walters"
+__email__ = "William.A.Walters@colorado.edu"
+__status__ = "Development"
+
+from os.path import join, basename
+from glob import glob
+from string import letters, digits
+
+from cogent.parse.fasta import MinimalFastaParser
+
+from qiime.util import duplicates_indices
+
+def add_qiime_labels(mapping_f,
+                     fasta_dir,
+                     output_dir="./",
+                     count_start=0):
+    """ Main function for combining fasta files, writing valid QIIME labels
+    
+    mapping_f:  open file object of the SampleID to fasta file name.
+    fasta_dir:  Directory of fasta files to combine into a single file.
+    output_dir:  Directory to write output combined file to.
+    count_start:  Number to start enumeration of fasta labels with.
+    
+    """
+    
+    fasta_name_to_sample_id = check_mapping_data(mapping_f)
+    
+    fasta_files = get_fasta_fps(fasta_dir)
+    
+    check_fasta_fps(fasta_name_to_sample_id, fasta_files)
+    
+    write_combined_fasta(fasta_name_to_sample_id, fasta_files, output_dir,
+     counter=count_start)
+    
+def check_mapping_data(mapping_data):
+    """ Checks mapping data for MIMARKS SampleIDs, unique IDs, fasta file names
+    
+    Also returns a dict of fasta file name: SampleID
+    
+    mapping_data:  list of lines of data from mapping file
+    """
+    
+    valid_mimarks = letters + digits + "."
+    
+    fasta_name_to_sample_id = {}
+
+    fasta_names = []
+    for line in mapping_data:
+        curr_line = line.strip().split('\t')
+        if not curr_line or line.startswith("#"):
+            continue
+        try:
+            fasta_name_to_sample_id[basename(curr_line[1].strip())] =\
+             curr_line[0]
+        except IndexError:
+            raise IndexError,("Found non-tab separated line in mapping "+\
+             "data.  Offending line is: %s" % line)
+        for curr_char in curr_line[0]:
+            if curr_char not in valid_mimarks:
+                raise ValueError,("Found invalid character in line: %s\n" %\
+                 line + "SampleIDs must be alphanumeric and . characters "+\
+                 "only")
+        fasta_names.append(curr_line[1].strip())
+       
+    fasta_name_dups = duplicates_indices(fasta_names)
+    if fasta_name_dups:
+        raise ValueError,("Found duplicate fasta names: %s" %\
+         "\t".join([fasta_name for fasta_name in fasta_name_dups.keys()]))
+         
+    return fasta_name_to_sample_id
+        
+def get_fasta_fps(fasta_dir):
+    """ Returns list of fasta filepaths (only .fna, .fasta, and .fa files)
+    
+    fasta_dir:  Directory of fasta files to check
+    """
+    fasta_files = glob(fasta_dir + "/*.fna")
+    fasta_files.extend(glob(fasta_dir + "/*.fa"))
+    fasta_files.extend(glob(fasta_dir + "/*.fasta"))
+    
+    return fasta_files
+    
+def write_combined_fasta(fasta_name_to_sample_id,
+                         fasta_files,
+                         output_dir="./",
+                         counter=0):
+    """ Writes combined, enumerated fasta file
+    
+    fasta_name_to_sample_id:  dict of fasta file name to SampleID
+    fasta_files: list of filepaths to iterate through
+    output_dir:  output directory to write combined file to
+    counter:  Starting number to enumerate sequences with
+    """
+    
+    combined_file_out = open(join(output_dir + "/", "combined_seqs.fna"), "w")
+    
+    for curr_fasta in fasta_files:
+        for label, seq in MinimalFastaParser(open(curr_fasta, "U")):
+            combined_file_out.write(">%s_%d %s\n" %\
+             (fasta_name_to_sample_id[basename(curr_fasta)], counter, label))
+            combined_file_out.write("%s\n" % seq)
+            counter += 1
+            
+def check_fasta_fps(fasta_name_to_sample_id,
+                    fasta_files):
+    """ Checks that all fasta files found match fasta names in mapping file
+    
+    fasta_name_to_sample_id:  dict of fasta file names to sample IDs
+    fasta_files:  list of fasta filepaths
+    """
+    
+    for fp in fasta_files:
+        curr_filename = basename(fp)
+        if curr_filename not in fasta_name_to_sample_id.keys():
+            raise ValueError,("found %s, but %s not found in mapping file." %\
+             (fp, curr_filename))
+    
+    return True
---
+++ scripts/add_qiime_labels.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+from __future__ import division
+
+__author__ = "William Walters"
+__copyright__ = "Copyright 2011, The QIIME Project"
+__credits__ = ["William Walters"]
+__license__ = "GPL"
+__version__ = "1.4.0-dev"
+__maintainer__ = "William Walters"
+__email__ = "William.A.Walters@colorado.edu"
+__status__ = "Development"
+
+from os.path import isdir 
+
+from qiime.util import parse_command_line_parameters, get_options_lookup,\
+ make_option, create_dir
+from qiime.add_qiime_labels import add_qiime_labels
+
+
+options_lookup = get_options_lookup()
+script_info={}
+script_info['brief_description']="""Takes a directory and a mapping file of 
+SampleIDs to fasta file names, combines all files that have valid fasta 
+extensions into a single fasta file, with valid QIIME fasta labels."""
+script_info['script_description']="""A tab separated text file with SampleIDs 
+and fasta file names (just the file name itself, not the full or relative 
+filepath) is used to generate a combined fasta file with valid
+QIIME labels based upon the SampleIDs specified in the mapping file.
+
+This script is to handle situations where fasta data comes already 
+demultiplexed into a one fasta file per sample basis.  Apart from altering
+the fasta label to add a QIIME compatible label at the beginning (example:
+>FLP3FBN01ELBSX length=250 xy=1766_0111 region=1 run=R_2008_12_09_13_51_01_
+could become 
+>control.sample_1 FLP3FBN01ELBSX length=250 xy=1766_0111 region=1 run=R_2008_12_09_13_51_01_
+
+Note that limited checking is done on the mapping file.  The only tests
+are that every fasta file name is unique, and that SampleIDs are
+MIMARKS compliant (alphanumeric and period characters only).  Duplicate 
+SampleIDs are allowed, so care should be taken that there are no typos.
+
+No changes are made to the sequences.
+"""
+script_info['script_usage']=[]
+script_info['script_usage'].append(("""Example:""","""Specify fasta_dir as the input directory of fasta files, use the SampleID to fasta file mapping file example_mapping.txt, start enumerating with 1000000 following SampleIDs, and output the data to the directory combined_fasta""","""add_qiime_labels.py -i fasta_dir -m example_mapping.txt -n 1000000 -o combined_fasta"""))
+script_info['output_description']="""A combined_seqs.fasta file will be created in the output directory"""
+script_info['required_options']= [\
+    make_option('-m', '--mapping_fp',type='existing_filepath',
+                help='SampleID to fasta file name mapping file filepath'),
+    make_option('-i', '--fasta_dir',
+                help='Directory of fasta files to combine and label.')
+    
+]
+script_info['optional_options']= [\
+    make_option('-o', '--output_dir',
+        help='Required output directory for log file and corrected mapping '+\
+        'file, log file, and html file. [default: %default]', default="./"),
+    make_option('-n', '--count_start',
+        help='Specify the number to start enumerating sequence labels with. '+\
+        '[default: %default]', default=0, type="int")
+    ]
+        
+script_info['version'] = __version__
+
+def main():
+    option_parser, opts, args =\
+     parse_command_line_parameters(**script_info)
+      
+    mapping_fp = opts.mapping_fp
+    fasta_dir = opts.fasta_dir
+    output_dir = opts.output_dir
+    count_start = int(opts.count_start)
+
+    # Check input filepaths
+    try:
+        test_mapping_f = open(mapping_fp, "U")
+    except IOError:
+        raise IOError,("Cannot open mapping filepath "+\
+         "%s, please check filepath and permissions." % mapping_fp)
+         
+    if not isdir(fasta_dir):
+        raise IOError,("Specified fasta dir "+
+         "%s, does not exist" % fasta_dir)
+    
+    # Create output directory, check path/access to mapping file
+    create_dir(output_dir)
+    
+    add_qiime_labels(open(mapping_fp, "U"), fasta_dir, output_dir, count_start)
+
+
+if __name__ == "__main__":
+    main()
