849ccfdee7c732989963ed9349998fdb614348be
Greg Caporaso <gregcaporaso@gmail.com>
2012-06-12 21:09:20 +0000

    added ability to specify a subset of samples for core microbiome calculations

--- scripts/compute_core_microbiome.py
+++ scripts/compute_core_microbiome.py
@@ -17,11 +17,17 @@ from biom.exception import TableException
 from qiime.util import parse_command_line_parameters, make_option
 from qiime.format import format_biom_table
 from qiime.core_microbiome import filter_table_to_core
+from qiime.filter import sample_ids_from_metadata_description
 
 script_info = {}
 script_info['brief_description'] = "Identify the core microbiome."
 script_info['script_description'] = ""
-script_info['script_usage'] = [("","Identify the core OTUs in in1.biom, defined as the OTUs that are present in at least 50% of the samples. Write the list of core OTUs to a text file, and a new BIOM file containing only the core OTUs.","%prog -i otu_table.biom -o core_50.txt --fraction_for_core 0.5 --output_table_fp otu_table_core_50.biom")]
+script_info['script_usage'] = []
+script_info['script_usage'].append(("","Identify the core OTUs in otu_table.biom, defined as the OTUs that are present in at least 50% of the samples. Write the list of core OTUs to a text file, and a new BIOM file containing only the core OTUs.","%prog -i otu_table.biom -o core_50.txt --fraction_for_core 0.5 --output_table_fp otu_table_core_50.biom"))
+
+script_info['script_usage'].append(("","Identify the core OTUs in otu_table.biom, defined as the OTUs that are present in all of the samples in the 'Fast' treatment (as specified in the mapping file). Write the list of core OTUs to a text file.","%prog -i otu_table.biom -o fasting_core_100.txt --fraction_for_core 1.0 --mapping_fp map.txt --valid_states \"Treatment:Fast\""))
+
+
 script_info['output_description']= ""
 script_info['required_options'] = [\
  make_option('-i','--input_fp',type="existing_filepath",help='the input otu table in BIOM format'),
@@ -34,6 +40,10 @@ script_info['optional_options'] = [
              help='the otu table filtered to core otus only [default: %default]'),\
  make_option('--otu_md',default='taxonomy',
              help='the otu metadata category to write to the output file [defualt: %default]'),\
+ make_option('--mapping_fp',type='existing_filepath',
+  help='mapping file path (for use with --valid_states) [default: %default]'),
+ make_option('--valid_states',
+  help='description of sample ids to retain (for use with --mapping_fp) [default: %default]')
 ]
 script_info['version'] = __version__
 
@@ -48,21 +58,42 @@ def main():
     output_table_fp = opts.output_table_fp
     fraction_for_core = opts.fraction_for_core
     otu_md = opts.otu_md
-    
-    # get core across all samples
-    sample_ids = None
+    valid_states = opts.valid_states
+    mapping_fp = opts.mapping_fp
+
+    if valid_states and opts.mapping_fp:
+        sample_ids = sample_ids_from_metadata_description(open(mapping_fp,'U'),
+                                                          valid_states)
+        if len(sample_ids) < 1:
+            option_parser.error(\
+             "--valid_states pattern didn't match any entries in mapping file: \"%s\"" %\
+             valid_states)
+    else:
+        # get core across all samples if user doesn't specify a subset of the 
+        # samples to work with
+        sample_ids = None
     
     input_table = parse_biom_table(open(input_fp,'U'))
     output_f = open(output_fp,'w')
+    
+    sample_percentage_str = "%1.2f%%" % (fraction_for_core * 100.)
     
     try:
         core_table = filter_table_to_core(input_table,
                                           sample_ids,
                                           fraction_for_core)
     except TableException:
-        output_f.write("# No OTUs present in %1.2f%% of samples." % (fraction_for_core * 100.))
+        output_f.write("# No OTUs present in %s of samples." % sample_percentage_str)
         output_f.close()
         exit()
+    
+    # write some header information to file
+    if sample_ids == None:
+        output_f.write("# Core OTUs across %s of samples.\n" % sample_percentage_str)
+    else:
+        output_f.write(\
+         "# Core OTUs across %s of samples matching the sample metadata pattern \"%s\":\n# %s\n" %\
+          (sample_percentage_str, valid_states,' '.join(sample_ids)))
     
     # write the otu id and corresponding metadata for all core otus
     for value, id_, md in core_table.iterObservations():
