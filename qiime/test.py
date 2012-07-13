7c80b128b6d266f690a272eaa1e28b8f5140dc62
Greg Caporaso <gregcaporaso@gmail.com>
2012-07-13 02:00:22 +0000

    centralized code for timing out tests

--- qiime/test.py
+++ qiime/test.py
@@ -11,6 +11,7 @@ __maintainer__ = "Greg Caporaso"
 __email__ = "gregcaporaso@gmail.com"
 __status__ = "Development"
 
+import signal
 from os.path import isdir, split, join, abspath, exists
 from os import chdir, getcwd
 from shutil import copytree, rmtree
@@ -19,6 +20,36 @@ from site import addsitedir
 from sys import path
 from cogent.util.misc import remove_files
 from qiime.util import qiime_system_call
+
+
+### Code for timing out tests that exceed a time limit
+## The test case timing code included in this file is adapted from
+## recipes provided at:
+##  http://code.activestate.com/recipes/534115-function-timeout/
+##  http://stackoverflow.com/questions/492519/timeout-on-a-python-function-call
+
+# to use this, call initiate_timeout(allowed_seconds_per_test) in 
+# TestCase.setUp() and then disable_timeout() in TestCase.tearDown()
+
+class TimeExceededError(Exception):
+    pass
+
+def initiate_timeout(seconds=60):
+    
+    def timeout(signum, frame):
+        raise TimeExceededError,\
+         "Test failed to run in allowed time (%d seconds)." % seconds
+    
+    signal.signal(signal.SIGALRM, timeout)
+    # set the 'alarm' to go off in allowed_seconds seconds
+    signal.alarm(seconds)
+
+def disable_timeout():
+    # turn off the alarm
+    signal.alarm(0)
+
+### End code for timing out tests that exceed a time limit
+
 
 def run_script_usage_tests(qiime_test_data_dir,
                            qiime_scripts_dir,
