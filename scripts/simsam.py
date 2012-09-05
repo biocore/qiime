ee235325d4de65bd036f64be7b7010709b4c45ed
Justin Kuczynski <justinak@gmail.com>
2012-09-05 17:18:46 +0000

    added script_description for simam.py

--- scripts/simsam.py
+++ scripts/simsam.py
@@ -20,7 +20,14 @@ from biom.parse import parse_biom_table
 
 script_info = {}
 script_info['brief_description'] = "Simulate samples for each sample in an OTU table, using a phylogenetic tree."
-script_info['script_description'] = ""
+script_info['script_description'] = """ This script makes n samples related to each sample in an input otu table
+
+An input OTU table with 3 samples and n=2 will result in an output otu table with 6 samples total: 3 clusters of 2 related samples.
+
+To simulate each of new samples, this script uses a sample in the input OTU table, and for each OTU in that sample the script
+traverses rootward on the tree a distance specified by '-d' to a point x. It then randomly selects a tip that decends from x,
+(call that new tip 'o2'), and reassigns all observations of the original OTU to the tip/OTU 'o2'.
+"""
 script_info['script_usage'] = [("","Make 3 related sample for each sample in otu_table.biom.","%prog -i otu_table.biom -t rep_set.tre -o otu_table.simsam.biom -d .001 -n 3")]
 script_info['output_description']= "an otu table, samples are named: 'original_sample_0, original_sample_1 ...'"
 script_info['required_options'] = [
