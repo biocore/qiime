f3c0984beb3beb5333a93a8eba8ee1596ae441c4
Antonio Gonzalez <antgonza@gmail.com>
2012-09-24 02:31:41 +0000

    Fixing some errors in the documentation

--- scripts/distance_matrix_from_mapping.py
+++ scripts/distance_matrix_from_mapping.py
@@ -25,11 +25,11 @@ warnings.filterwarnings('ignore', 'Not using MPI as mpi4py not found')
 
 script_info={}
 script_info['brief_description']="""Calculate the pairwise dissimilarity on one column of a mappping file"""
-script_info['script_description']="""The input for this script is a mapping file and the name of a column from which a distance matrix will be created. The output of this script is a distance matrix containing a dissimilarity value for each pairwise comparison.
+script_info['script_description']="""The input for this script is a mapping file and the name of a column, it has to be numeric, from which a distance matrix will be created. The output of this script is a distance matrix containing a dissimilarity value for each pairwise comparison.
 
 As this is a univariate procedure only one metric is supported: d = c-b."""
 script_info['script_usage']=[]
-script_info['script_usage'].append(("""Pairwise dissimilarity:""","""To calculate the distance matrix (using e.g. euclidean distance) on a column of the mapping file, where the results are output to DOB_euclidean.txt, use the following command:""","""distance_matrix_from_mapping.py -i Fasting_Map.txt -c DOB -m euclidean -o distance_matrix/"""))
+script_info['script_usage'].append(("""Pairwise dissimilarity:""","""To calculate the distance matrix (using euclidean distance) on a column of the mapping file, where the results are output to DOB.txt, use the following command:""","""distance_matrix_from_mapping.py -i Fasting_Map.txt -c DOB"""))
 script_info['output_description']="""The output of distance_matrix_from_mapping.py is a file containing a distance matrix between rows corresponding to a column in a mapping file."""
 script_info['required_options']=[
  make_option('-i', '--input_path',
