6e4177d5118befadd4a7e7b0266d652c549838e3
Antonio Gonzalez <antgonza@gmail.com>
2012-10-16 21:51:44 -0600

    adding test data for make_qiime_py_file.py

---
+++ make_qiime_py_file/my_lib.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# File created on 16 Oct 2012
+from __future__ import division
+
+__author__ = "Greg Caporaso"
+__copyright__ = "Copyright 2011, The QIIME project"
+__credits__ = ["Greg Caporaso"]
+__license__ = "GPL"
+__version__ = "1.5.0-dev"
+__maintainer__ = "Greg Caporaso"
+__email__ = "gregcaporaso@gmail.com"
+__status__ = "Development"
+
