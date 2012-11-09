b021e7628639f2f00b553d3bcf54c08ee48da156
Yoshiki <yoshiki89@gmail.com>
2012-11-08 18:41:13 -0700

    Added, docstrings and cleaned up some code

--- qiime/add_alpha_to_mapping_file.py
+++ qiime/add_alpha_to_mapping_file.py
@@ -4,7 +4,7 @@ from __future__ import division
 
 __author__ = "Yoshiki Vazquez-Baeza"
 __copyright__ = "Copyright 2011, The QIIME project"
-__credits__ = ["Yoshiki Vazquez-Baeza"]
+__credits__ = ["Yoshiki Vazquez-Baeza", "Antonio Gonzalez-Pena"]
 __license__ = "GPL"
 __version__ = "1.5.0-dev"
 __maintainer__ = "Yoshiki Vazquez-Baeza"
@@ -16,20 +16,37 @@ from math import floor
 from numpy import array
 from copy import deepcopy
 
-def check_mapping_file_headers():
-    pass
-
-
-def add_alpha_diversity_values_to_mapping_file(alpha_metrics, alpha_sample_ids,\
+def add_alpha_diversity_values_to_mapping_file(metrics, alpha_sample_ids,\
                                             alpha_data, mapping_file_headers,\
-                                            mapping_file_data, number_of_bins,\
+                                            mapping_file_data, bins,\
                                             missing_value_name='N/A'):
-    """ """
+    """add 3 columns in the mapping file representing the alpha diversity data
 
+    Inputs:
+    metrics: list of alpha diversity metrics
+    alpha_sample_ids: list of sample identifiers in the alpha diversity data
+    alpha_data: alpha diversity data, as many columns as metrics and as many
+    rows as elments in alpha_sample_ids
+    mapping_file_headers: list of headers for the metadata mapping file
+    mapping_file_data: metadata mapping file data
+    bins: bins to classify the alpha diversity data
+    missing_value_name: string to place for the sample ids in the mapping file
+    but not in the alpha diversity data
+
+    Output:
+    mapping_file_headers: modified headers mapping file headers, including three
+    new columns per metric i. e. for chao1 the new fields would be:
+    'chao1_alpha', 'chao1_alpha_norm' and 'chao1_alpha_bin'
+    mapping_file_data: extended data including the alpha diversity values,
+    normalized values and bins
+
+    """
+
+    # data will be modified and returned so get your own copy
     new_mapping_file_data = deepcopy(mapping_file_data)
     new_mapping_file_headers = deepcopy(mapping_file_headers)
 
-    for index, metric in enumerate(alpha_metrics):
+    for index, metric in enumerate(metrics):
         # get the diversity value for this metric
         data = [[row[index]] for row in alpha_data]
         metric_max = max(data)[0]
@@ -44,7 +61,7 @@ def add_alpha_diversity_values_to_mapping_file(alpha_metrics, alpha_sample_ids,\
         for value in data:
             norm_value = (value[0]-metric_min)/(metric_max-metric_min)
             value.append(norm_value)
-            value.append(_get_level(norm_value, number_of_bins, 'bin'))
+            value.append(_get_level(norm_value, bins, 'bin'))
 
         # iterate using the mapping file instead of using the alpha diversity
         # data because more often that you will like you will have more samples
@@ -62,7 +79,20 @@ def add_alpha_diversity_values_to_mapping_file(alpha_metrics, alpha_sample_ids,\
     return new_mapping_file_data, new_mapping_file_headers
 
 def _get_level(value, levels, prefix=None):
-    """ """
+    """get a level from a value between 0 to 1; return a label or an integer
+
+    Input:
+    value: normalized value to assign a level to, must be between 0 and 1
+    levels: number of in which a value can be assigned to
+    prefix: a string prefix serves as a tagger
+
+    Output:
+    output: (str) if a prefix is provided a string of the form: prefix_2_of_5 is
+    returned, where 2 is the level assigned to the passed value and 5 is the
+    number of levels specified. (int) If a prefix is not provided the level of
+    the value is returned as an integer
+
+    """
     assert value <= 1, "The value cannot be greater than 1"
     assert value >= 0, "The value cannot be less than 0"
     assert levels > 0 and type(levels) is int, "The number of levels must be "+\
