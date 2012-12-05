d4f0789141faf0ebeff7d8af0c8efbabd6c70d6a
Greg Caporaso <gregcaporaso@gmail.com>
2012-12-05 09:33:16 -0700

    fixed typo

--- scripts/filter_taxa_from_otu_table.py
+++ scripts/filter_taxa_from_otu_table.py
@@ -18,7 +18,7 @@ from qiime.util import parse_command_line_parameters, make_option
 
 script_info = {}
 script_info['brief_description'] = "Filter taxa from an OTU table"
-script_info['script_description'] = "This scripts filters an OTU table based on taxonomic metata. It can be applied for positive filtering (i.e., keeping only certain taxa), negative filtering (i.e., discarding only certain taxa), or both at the same time."
+script_info['script_description'] = "This scripts filters an OTU table based on taxonomic metadata. It can be applied for positive filtering (i.e., keeping only certain taxa), negative filtering (i.e., discarding only certain taxa), or both at the same time."
 script_info['script_usage'] = []
 script_info['script_usage'].append(("","Filter otu_table.biom to include only OTUs identified as __Bacteroidetes or p__Firmicutes.","%prog -i otu_table.biom -o otu_table_bac_firm_only.biom -p p__Bacteroidetes,p__Firmicutes"))
 script_info['script_usage'].append(("","Filter otu_table.biom to exclude OTUs identified as p__Bacteroidetes or p__Firmicutes.","%prog -i otu_table.biom -o otu_table_non_bac_firm.biom -n p__Bacteroidetes,p__Firmicutes"))
