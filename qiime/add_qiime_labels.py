bee8d1845967382e756a81d143709110be8ecfd7
walterst <william.a.walters@gmail.com>
2012-12-18 13:42:58 -0700

    Modified add_qiime_labels.py script to use a standard metadata mapping file instead of custom mapping file to be more intuitive to use with the rest of QIIME.  Updated unit tests.

--- qiime/add_qiime_labels.py
+++ qiime/add_qiime_labels.py
@@ -11,87 +11,121 @@ __email__ = "William.A.Walters@colorado.edu"
 __status__ = "Development"
 
 from os.path import join, basename
-from glob import glob
 from string import letters, digits
 
 from cogent.parse.fasta import MinimalFastaParser
 
 from qiime.util import duplicates_indices
+from qiime.check_id_map import process_id_map
 
 def add_qiime_labels(mapping_f,
                      fasta_dir,
-                     output_dir="./",
+                     filename_column,
+                     output_dir=".",
                      count_start=0):
     """ Main function for combining fasta files, writing valid QIIME labels
     
-    mapping_f:  open file object of the SampleID to fasta file name.
-    fasta_dir:  Directory of fasta files to combine into a single file.
-    output_dir:  Directory to write output combined file to.
-    count_start:  Number to start enumeration of fasta labels with.
+    mapping_f:  open file object of the metadata mapping file
+    fasta_dir:  Directory of fasta files to combine into a single file
+    filename_column:  Column of metadata mapping file containing fasta filenames
+    output_dir:  Directory to write output combined file to
+    count_start:  Number to start enumeration of fasta labels with
     
     """
     
-    fasta_name_to_sample_id = check_mapping_data(mapping_f)
+    headers, mapping_data, run_description, errors, warnings= \
+        process_id_map(mapping_f, has_barcodes=False, \
+        disable_primer_check=True, added_demultiplex_field=None,
+        variable_len_barcodes=False)
     
-    fasta_files = get_fasta_fps(fasta_dir)
+    fasta_name_to_sample_id = check_mapping_data(mapping_data, headers,
+     filename_column)
     
-    check_fasta_fps(fasta_name_to_sample_id, fasta_files)
+    fasta_files = get_fasta_fps(fasta_dir, fasta_name_to_sample_id.keys())
     
     write_combined_fasta(fasta_name_to_sample_id, fasta_files, output_dir,
      counter=count_start)
     
-def check_mapping_data(mapping_data):
+def check_mapping_data(mapping_data, headers, filename_column):
     """ Checks mapping data for MIMARKS SampleIDs, unique IDs, fasta file names
     
     Also returns a dict of fasta file name: SampleID
     
     mapping_data:  list of lines of data from mapping file
+    headers: list of header strings
+    filename_column:  Column of metadata mapping file containing fasta filenames
     """
+    
+    # First make sure there is a SampleID and filename_column present
+    try:
+        sample_id_ix = headers.index("SampleID")
+    except ValueError:
+        raise ValueError,("SampleID column not found in mapping file, please "+\
+         "check mapping file with check_id_map.py")
+    
+    try:
+        filename_col_ix = headers.index(filename_column)
+    except ValueError:
+        raise ValueError,("Specified column %s not found in mapping file." %\
+         filename_column)
     
     valid_mimarks = letters + digits + "."
     
     fasta_name_to_sample_id = {}
 
     fasta_names = []
+    sample_ids = []
     for line in mapping_data:
-        curr_line = line.strip().split('\t')
-        if not curr_line or line.startswith("#"):
-            continue
+
         try:
-            fasta_name_to_sample_id[basename(curr_line[1].strip())] =\
-             curr_line[0]
+            fasta_name_to_sample_id[basename(line[filename_col_ix].strip())] =\
+             line[sample_id_ix]
         except IndexError:
-            raise IndexError,("Found non-tab separated line in mapping "+\
-             "data.  Offending line is: %s" % line)
-        for curr_char in curr_line[0]:
+            raise IndexError,("Missing filename column data in line %s " %\
+             line)
+
+        for curr_char in line[sample_id_ix]:
             if curr_char not in valid_mimarks:
                 raise ValueError,("Found invalid character in line: %s\n" %\
                  line + "SampleIDs must be alphanumeric and . characters "+\
                  "only")
-        fasta_names.append(curr_line[1].strip())
+        sample_ids.append(line[sample_id_ix].strip())
+        fasta_names.append(line[filename_col_ix].strip())
        
     fasta_name_dups = duplicates_indices(fasta_names)
     if fasta_name_dups:
         raise ValueError,("Found duplicate fasta names: %s" %\
          "\t".join([fasta_name for fasta_name in fasta_name_dups.keys()]))
          
+    sample_id_dups = duplicates_indices(sample_ids)
+    if sample_id_dups:
+        raise ValueError,("Found duplicate SampleID names: %s" %\
+         "\t".join([sample_id for sample_id in sample_id_dups.keys()]))
+         
     return fasta_name_to_sample_id
         
-def get_fasta_fps(fasta_dir):
+def get_fasta_fps(fasta_dir, fasta_files):
     """ Returns list of fasta filepaths (only .fna, .fasta, and .fa files)
     
     fasta_dir:  Directory of fasta files to check
+    fasta_files:  list of fasta filenames to open
     """
-    fasta_files = glob(fasta_dir + "/*.fna")
-    fasta_files.extend(glob(fasta_dir + "/*.fa"))
-    fasta_files.extend(glob(fasta_dir + "/*.fasta"))
-    fasta_files.extend(glob(fasta_dir + "/*.fas"))
+
+    fasta_filepaths = []
+    for curr_file in fasta_files:
+        curr_fp = join(fasta_dir, curr_file)
+        try:
+            file_test = open(curr_fp, "U")
+            file_test.close()
+        except IOError:
+            raise IOError,("Unable to open %s" % curr_fp)
+        fasta_filepaths.append(curr_fp)
     
-    return fasta_files
+    return fasta_filepaths
     
 def write_combined_fasta(fasta_name_to_sample_id,
                          fasta_files,
-                         output_dir="./",
+                         output_dir=".",
                          counter=0):
     """ Writes combined, enumerated fasta file
     
@@ -109,19 +143,3 @@ def write_combined_fasta(fasta_name_to_sample_id,
              (fasta_name_to_sample_id[basename(curr_fasta)], counter, label))
             combined_file_out.write("%s\n" % seq)
             counter += 1
-            
-def check_fasta_fps(fasta_name_to_sample_id,
-                    fasta_files):
-    """ Checks that all fasta files found match fasta names in mapping file
-    
-    fasta_name_to_sample_id:  dict of fasta file names to sample IDs
-    fasta_files:  list of fasta filepaths
-    """
-    
-    for fp in fasta_files:
-        curr_filename = basename(fp)
-        if curr_filename not in fasta_name_to_sample_id.keys():
-            raise ValueError,("found %s, but %s not found in mapping file." %\
-             (fp, curr_filename))
-    
-    return True
