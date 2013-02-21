35511b58a4a48d73bb6469475ee055736fb6a433
Greg Caporaso <gregcaporaso@gmail.com>
2013-02-20 19:42:01 -0700

    removed unused imports

--- tests/test_workflow/test_util.py
+++ tests/test_workflow/test_util.py
@@ -14,25 +14,14 @@ __status__ = "Development"
 import os
 from shutil import rmtree
 from glob import glob
-from tarfile import open as open_tarfile
-from os.path import join, exists, getsize, split, splitext, dirname
-from os import makedirs, system, chdir, getcwd
-from numpy import array, absolute
-from cogent import LoadTree, LoadSeqs
-from cogent.parse.fasta import MinimalFastaParser
+from os.path import exists, join, getsize
 from cogent.util.unit_test import TestCase, main
 from cogent.util.misc import remove_files
-from cogent.app.util import ApplicationNotFoundError
 from qiime.util import get_tmp_filename
-from cogent.parse.binary_sff import parse_binary_sff
 from qiime.util import (load_qiime_config, 
-                        count_seqs,
                         get_qiime_temp_dir,
                         create_dir)
-from qiime.parse import (parse_qiime_parameters,
-                         parse_distmat_to_dict,
-                         parse_distmat,
-                         parse_taxa_summary_table)
+from qiime.parse import (parse_qiime_parameters)
 from biom.parse import parse_biom_table
 from qiime.test import (initiate_timeout,
                         disable_timeout,
