6caf37856ff63264c29f65df20df1f731e0a9226
jose <jose@jose-K53SJ.(none)>
2013-03-05 18:05:35 -0700

    Forgot to modify the credits of the script

--- scripts/ampliconnoise.py
+++ scripts/ampliconnoise.py
@@ -4,7 +4,7 @@ from __future__ import division
 
 __author__ = "Justin Kuczynski"
 __copyright__ = "Copyright 2011, The QIIME Project"
-__credits__ = ["Justin Kuczynski"]
+__credits__ = ["Justin Kuczynski","Jose A. Navas Molina"]
 __license__ = "GPL"
 __version__ = "1.6.0-dev"
 __maintainer__ = "Justin Kuczynski"
