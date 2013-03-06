0463276cccccbb30cb3810ad5a5427cc6eca1dbb
jose <jose@jose-K53SJ.(none)>
2013-03-05 17:57:45 -0700

    Fixing the option types for ampliconnoise.py

--- scripts/ampliconnoise.py
+++ scripts/ampliconnoise.py
@@ -63,8 +63,8 @@ script_info['script_usage'] = [("","Run ampliconnoise, write output to anoise_ou
 script_info['output_description']= "a fasta file of sequences, with labels as:'>sample1_0' , '>sample1_1' ..."
 script_info['required_options'] = [
  options_lookup['mapping_fp'],
- make_option('-i','--sff_filepath',help='sff.txt filepath'),
- make_option('-o','--output_filepath',help='the output file'),
+ make_option('-i','--sff_filepath',type='existing_filepath',help='sff.txt filepath'),
+ make_option('-o','--output_filepath',type='new_filepath',help='the output file'),
 
 
 ]
