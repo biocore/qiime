71557497bdb240d6d0c7cee96101498e5de4f6cd
Greg Caporaso <gregcaporaso@gmail.com>
2013-03-08 10:08:31 -0700

    all links now open in new tab/window

--- qiime/workflow/core_diversity_analyses.py
+++ qiime/workflow/core_diversity_analyses.py
@@ -72,11 +72,11 @@ def get_index_page_header():
     return """<html>
 <head><title>QIIME results</title></head>
 <body>
-<a href="http://www.qiime.org"><img src=\"http://qiime.org/_static/wordpressheader.png\" alt="www.qiime.org""/></a><p>
+<a href="http://www.qiime.org" target="_blank"><img src=\"http://qiime.org/_static/wordpressheader.png\" alt="www.qiime.org""/></a><p>
 """
     
 def get_index_page_footer():
-    return """<p><b>Need help?</b><br>You can get answers to your questions on the <a href="http://forum.qiime.org">QIIME Forum</a>.<br>See the <a href="http://qiime.org/tutorials/index.html">QIIME tutorials</a> for examples of additional analyses that can be run.<br>You can find documentation of the QIIME scripts in the <a href="http://qiime.org/scripts/index.html">QIIME script index</a>.
+    return """<p><b>Need help?</b><br>You can get answers to your questions on the <a href="http://forum.qiime.org" target="_blank">QIIME Forum</a>.<br>See the <a href="http://qiime.org/tutorials/index.html" target="_blank">QIIME tutorials</a> for examples of additional analyses that can be run.<br>You can find documentation of the QIIME scripts in the <a href="http://qiime.org/scripts/index.html" target="_blank">QIIME script index</a>.
 </body></html>"""
 
 
