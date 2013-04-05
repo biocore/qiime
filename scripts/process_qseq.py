3473c1826d8072620faf5ee8eedb6dd99bfdd3b8
jose <jose@jose-K53SJ.(none)>
2013-04-04 18:43:06 -0600

    Fixing input_dir option type for process_qseq.py

--- scripts/process_qseq.py
+++ scripts/process_qseq.py
@@ -4,7 +4,7 @@ from __future__ import division
 
 __author__ = "Greg Caporaso"
 __copyright__ = "Copyright 2011, The QIIME project"
-__credits__ = ["Greg Caporaso"]
+__credits__ = ["Greg Caporaso", "Jose Antonio Navas Molina"]
 __license__ = "GPL"
 __version__ = "1.6.0-dev"
 __maintainer__ = "Greg Caporaso"
@@ -22,14 +22,21 @@ from qiime.format import illumina_data_to_fastq
 options_lookup = get_options_lookup()
 
 script_info = {}
-script_info['brief_description'] = "Given a directory of per-swath qseq files, this script generates a single fastq per lane."
+script_info['brief_description'] = "Given a directory of per-swath qseq files,\
+ this script generates a single fastq per lane."
 script_info['script_description'] = ""
-script_info['script_usage'] = [("","Generate fastq files from all lanes of read 1 data in the current directory.","process_qseq.py -i ./ -o ./fastq/ -r 1"),
-                               ("","Generate fastq files from all lanes of read 2 data in the current directory, truncating the sequences after the first 12 bases.","process_qseq.py -i ./ -o ./fastq/ -r 2 -b 12")]
+script_info['script_usage'] = [
+("","Generate fastq files from all lanes of read 1 data in the current\
+ directory.","process_qseq.py -i ./ -o ./fastq/ -r 1"),
+("","Generate fastq files from all lanes of read 2 data in the current\
+ directory, truncating the sequences after the first 12 bases.",
+"process_qseq.py -i ./ -o ./fastq/ -r 2 -b 12")]
 script_info['output_description']= ""
 script_info['required_options'] = [
- make_option('-i','--input_dir',type='new_dirpath',help='the input directory'),
- make_option('-o','--output_dir',type='new_dirpath',help='the output directory'),
+ make_option('-i','--input_dir',type='existing_dirpath',
+  help='the input directory'),
+ make_option('-o','--output_dir',type='new_dirpath',
+  help='the output directory'),
  make_option('-r','--read',help='the read number to consider',type='int')
 ]
 script_info['optional_options'] = [
@@ -37,11 +44,12 @@ script_info['optional_options'] = [
   help='the lane numbers to consider, comma-separated [defaut: %default]',
   default='1,2,3,4,5,6,7,8'),
  make_option('-b','--bases',type='int',
-  help='the number of bases to include (useful for slicing a barcode) [defaut: all]',
+  help='the number of bases to include (useful for slicing a barcode)\
+ [defaut: all]',
   default=None),
  make_option('--ignore_pass_filter',action='store_true',default=False,
-  help='ignore the illumina pass filter [default:%default; reads with 0 in pass '+\
-       ' filter field are discarded]')
+  help='ignore the illumina pass filter [default:%default; reads with 0 in '+\
+       ' pass filter field are discarded]')
 ]
 script_info['version'] = __version__
 
@@ -74,7 +82,7 @@ def main():
         for read1_fp in read1_fps:        
             for record in iter_split_lines(open(read1_fp,'U')):
                 fastq_s, pass_filter = illumina_data_to_fastq(record,
-                                                              number_of_bases=bases)
+                                                        number_of_bases=bases)
                 if ignore_pass_filter or pass_filter != 0:
                     output_f.write('%s\n' % fastq_s)
         output_f.close()
