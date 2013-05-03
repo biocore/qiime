c20570fe1186a6de8fe3c0fe7ee1ada1d5973cf3
Gregory Ditzler <gregory.ditzler@gmail.com>
2013-05-03 11:31:24 -0400

    Added script info to fizzy, fixed CLI calls, and fixed the types in the make_option. The current implementation has been successfully tested from the commandline and as a component of QIIME being used in a script (see /tests/test_fizzy.py).

--- scripts/fizzy.py
+++ scripts/fizzy.py
@@ -16,21 +16,49 @@ __status__ = "Release"
 from qiime.util import parse_command_line_parameters, make_option
 import qiime.fizzy as fizzy 
 
+
 script_info = {}
-script_info['brief_description'] = ""
-script_info['script_description'] = ""
-script_info['script_usage'] = [("","","")]
-script_info['output_description']= ""
+script_info['brief_description'] = """Run feature selection on aubundance data \
+	contained in a Biom file."""
+script_info['script_description'] ="""This script will run a feature selection 
+	algorithm on abundance data contained in a Biom file given a label file, which \
+	is stored in tab-delimited format. The current feature selection methods uses \
+	a forward search algorithm to select the features. The objective functions are \
+	based on information theory. At the moment, users are limited to the objective \
+	functions implemented in the PyFeast feature selection module. """
+script_info['script_usage'] = [(\
+	"""Run JMI feature selection on a Biom file:""",\
+	"""To perform feature selection the biom file and TSV file must be specified \
+	in advance. Here we use JMI and select 15 features. """,\
+	"""%prog -i data.biom -c label.tsv -f jmi -k 15""")]
+script_info['output_description']= """Text file containing the top features \
+	selected by the algorithm. """
+
 script_info['required_options'] = [
 	# Example required option
-	make_option('-i','--input_file',type="existing_file",help='input sparse biome file'),\
-	make_option('-c','--label_file',type="existing_file",help='csv file with labeling scheme')
+	make_option('-i', \
+		'--input_path',\
+		type="existing_path",
+		help='input sparse biome file'),\
+	make_option('-c',\
+		'--label_path',\
+		type="existing_path",\
+		help='csv file with labeling scheme')
 ]
 script_info['optional_options'] = [
 	# Example optional option
-	make_option('-o','--output_file',type="new_dirpath",help='the output directory [default: %default]'),\
-	make_option('-k','--n_select',type="feature2select",help='number of feature to select [default: 15]'),\
-	make_option('-f','--fs_method',type="alg_parameter",help='feature selection method [options: jmi, mrmr, mim, mifs]')
+	make_option('-o',\
+		'--output_path',\
+		type="new_dirpath",\
+		help='the output directory [default: %default]'),\
+	make_option('-k',\
+		'--n_select',\
+		type="int",\
+		help='number of feature to select [default: 15]'),\
+	make_option('-f',\
+		'--fs_method',\
+		type="string",\
+		help='feature selection method [options: jmi, mrmr, mim, mifs]')
 ]
 script_info['version'] = __version__
 
@@ -49,20 +77,20 @@ def main():
 	# check to see if the user has see the output file. if the output file
 	# has not been set then we will make a generic file with a "meaningless"
 	# file name
-	if not opts.output_file:
+	if not opts.output_path:
 		out_fmt = 'output.txt'
 	else:
-		out_fmt = opts.output_file
+		out_fmt = opts.output_path
 
 	if not opts.n_select:
 		n_select = 15
 	else:
-		n_select = opts.n_select
+		n_select = int(opts.n_select)
 
 	# run the fizzy feature selection routine
 	fizzy.run_feature_selection( \
-		open(opts.input_file,'U'), \
-		open(opts.label_file,'U'), \
+		open(opts.input_path,'U'), \
+		open(opts.label_path,'U'), \
 		out_fmt, \
 		fs_method, \
 		n_select)
