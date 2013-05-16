0d4f0d8e3706d5aa0252554bfeef782282185104
Gregory Ditzler <gregory.ditzler@gmail.com>
2013-05-16 14:41:26 -0400

    fixed typo in fizzy script.

--- scripts/fizzy.py
+++ scripts/fizzy.py
@@ -98,7 +98,7 @@ def main():
 	fizzy.run_feature_selection( \
 		open(opts.input_path,'U'), \
 		open(opts.label_path,'U'), \
-		opts.column_label \
+		opts.column_label, \
 		out_fmt, \
 		fs_method, \
 		n_select)
