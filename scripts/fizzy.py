377219034bb9b6803fcaa8a660b4c6b5fd0c48e3
Gregory Ditzler <gregory.ditzler@gmail.com>
2013-05-23 14:27:20 -0400

    fixed documentation for fizzy call.

--- scripts/fizzy.py
+++ scripts/fizzy.py
@@ -30,7 +30,7 @@ script_info['script_usage'] = [(
 	"""To perform feature selection the biom file, map file must 
 	be specified in advance, and the label column in the map file. 
 	Here we use JMI and select 15 features. """,
-	"""%prog -i data.biom -m map.txt -c Class_Column -f jmi -k 15""")]
+	"""%prog -i data.biom -m map.txt -c Class -f jmi -k 15""")]
 script_info['output_description']= """Text file containing the top features 
 	selected by the algorithm. """
 
@@ -39,34 +39,34 @@ script_info['output_description']= """Text file containing the top features
 # that we can set some default parameters to run a feature selection 
 # algorithm.
 script_info['required_options'] = [
-	make_option('-i', \
-		'--input_path',\
+	make_option('-i', 
+		'--input_path',
 		type="existing_filepath",
-		help='input sparse biome file'),\
-	make_option('-m',\
-		'--map_path',\
-		type="existing_filepath",\
+		help='input sparse biome file'),
+	make_option('-m',
+		'--map_path',
+		type="existing_filepath",
 		help='map file with labeling scheme'),
-	make_option('-c',\
-		'--column_label',\
-		type="string",\
+	make_option('-c',
+		'--column_label',
+		type="string",
 		help='column indicating the labels in the map file.')
 ]
 script_info['optional_options'] = [
-	make_option('-o',\
-		'--output_path',\
-		type="new_dirpath",\
-		help='the output directory [default: %default]',\
-		default='output.txt'),\
-	make_option('-k',\
-		'--n_select',\
-		type="int",\
-		help='number of feature to select [default: %default]',\
-		default=15),\
-	make_option('-f',\
-		'--fs_method', \
-		type='choice', \
-		help='feature selection method. valid options are ' \
+	make_option('-o',
+		'--output_path',
+		type="new_dirpath",
+		help='the output directory [default: %default]',
+		default='output.txt'),
+	make_option('-k',
+		'--n_select',
+		type="int",
+		help='number of feature to select [default: %default]',
+		default=15),
+	make_option('-f',
+		'--fs_method',
+		type='choice', 
+		help='feature selection method. valid options are ' 
 		+ ', '.join(fizzy.get_fs_methods()) + '. [default: %default]',
 		choices=fizzy.get_fs_methods(),
 		default='MIM')
