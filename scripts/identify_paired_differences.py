54efea75d27dcf6c43a590909d0e43557b29df9d
Greg Caporaso <gregcaporaso@gmail.com>
2013-07-09 20:37:13 -0700

    updated header information

--- scripts/identify_paired_differences.py
+++ scripts/identify_paired_differences.py
@@ -3,10 +3,10 @@
 from __future__ import division
 
 __author__ = "Greg Caporaso"
-__copyright__ = "Copyright 2013, The BiPy project"
+__copyright__ = "Copyright 2013, The QIIME project"
 __credits__ = ["Greg Caporaso"]
 __license__ = "GPL"
-__version__ = "0.0.0"
+__version__ = "1.7.0-dev"
 __maintainer__ = "Greg Caporaso"
 __email__ = "gregcaporaso@gmail.com"
 __status__ = "Development"
