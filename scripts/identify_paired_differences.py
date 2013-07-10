23879668a7c39c3849931666d8f6ef6ab019c307
Greg Caporaso <gregcaporaso@gmail.com>
2013-07-10 11:27:04 -0700

    added ability to run analyses on a subset of the samples

--- scripts/identify_paired_differences.py
+++ scripts/identify_paired_differences.py
@@ -24,6 +24,8 @@ from qiime.parse import (extract_per_individual_state_metadata_from_mapping_f,
  extract_per_individual_state_metadata_from_mapping_f_and_biom)
 from qiime.util import (parse_command_line_parameters, 
                   make_option)
+from qiime.filter import (filter_mapping_file_from_mapping_f,
+                          sample_ids_from_metadata_description)
 
 script_info = {}
 script_info['brief_description'] = "Generate plots which illustrate the change in some data point(s) with a state change on a per-individual basis."
@@ -32,6 +34,7 @@ script_info['script_usage'] = []
 script_info['script_usage'].append(("Generate plots for two categories from the mapping file.","","%prog -m map.txt --metadata_categories 'Streptococcus Abundance,Veillonella Abundance' --state_category TreatmentState --state_values Pre,Post --individual_id_category PersonalID -o taxa_results"))
 script_info['script_usage'].append(("Generate plots for four categories from the mapping file, where the y-axes should be set on a per-plot basis.","","%prog -m map.txt --metadata_categories 'Streptococcus Abundance,Veillonella Abundance,Phylogenetic Diversity,Observed OTUs' --state_category TreatmentState --state_values Pre,Post --individual_id_category PersonalID -o taxa_and_alpha_results --suppress_share_y_axis"))
 script_info['script_usage'].append(("Generate plots for all observations in a biom file, where the y-axes should be set on a per-plot basis.","","%prog -m map.txt -b otu_table.biom --state_category TreatmentState --state_values Pre,Post --individual_id_category PersonalID -o otu_results --suppress_share_y_axis"))
+script_info['script_usage'].append(("Generate plots for all observations in a biom file, where the y-axes should be set on a per-plot basis, and only including samples from individuals whose 'TreatmentResponse' was 'Improved' (as defined in the mapping file).","","%prog -m map.txt -b otu_table.biom --state_category TreatmentState --state_values Pre,Post --individual_id_category PersonalID -o otu_results_improved_only --suppress_share_y_axis --valid_states TreatmentResponse:Improved"))
 
 script_info['output_description']= ""
 
@@ -47,13 +50,11 @@ script_info['optional_options'] = [
  make_option('--suppress_share_y_axis',default=False,action='store_true',help='set the scale of the y-axes will be set on a per-plot basis, rather than made consistent across all subplots [default: %default]'),
   make_option('--metadata_categories',help='ordered list of the mapping file column names to test for paired differences (usually something like "StreptococcusAbundance,Phylogenetic Diversity") [default: %default]',default=None),
   make_option('--observation_ids',help='ordered list of the observation ids to test for paired differences if a biom table is provided (usually something like "otu1,otu2") [default: compute paired differences for all observation ids]',default=None),
-  make_option('-b','--biom_table_fp',help='path to biom table to use for computing paired differences [default: %default]', default=None)
-  
- ]
+  make_option('-b','--biom_table_fp',help='path to biom table to use for computing paired differences [default: %default]', default=None),
+  make_option('-s','--valid_states', help="string describing valid samples that should be included based on their metadata (e.g. 'TreatmentResponse:Improved') [default: %default]",default=None),
+]
 
 script_info['version'] = __version__
-
-
 
 def main():
     option_parser, opts, args =\
@@ -67,6 +68,7 @@ def main():
     output_dir = opts.output_dir
     biom_table_fp = opts.biom_table_fp
     observation_ids = opts.observation_ids
+    valid_states = opts.valid_states
     
     if metadata_categories and biom_table_fp:
         option_parser.error("Can only pass --metadata_categories or --biom_table_fp, not both.")
@@ -78,12 +80,18 @@ def main():
     if len(state_values) != 2:
         option_parser.error("Exactly two state_values must be passed separated by a comma.")
     
+    mapping_f = list(open(mapping_fp,'U'))
+    if valid_states:
+        sample_ids_to_keep = sample_ids_from_metadata_description(
+                              mapping_f,valid_states)
+        mapping_f = filter_mapping_file_from_mapping_f(mapping_f,sample_ids_to_keep).split('\n')
+    
     if biom_table_fp:
         biom_table = parse_biom_table(open(biom_table_fp,'U'))
         metadata_categories = observation_ids or biom_table.ObservationIds
         personal_ids_to_state_metadata = \
          extract_per_individual_state_metadata_from_mapping_f_and_biom(
-                                     open(mapping_fp,'U'),
+                                     mapping_f,
                                      biom_table,
                                      state_category,
                                      state_values,
@@ -93,7 +101,7 @@ def main():
         metadata_categories = metadata_categories.split(',')
         personal_ids_to_state_metadata = \
          extract_per_individual_state_metadata_from_mapping_f(
-                                     open(mapping_fp,'U'),
+                                     mapping_f,
                                      state_category,
                                      state_values,
                                      individual_id_category,
@@ -121,11 +129,11 @@ def main():
     
     paired_difference_output_fp = join(opts.output_dir,'paired_difference_comparisons.txt')
     paired_difference_output_f = open(paired_difference_output_fp,'w')
-    paired_difference_output_f.write("#Metadata category\tMean difference\tMedian difference\tt one sample\tt one sample parametric p-value\tt one sample parametric p-value (Bonferroni-corrected)\n")
-    
+    paired_difference_output_f.write("#Metadata category\tNum differences (i.e., n)\nMean difference\tMedian difference\tt one sample\tt one sample parametric p-value\tt one sample parametric p-value (Bonferroni-corrected)\n")
+    paired_difference_results = []
     plot_output_fp = join(opts.output_dir,'plots.pdf')
 
-    
+
     for category_number, metadata_category in enumerate(metadata_categories):
         personal_ids_to_state_metadatum = personal_ids_to_state_metadata[metadata_category]
 
@@ -163,13 +171,13 @@ def main():
         t = t_one_sample_results[0]
         p_value = t_one_sample_results[1]
         bonferroni_p_value = min([p_value * num_metadata_categories,1.0])
-        paired_difference_output_f.write('\t'.join([metadata_category,
-                                        str(mean(differences)),
-                                        str(median(differences)),
-                                        str(t),
-                                        str(p_value),
-                                        str(bonferroni_p_value)]))
-        paired_difference_output_f.write('\n')
+        paired_difference_results.append([metadata_category,
+                                        len(differences),
+                                        mean(differences),
+                                        median(differences),
+                                        t,
+                                        p_value,
+                                        bonferroni_p_value])
         
         # Finalize plot for current metadata category
         current_subplot.set_title(metadata_category,size=8)
@@ -177,6 +185,12 @@ def main():
         current_subplot.set_xticklabels(state_values,size=6)
     
     fig.savefig(plot_output_fp)
+    # sort output by absolute value of t (largest to smallest)
+    paired_difference_results.sort(key=lambda x: abs(x[3]))
+    paired_difference_results.reverse()
+    for r in paired_difference_results:
+        paired_difference_output_f.write('\t'.join(map(str,r)))
+        paired_difference_output_f.write('\n')
     paired_difference_output_f.close()
         
 
