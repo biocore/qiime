85a2ba2fdd0853a6c7c68f7ee6018a7856bdd19c
Gregory Ditzler <gregory.ditzler@gmail.com>
2013-10-07 10:31:28 -0400

    converted 2 space indentation to 4 spaces

--- qiime/fizzy.py
+++ qiime/fizzy.py
@@ -17,122 +17,122 @@ __status__ = "Development"
 
 
 def get_fs_methods():
-	"""
-		get_fs_methods()
-		return the feature selection methods that are 
-		available for use in a list. note that the options
-		are case sensitive. 
-	"""
-	return ['CIFE','CMIM','CondMI','Condred','ICAP','JMI','MIM','MIFS','mRMR']
+    """
+        get_fs_methods()
+        return the feature selection methods that are 
+        available for use in a list. note that the options
+        are case sensitive. 
+    """
+    return ['CIFE','CMIM','CondMI','Condred','ICAP','JMI','MIM','MIFS','mRMR']
 
 def parse_biom(fname): 
-	"""
-		parse_biom(fname)
-		@fname - this is the file handle. Ex. Use something like:
-			data, features = parse_biom(open('file.biom','U'))
-		@data_matrix (return) - dense matrix for feature selection
-		@variable_names (return) - feature names in a list
-		@observation_names (return) - names of the samples in the 
-			database found in fname. 
-	"""
-	biom_table = parse_biom_table(fname)
-	observation_names = list(biom_table.SampleIds)
-	variable_names = list(biom_table.ObservationIds)
-	data_matrix = []
-	for data in biom_table.iterObservationData():
-		data_matrix.append(data)
-	data_matrix = numpy.array(data_matrix)
+    """
+        parse_biom(fname)
+        @fname - this is the file handle. Ex. Use something like:
+            data, features = parse_biom(open('file.biom','U'))
+        @data_matrix (return) - dense matrix for feature selection
+        @variable_names (return) - feature names in a list
+        @observation_names (return) - names of the samples in the 
+            database found in fname. 
+    """
+    biom_table = parse_biom_table(fname)
+    observation_names = list(biom_table.SampleIds)
+    variable_names = list(biom_table.ObservationIds)
+    data_matrix = []
+    for data in biom_table.iterObservationData():
+        data_matrix.append(data)
+    data_matrix = numpy.array(data_matrix)
 
-	return data_matrix.transpose(), variable_names, observation_names
+    return data_matrix.transpose(), variable_names, observation_names
 
 
 def parse_map_file(fname, column_name, observation_names):
-	"""
-		parse_map_file(fname)
-		@fname - file handle
-		@column_name - name of the column that contains the class 
-			labels
-		@observation_names - names of the observations in the order
-			of which they appear in the data set. 
-		@labels - numpy array of class labels
-	"""
-	from qiime.parse import parse_mapping_file_to_dict
-	obj, comm = parse_mapping_file_to_dict(fname)
-	label_full = []
-	labels = []
+    """
+        parse_map_file(fname)
+        @fname - file handle
+        @column_name - name of the column that contains the class 
+            labels
+        @observation_names - names of the observations in the order
+            of which they appear in the data set. 
+        @labels - numpy array of class labels
+    """
+    from qiime.parse import parse_mapping_file_to_dict
+    obj, comm = parse_mapping_file_to_dict(fname)
+    label_full = []
+    labels = []
 
-	# grab the class labels which are likely to be in string 
-	# format. 
+    # grab the class labels which are likely to be in string 
+    # format. 
 
-	for id_set in observation_names:
-		if(id_set not in obj):
-			raise ValueError("Unknown observation name supplied. Make sure that the observation name is in map file you specified")
-		if(column_name not in obj[id_set]):
-			raise ValueError("Unknown class name supplied. Make sure that the column name is in map file you specified")
-		label_full.append(obj[id_set][column_name])
-	
-  # now that we have the full class labels we need to determine
-	# the number of unique classes in the the data. if the number of
-	# classes is equal to the number of obervations, throw an error. 
-	# its likely the user does not know what they are doing. 
-	unique_classes = numpy.unique(label_full)
-	if len(unique_classes) == len(observation_names):
-		raise ValueError("Number of classes is equal to the number of observations.  The number of classes must be less than the number of observations in map file that was specified.")
+    for id_set in observation_names:
+        if(id_set not in obj):
+            raise ValueError("Unknown observation name supplied. Make sure that the observation name is in map file you specified")
+        if(column_name not in obj[id_set]):
+            raise ValueError("Unknown class name supplied. Make sure that the column name is in map file you specified")
+        label_full.append(obj[id_set][column_name])
+    
+    # now that we have the full class labels we need to determine
+    # the number of unique classes in the the data. if the number of
+    # classes is equal to the number of obervations, throw an error. 
+    # its likely the user does not know what they are doing. 
+    unique_classes = numpy.unique(label_full)
+    if len(unique_classes) == len(observation_names):
+        raise ValueError("Number of classes is equal to the number of observations.  The number of classes must be less than the number of observations in map file that was specified.")
 
-	for str_lab in label_full:
-		for n,uclass in enumerate(unique_classes):
-			if str_lab == uclass:
-				labels.append(float(n))
-				break
-	return numpy.array(labels)
+    for str_lab in label_full:
+        for n,uclass in enumerate(unique_classes):
+            if str_lab == uclass:
+                labels.append(float(n))
+                break
+    return numpy.array(labels)
 
 
 def run_pyfeast(data, labels, features, method='MIM', n_select=15):
-	"""
-		run_pyfeast(data, labels, method)
-		@data - numpy data (dense)
-		@labels - vector of class labels (discrete)
-		@method - feature selection method
+    """
+        run_pyfeast(data, labels, method)
+        @data - numpy data (dense)
+        @labels - vector of class labels (discrete)
+        @method - feature selection method
 
-		The feature selection method is based off of the FEAST 
-		C variable selection toolbox. 
+        The feature selection method is based off of the FEAST 
+        C variable selection toolbox. 
 
-		Reference:
-		Gavin Brown, Adam Pocock, Ming-Jie Zhao, and Mikel Lujan, 
-			"Conditional Likelihood Maximisation: A Unifying Framework 
-			for Information Theoretic Feature Selection," Journal of 
-			Machine Learning Research, vol. 13, pp. 27--66, 2012.
-			(http://jmlr.csail.mit.edu/papers/v13/brown12a.html)
-	"""
-	
-	try:
-		import feast
-	except ImportError:
-		raise ApplicationNotFoundError("Error loading the PyFeast module. It is likely that you do not have PyFeast installed locally.")
+        Reference:
+        Gavin Brown, Adam Pocock, Ming-Jie Zhao, and Mikel Lujan, 
+            "Conditional Likelihood Maximisation: A Unifying Framework 
+            for Information Theoretic Feature Selection," Journal of 
+            Machine Learning Research, vol. 13, pp. 27--66, 2012.
+            (http://jmlr.csail.mit.edu/papers/v13/brown12a.html)
+    """
+    
+    try:
+        import feast
+    except ImportError:
+        raise ApplicationNotFoundError("Error loading the PyFeast module. It is likely that you do not have PyFeast installed locally.")
 
-	try:
-		fs_method = getattr(feast, method)
-	except AttributeError:
-		raise AttributeError("Unknown feature selection method is being specified for PyFeast. Make sure the feature selection method being selected is a valid one. ")
+    try:
+        fs_method = getattr(feast, method)
+    except AttributeError:
+        raise AttributeError("Unknown feature selection method is being specified for PyFeast. Make sure the feature selection method being selected is a valid one. ")
 
-	sf = fs_method(data, labels, n_select)
-	reduced_set = []
-	for k in range(len(sf)):
-		reduced_set.append(features[int(sf[k])])
-	return reduced_set
+    sf = fs_method(data, labels, n_select)
+    reduced_set = []
+    for k in range(len(sf)):
+        reduced_set.append(features[int(sf[k])])
+    return reduced_set
 
 
 def run_feature_selection(file_biom, file_map, column_name, method='MIM', n_select=15):
-	"""
-		run_feature_selection(fname_biom, fname_csv, method)
-		@file_biom - handle of the biom file
-		@file_map - handle of the csv file
-		@column_name - column name containing the class labels found 
-			in the map file. 
-		@method - feature selection method [see PyFeast docs]
-		@n_select - number of features to selectio (integer)
-	"""
-	data_matrix, variable_names, observation_names = parse_biom(file_biom)
-	label_vector = parse_map_file(file_map, column_name, observation_names)
-	reduced_set = run_pyfeast(data_matrix, label_vector, variable_names, method, n_select)
-	return reduced_set 
+    """
+        run_feature_selection(fname_biom, fname_csv, method)
+        @file_biom - handle of the biom file
+        @file_map - handle of the csv file
+        @column_name - column name containing the class labels found 
+            in the map file. 
+        @method - feature selection method [see PyFeast docs]
+        @n_select - number of features to selectio (integer)
+    """
+    data_matrix, variable_names, observation_names = parse_biom(file_biom)
+    label_vector = parse_map_file(file_map, column_name, observation_names)
+    reduced_set = run_pyfeast(data_matrix, label_vector, variable_names, method, n_select)
+    return reduced_set 
--- tests/test_fizzy.py
+++ tests/test_fizzy.py
@@ -15,122 +15,122 @@ import qiime.fizzy as fizzy
 import sys, os 
 from cogent.util.unit_test import TestCase, main
 from qiime.util import (get_qiime_scripts_dir,
-	load_qiime_config,
-	get_tmp_filename,
-	get_qiime_temp_dir,
-	create_dir)
+    load_qiime_config,
+    get_tmp_filename,
+    get_qiime_temp_dir,
+    create_dir)
 import shutil
 import StringIO
 
 class FizzyTests(TestCase):
 
-	def setUp(self):
-		"""
-			setup function defined by the bioler plate qiime 
-			test functions. 
-		"""
-		self.biom_file_handle = StringIO.StringIO(biom_file_string)
-		self.map_file_handle = StringIO.StringIO(map_file_string)
+    def setUp(self):
+        """
+            setup function defined by the bioler plate qiime 
+            test functions. 
+        """
+        self.biom_file_handle = StringIO.StringIO(biom_file_string)
+        self.map_file_handle = StringIO.StringIO(map_file_string)
 
 
-	def uniform_data(self, n_observations, n_features, n_select):
-		""" Generate some uniform data to use for test_pyfeast_run """
-		import numpy as np
-		xmax = 10
-		xmin = 1
+    def uniform_data(self, n_observations, n_features, n_select):
+        """ Generate some uniform data to use for test_pyfeast_run """
+        import numpy as np
+        xmax = 10
+        xmin = 1
 
-		data = 1.0*np.random.randint(xmax + 1, size = (n_features, n_observations))
-		labels = np.zeros(n_observations)
-		delta = n_select * (xmax - xmin) / 2.0
+        data = 1.0*np.random.randint(xmax + 1, size = (n_features, n_observations))
+        labels = np.zeros(n_observations)
+        delta = n_select * (xmax - xmin) / 2.0
 
-		for m in range(n_observations):
-			zz = 0.0
-			for k in range(n_select):
-				zz += data[k, m]
-				if zz > delta:
-					labels[m] = 1
-				else:
-					labels[m] = 2
-		data = data.transpose()
-		return data, labels
+        for m in range(n_observations):
+            zz = 0.0
+            for k in range(n_select):
+                zz += data[k, m]
+                if zz > delta:
+                    labels[m] = 1
+                else:
+                    labels[m] = 2
+        data = data.transpose()
+        return data, labels
 
 
-	def test_get_fs_methods(self):
-		"""
-			this test is going to make sure that we are only 
-			implementing feature selection methods that are in 
-			a predefined list. 
-		"""
-		self.assertEqual(fizzy.get_fs_methods(), information_theoretic_methods)
+    def test_get_fs_methods(self):
+        """
+            this test is going to make sure that we are only 
+            implementing feature selection methods that are in 
+            a predefined list. 
+        """
+        self.assertEqual(fizzy.get_fs_methods(), information_theoretic_methods)
 
-	def test_parse_biom(self):
-		"""
+    def test_parse_biom(self):
+        """
       this test is going to ensure that we can properly parse our biom file
-		"""
-		correct_biom = ([[  1.,   0.,   1.,   6.], [  0.,   5.,   1.,  10.], [  4.,   7.,   9.,   8.]], [u'OTU0', u'OTU1', u'OTU2', u'OTU3'], [u'ID0', u'ID1', u'ID2'])
+        """
+        correct_biom = ([[  1.,   0.,   1.,   6.], [  0.,   5.,   1.,  10.], [  4.,   7.,   9.,   8.]], [u'OTU0', u'OTU1', u'OTU2', u'OTU3'], [u'ID0', u'ID1', u'ID2'])
 
-		parsed_biom = fizzy.parse_biom(self.biom_file_handle)
-		self.assertEqual(parsed_biom, correct_biom)
-			
-	
-	def test_parse_map_file(self):
-		"""
-			this test ensures that the map file is proerply parsed
-		"""
+        parsed_biom = fizzy.parse_biom(self.biom_file_handle)
+        self.assertEqual(parsed_biom, correct_biom)
+            
+    
+    def test_parse_map_file(self):
+        """
+            this test ensures that the map file is proerply parsed
+        """
 
-		correct_map = [0,1.,1.]
+        correct_map = [0,1.,1.]
 
-		parsed_map = fizzy.parse_map_file(self.map_file_handle, "Class", [u'ID0',u'ID1',u'ID2'])
+        parsed_map = fizzy.parse_map_file(self.map_file_handle, "Class", [u'ID0',u'ID1',u'ID2'])
 
-		self.assertEqual(parsed_map, correct_map)
-
-
-	def test_parse_map_invalid_column_name(self):
-
- 		self.assertRaises(ValueError, fizzy.parse_map_file, self.map_file_handle, "Unknown", [u'ID0',u'ID1',u'ID2'])
-
-	def test_parse_map_invalid_sample_name(self):
-
- 		self.assertRaises(ValueError, fizzy.parse_map_file, self.map_file_handle, "Class", [u'ID99',u'ID1',u'ID2'])
-
-	def test_parse_map_invalid_equal_num_sample_classes(self):
-
-	 		self.assertRaises(ValueError, fizzy.parse_map_file, self.map_file_handle, "Class", [u'ID0',u'ID1'])
-			
-	def test_run_pyfeast(self):
-		"""
-			test the run_pyfeast function by creating some uniform data, and running
-			the MIM feature selection method.
-
-			Assert that each of the selected features are in the appropriate range
-		"""
-		n_select = 5
-		(data, labels)  = self.uniform_data(100, 50, n_select)
-		variable_names = labels # we don't need this to  be differenttest pyfeast, so just give it the same sized vector
-		
-		selected_features = fizzy.run_pyfeast(data, labels, variable_names, "MIM", n_select)
-		selected_features = sorted(selected_features)
-
-		for k in range(n_select):
-			if k != selected_features[k]:
-				self.assertTrue(k in range(n_select))
-
-	def test_run_pyfeast_invalid(self):
-		"""test the run_pyfeast function with an invalid algorithm name"""
-
- 		self.assertRaises(AttributeError, fizzy.run_pyfeast, None, None, None, "An Invalid Name", None)
-
-	def test_run_feature_selection(self):  
-		column_name = "Class"
-
-		selected_features = fizzy.run_feature_selection(self.biom_file_handle, self.map_file_handle, column_name, n_select=3)
-		self.assertEqual(selected_features, ['OTU0', "OTU1", "OTU3"])
+        self.assertEqual(parsed_map, correct_map)
 
 
-		
+    def test_parse_map_invalid_column_name(self):
+
+        self.assertRaises(ValueError, fizzy.parse_map_file, self.map_file_handle, "Unknown", [u'ID0',u'ID1',u'ID2'])
+
+    def test_parse_map_invalid_sample_name(self):
+
+        self.assertRaises(ValueError, fizzy.parse_map_file, self.map_file_handle, "Class", [u'ID99',u'ID1',u'ID2'])
+
+    def test_parse_map_invalid_equal_num_sample_classes(self):
+
+            self.assertRaises(ValueError, fizzy.parse_map_file, self.map_file_handle, "Class", [u'ID0',u'ID1'])
+            
+    def test_run_pyfeast(self):
+        """
+            test the run_pyfeast function by creating some uniform data, and running
+            the MIM feature selection method.
+
+            Assert that each of the selected features are in the appropriate range
+        """
+        n_select = 5
+        (data, labels)  = self.uniform_data(100, 50, n_select)
+        variable_names = labels # we don't need this to  be differenttest pyfeast, so just give it the same sized vector
+        
+        selected_features = fizzy.run_pyfeast(data, labels, variable_names, "MIM", n_select)
+        selected_features = sorted(selected_features)
+
+        for k in range(n_select):
+            if k != selected_features[k]:
+                self.assertTrue(k in range(n_select))
+
+    def test_run_pyfeast_invalid(self):
+        """test the run_pyfeast function with an invalid algorithm name"""
+
+        self.assertRaises(AttributeError, fizzy.run_pyfeast, None, None, None, "An Invalid Name", None)
+
+    def test_run_feature_selection(self):  
+        column_name = "Class"
+
+        selected_features = fizzy.run_feature_selection(self.biom_file_handle, self.map_file_handle, column_name, n_select=3)
+        self.assertEqual(selected_features, ['OTU0', "OTU1", "OTU3"])
+
+
+        
 information_theoretic_methods = ['CIFE','CMIM','CondMI', 'Condred','ICAP','JMI','MIM','MIFS','mRMR']
-map_file_string = """#SampleID	Class\nID0	S1\nID1	S2\nID2	S2\n"""
+map_file_string = """#SampleID  Class\nID0  S1\nID1 S2\nID2 S2\n"""
 biom_file_string = """{"id": "None","format": "Biological Observation Matrix 1.0.0","format_url": "http://biom-format.org","type": "OTU table","generated_by": "BIOM-Format 1.1.2","date":"2013-03-27T13:59:38.949014","matrix_type": "sparse","matrix_element_type":"float","shape": [4, 3],     "data": [[0,0,1.0],[0,2,4.0],[1,1,5.0],[1,2,7.0],[2,0,1.0],[2,1,1.0],[2,2,9.0],[3,0,6.0],[3,1,10.0],[3,2,8.0]],"rows":[{"id": "OTU0", "metadata": null},{"id": "OTU1", "metadata": null},{"id": "OTU2", "metadata": null},{"id": "OTU3", "metadata": null}],"columns": [{"id": "ID0", "metadata": null},{"id": "ID1", "metadata": null},{"id": "ID2", "metadata": null}]}"""
 
 if __name__ == "__main__":
-	main()
+    main()
