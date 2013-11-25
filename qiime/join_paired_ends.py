840a6e38094fa257953277e4528b6df159db6f2a
mikerobeson <soilbdelloid@gmail.com>
2013-11-25 16:54:00 -0500

    updated the code in ' write_synced_barcodes_fastq' function to better handle cases when we reach the end of the index reads file before the joined paired-ends file. With other minor edits

--- qiime/join_paired_ends.py
+++ qiime/join_paired_ends.py
@@ -22,7 +22,21 @@ join_method_names = {'fastq-join':join_paired_end_reads_fastqjoin,
 
 
 def write_synced_barcodes_fastq(joined_fp, index_fp):
-    """Writes new index file based on surviving assembled paired-ends."""
+    """Writes new index file based on surviving assembled paired-ends.
+       -joined_fp : file path to paired-end assembled fastq file
+       -index_fp : file path to index / barcode reads fastq file
+
+       This function iterates through the joined reads file and index file. 
+       Only those index-reads within the file at index_fp, that have headers
+       matching those within the joined-pairs at joined_fp, are written 
+       to file. 
+
+     WARNING: Assumes reads are in the same order in both files,
+              except for cases in which the corresponding
+              read in the joined_fp file is missing (i.e. pairs 
+              failed to assemble).
+
+    """
 
     j_path, j_ext = os.path.splitext(joined_fp)
     i_path, i_ext = os.path.splitext(index_fp)
@@ -50,15 +64,25 @@ def write_synced_barcodes_fastq(joined_fp, index_fp):
     index_fastq_iter = MinimalFastqParser(ih, strict=False)
     joined_fastq_iter = MinimalFastqParser(jh, strict=False) 
     # Write barcodes / index reads that we observed within
-    # the joined paired-ends:
+    # the joined paired-ends. Warn if index and joined data
+    # are not in order.
     for joined_label,joined_seq,joined_qual in joined_fastq_iter:
-        for index_label,index_seq,index_qual in index_fastq_iter:
-            if joined_label == index_label:
-                fastq_string = '@%s\n%s\n+\n%s\n'\
-                                %(index_label,index_seq,index_qual)
-                fbc_fh.write(fastq_string)
-                break # stop looping through 2nd for-loop when match is found
-
+        index_label,index_seq,index_qual = index_fastq_iter.next()
+        while joined_label != index_label:
+            try:
+                index_label,index_seq,index_qual = index_fastq_iter.next()
+            except StopIteration:
+                raise StopIteration, "\n\nReached end of index-reads file"+\
+                 " before iterating through joined paired-end-reads file!"+\
+                 " Except for missing paired-end reads that did not survive"+\
+                 " assembly, your index and paired-end reads files must be in"+\
+                 " the same order! Last ID processed was:\n"+ \
+                 " \'%s\'\n" %(joined_label)
+        else:
+            fastq_string = '@%s\n%s\n+\n%s\n'\
+                            %(index_label,index_seq,index_qual)
+            fbc_fh.write(fastq_string)
+    
     ih.close()
     jh.close()
     fbc_fh.close()
--- scripts/join_paired_ends.py
+++ scripts/join_paired_ends.py
@@ -16,19 +16,18 @@ from qiime.join_paired_ends import (join_method_names,
                                     write_synced_barcodes_fastq)
 from qiime.util import (parse_command_line_parameters, get_options_lookup, 
                         make_option, load_qiime_config, create_dir)
-from os.path import abspath, dirname, splitext
+import os
 import gzip
 
-# TODO:
-# add method specific options
-# rename output files to something more consistent between methods
 
 options_lookup = get_options_lookup()
 qiime_config = load_qiime_config()
 
 script_info={}
 script_info['brief_description']= """Joins paired-end Illumina reads."""
-script_info['script_description'] = """This script take forward and reverse Illumina reads and joins them based on the method chosen. Will optionally, create an updated index reads file to match the surviving joined paired-ends. Currently, there are two methods that can be selected by the user:
+script_info['script_description'] = """This script take forward and reverse Illumina reads and joins them based on the method chosen. Will optionally, create an updated index reads file to match the surviving joined paired-ends. If the option to write an updated index file is chosen, be sure that the order of the index-reads file is in the same order as the reads files you plan to join.
+
+Currently, there are two methods that can be selected by the user to join paired-end data:
 
 1. fastq-join - Erik Aronesty, 2011. ea-utils : "Command-line tools for processing biological sequencing data" (http://code.google.com/p/ea-utils)
 
@@ -42,20 +41,22 @@ script_info['output_description'] = """All paired-end joining software will retu
 
 
 1. fastq-join will output fastq-formatted files as:
-   \"..._join\" - assembled / joined reads output
-   \"..._un1\" - unassembled / unjoined reads1 output
-   \"..._un2\" - unassembled / unjoined reads2 output
+   \"*.join\" - assembled / joined reads output
+   \"*.un1\" - unassembled / unjoined reads1 output
+   \"*.un2\" - unassembled / unjoined reads2 output
 
 2. SeqPrep will output fastq-formatted gzipped files as: 
-   \"..._assembled.gz\" - unassembled / unjoined reads1 output
-   \"..._unassembled_R1.gz\" - unassembled / unjoined reads1 output
-   \"..._unassembled_R2.gz\" - unassembled / unjoined reads2 output
+   \"*_assembled.gz\" - unassembled / unjoined reads1 output
+   \"*_unassembled_R1.gz\" - unassembled / unjoined reads1 output
+   \"*_unassembled_R2.gz\" - unassembled / unjoined reads2 output
 
 3. If a barcode / index file is provided via the \'-b\' option, an updated
    barcodes file will be output as:
    \"..._barcodes.fastq\"
     This barcode / index file must be used in conjunction with the joined
-    paired-ends file as input to \'split_libraries_fastq.py\'.
+    paired-ends file as input to \'split_libraries_fastq.py\'. Except for
+    missing reads that may result from failed merging of paired-ends, the
+    index-reads and joined-reads must be in the same order.
 
 """
 script_info['required_options'] = [\
@@ -73,36 +74,37 @@ script_info['optional_options'] = [\
                       ' [default: %default]', default='fastq-join'),
     make_option('-o', '--output_dir', action='store', type='new_dirpath',\
                 help='Path to store '+\
-                      'result file [default: <JOINED_METHOD>_joined]'),
+                      'result file [default: <PE_JOIN_METHOD>_joined]'),
     make_option('-b','--index_reads_fp',type='existing_filepath',
                 dest='index_reads_fp',
                 help='Path to read the barcode / index reads in FASTQ format.'
                 ' Will be filtered based on surviving joined pairs.'),
-    make_option('-p', '--perc_max_diff', action='store', type='int',
-                help='fastq-join option:'+\
-                     ' Maximum allowed % differences within region of overlap'+\
-                      ',  [default: %default]', default=8),
     make_option('-j', '--min_overlap', 
-                help='fastq-join and SeqPrep option:'+\
+                help='Applies to both fastq-join and SeqPrep methods.'+\
                       ' Minimum allowed overlap in base-pairs required to join pairs.'+\
                       ' Defaults to recomended settings: fastq-join (6), SeqPrep (15) '+\
                       ', [default: %default]', default='default'),
+    make_option('-p', '--perc_max_diff', action='store', type='int',
+                help='Only applies to fastq-join method, otherwise ignored.'+\
+                     ' Maximum allowed % differences within region of overlap'+\
+                      ',  [default: %default]', default=8),
     make_option('-y', '--max_ascii_score', action='store', type='string',
-                help='SeqPrep option:'+\
+                help='Only applies to SeqPrep method, otherwise ignored.'+\
                       ' Maximum quality score / ascii code allowed to appear within'+\
-                      ' joined pairs output.' 
+                      ' joined pairs output. For more information see:'+\
+                      ' http://en.wikipedia.org/wiki/FASTQ_format ' 
                       ' [default: %default]', default='J'),
     make_option('-n', '--min_frac_match', action='store', type='float',
-                help='SeqPrep option:'+\
+                help='Only applies to SeqPrep method, otherwise ignored.'+\
                       ' Minimum allowed fraction of matching bases required to join reads'+\
                       ',  [default: %default]', default=0.9),
     make_option('-g', '--max_good_mismatch', action='store', type='float',
-                help='SeqPrep option:'+\
+                help='Only applies to SeqPrep method, otherwise ignored.'+\
                       ' Maximum mis-matched high quality bases allowed'+\
                       ' to join reads. ' + '[default: %default]', default=0.2),
     make_option('-6', '--phred_64', action='store_true',
-                help='SeqPrep option:'+\
-                      ' If input reads are in phred+64 format. Output will '\
+                help='Only applies to SeqPrep method, otherwise ignored.'+\
+                      ' Set if input reads are in phred+64 format. Output will '\
                       'always be phred+33. [default: %default]',
                         default=False)]
 
@@ -117,14 +119,14 @@ def main():
     forward_reads_fp = opts.forward_reads_fp 
     reverse_reads_fp = opts.reverse_reads_fp
     pe_join_method = opts.pe_join_method
-    # fastq-join only values:
+    # fastq-join only options:
     perc_max_diff = opts.perc_max_diff
-    # SeqPrep only values:
+    # SeqPrep only options:
     max_ascii_score = opts.max_ascii_score
     min_frac_match = opts.min_frac_match
     max_good_mismatch = opts.max_good_mismatch
     phred_64 = opts.phred_64
-    # both fastq-join & SeqPrep
+    # both fastq-join & SeqPrep options
     min_overlap = opts.min_overlap
 
 
@@ -151,13 +153,11 @@ def main():
 
     # determine output directory:   
     if opts.output_dir: # user specified output directory
-        output_dir = abspath(opts.output_dir)
+        output_dir = os.path.abspath(opts.output_dir)
     else: # default output dir to location of infile
-        output_dir = dirname(abspath(forward_reads_fp))
+        output_dir = os.path.dirname(os.path.abspath(forward_reads_fp))
     
     create_dir(output_dir, fail_on_exist=False)
-
-    # TODO: check for valid combination of options
 
     # send parameters to appropriate join method
     # currently only two join methods exist:
