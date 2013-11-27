efb427f4f7d2de741ab1e1004af973b9d218dc14
Daniel McDonald <mcdonadt@colorado.edu>
2013-11-27 13:34:47 -0700

    simplified merge logic
    * headers now tracked in a set
    * explicit checks for ordered columns (e.g., SampleID)
    * decomposed multi-line join and list comprehension

--- qiime/merge_mapping_files.py
+++ qiime/merge_mapping_files.py
@@ -22,18 +22,18 @@ def merge_mapping_files(mapping_files,no_data_value='no_data'):
          mapping field associated with a sample ID (default: 'no_data')
     """
     mapping_data = defaultdict(dict)
-    all_headers = []
+    all_headers = set([])
     result = []
     
     # iterate over mapping files, parsing each
     for mapping_file in mapping_files:
         current_data, current_headers, current_comments = \
            parse_mapping_file(mapping_file,strip_quotes=False)
-        all_headers += current_headers
+        all_headers.update(set(current_headers))
 
         for entry in current_data:
             current_values = {k:v for k,v in zip(current_headers, entry)}
-            sample_id = current_values.pop('SampleID') 
+            sample_id = current_values['SampleID']
 
             if sample_id in mapping_data:
                 # if the sample id has already been seen, confirm that
@@ -53,31 +53,26 @@ def merge_mapping_files(mapping_files,no_data_value='no_data'):
 
             mapping_data[sample_id].update(current_values)
     
-    all_headers = {}.fromkeys(all_headers)
-    
     # remove and place the fields whose order is important
     ordered_beginning = []
     for e in ['SampleID','BarcodeSequence','LinkerPrimerSequence']:
-        try:
-            del all_headers[e]
+        if e in all_headers:
+            all_headers.remove(e)
             ordered_beginning.append(e)
-        except KeyError:
-            pass
             
     ordered_end = []
     for e in ['Description']:
-        try:
-            del all_headers[e]
+        if e in all_headers:
+            all_headers.remove(e)
             ordered_end.append(e)
-        except KeyError:
-            pass
-    ordered_headers = ordered_beginning  + list(all_headers) + ordered_end
+    
+    ordered_headers = ordered_beginning + list(all_headers) + ordered_end
     
     # generate the mapping file lines containing all fields
     result.append('#' + '\t'.join(ordered_headers))
     for sample_id, data in mapping_data.items():
-        result.append('\t'.join([sample_id] + \
-          [data.get(h,no_data_value) for h in ordered_headers[1:]]))
+        values = [data.get(k, no_data_value) for k in ordered_headers]
+        result.append('\t'.join(values))
     
     return result
 
