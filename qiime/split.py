0006fa2b3f06e2abf3308ceb5309140dc7b77ada
Daniel McDonald <mcdonadt@colorado.edu>
2013-11-27 13:05:38 -0700

    added credit

--- qiime/split.py
+++ qiime/split.py
@@ -4,7 +4,7 @@ from __future__ import division
 
 __author__ = "Greg Caporaso"
 __copyright__ = "Copyright 2011, The QIIME project"
-__credits__ = ["Greg Caporaso"]
+__credits__ = ["Greg Caporaso", "Daniel McDonald"]
 __license__ = "GPL"
 __version__ = "1.7.0-dev"
 __maintainer__ = "Greg Caporaso"
