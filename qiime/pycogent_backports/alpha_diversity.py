c1438072204dca1d61e3e95e06a9231a33caf245
Daniel McDonald <mcdonadt@colorado.edu>
2013-11-27 16:27:38 -0700

    added credit

--- qiime/pycogent_backports/alpha_diversity.py
+++ qiime/pycogent_backports/alpha_diversity.py
@@ -10,7 +10,8 @@ import cogent.maths.stats.rarefaction as rarefaction
 
 __author__ = "Rob Knight"
 __copyright__ = "Copyright 2007-2012, The Cogent Project"
-__credits__ = ["Rob Knight", "Justin Kuczynski, William Van Treuren"]
+__credits__ = ["Rob Knight", "Justin Kuczynski, William Van Treuren",
+               "Daniel McDonald"]
 __license__ = "GPL"
 __version__ = "1.5.3-dev"
 __maintainer__ = "Rob Knight"
