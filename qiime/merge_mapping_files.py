e7cd72f150e1323e17109d89919f6e79bca6f6b4
Daniel McDonald <mcdonadt@colorado.edu>
2013-11-27 13:06:56 -0700

    added whitespace to break up logic flow

--- qiime/merge_mapping_files.py
+++ qiime/merge_mapping_files.py
@@ -29,17 +29,21 @@ def merge_mapping_files(mapping_files,no_data_value='no_data'):
         current_data, current_headers, current_comments = \
            parse_mapping_file(mapping_file,strip_quotes=False)
         all_headers += current_headers
+
         for entry in current_data:
             sample_id = entry[0]
             current_values = {}
+        
             for header,value in zip(current_headers[1:],entry[1:]):
                 current_values[header] = value
+            
             if sample_id in mapping_data:
                 # if the sample id has already been seen, confirm that
                 # there is no conflicting values across the different 
                 # mapping files (e.g., pH=5.0 and pH=6.0)- if there is, 
                 # raise a ValueError
                 previous_data = mapping_data[sample_id]
+            
                 for header,value in current_values.items():
                     if header in previous_data and value != previous_data[header]:
                         raise ValueError,\
@@ -73,6 +77,7 @@ def merge_mapping_files(mapping_files,no_data_value='no_data'):
     for sample_id, data in mapping_data.items():
         result.append('\t'.join([sample_id] + \
           [data.get(h,no_data_value) for h in ordered_headers[1:]]))
+    
     return result
 
 def write_mapping_file(mapping_data,output_fp):
