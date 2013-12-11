c71cd07d6c7c48b6327a122671eb4932c0a7b3af
Daniel McDonald <mcdonadt@colorado.edu>
2013-12-11 00:04:05 -0700

    possible parallel support, doc/example

--- qiime/workflow/core.py
+++ qiime/workflow/core.py
@@ -46,6 +46,40 @@ wf = MyWorkflow()
 gen = (i for i in range(10))
 for i in wf(gen):
     print i
+
+
+# assumes MyWorkflow is in a separate module
+As a multiprocess example:
+
+# assumes: "ipcluster start -n 4"
+from IPython.parallel import Client
+from time import sleep
+from example import MyWorkflow
+
+def exec_wf():
+    result = []
+    for i in wf(gen):
+        result.append(i)
+    return result
+
+c = Client()
+dv = c[:]
+opts = {'add_value':None, 'sub_value':5}
+nprocs = len(c)
+for rank, worker in enumerate(c):
+    worker.execute("from example import MyWorkflow")
+    worker.execute("gen = (i for i in range(10))")
+    worker['rank'] = rank
+    worker['opts'] = opts
+    worker['nprocs'] = nprocs
+    worker.execute("wf = MyWorkflow(Options=opts, Rank=rank, NProcs=nprocs)")
+ar = dv.apply_async(exec_wf)
+while not ar.ready():
+    sleep(1)
+
+# not merged, but same result as the first single core example
+for foo in ar.get():
+    print foo
 """
 
 from itertools import chain
@@ -73,7 +107,8 @@ option_exists = Exists()
 class Workflow(object):
     """Arbitrary worflow support structure"""
 
-    def __init__(self, ShortCircuit=True, Debug=True, Options=None):
+    def __init__(self, ShortCircuit=True, Debug=True, Options=None, Rank=0,
+            NProcs=1, Finalize=None):
         """Build thy self
 
         ShortCiruit : if True, enables ignoring function groups when a given
@@ -93,6 +128,9 @@ class Workflow(object):
         self.ShortCircuit = ShortCircuit
         self.Failed = False
         self.FinalState = None
+        self.Rank = Rank
+        self.NProcs = NProcs
+        self.Finalize = Finalize
 
         for f in self._all_wf_methods():
             if not hasattr(f, '__workflowtag__'):
@@ -137,17 +175,24 @@ class Workflow(object):
 
         it, workflow = self._get_workflow(it)
         
+        count = 0
         for item in it:
             self.Failed = False
             self.FinalState = None
 
-            for f in workflow:
-                f(item)
+            if count % self.NProcs == self.Rank:
+                for f in workflow:
+                    f(item)
 
             if self.Failed and fail_callback is not None:
                 yield fail_callback(self)
             else:
                 yield success_callback(self)
+            
+            count += 1
+
+        if self.Finalize is not None:
+            self.Finalize(self)
 
     @staticmethod
     def tagFunction(f):
