f5ac0b2d8c0bc3fdf0245546f37e359cf2bff45f
Emily TerAvest <ejteravest@gmail.com>
2014-03-15 13:30:01 -0600

    more passing

--- tests/test_map_reads_to_reference.py
+++ tests/test_map_reads_to_reference.py
@@ -13,10 +13,12 @@ __email__ = "gregcaporaso@gmail.com"
 
 from shutil import rmtree
 from os.path import exists, join
+from tempfile import mkstemp , mkdtemp
+
 from cogent.util.unit_test import TestCase, main
-from cogent.util.misc import remove_files, create_dir
+from cogent.util.misc import remove_files
 from biom.parse import parse_biom_table
-from qiime.util import get_qiime_temp_dir, get_tmp_filename
+from qiime.util import get_qiime_temp_dir
 from qiime.map_reads_to_reference import (
     usearch_database_mapper, blat_database_mapper, bwa_short_database_mapper,
     bwa_sw_database_mapper, blat_nt_database_mapper)
@@ -33,11 +35,9 @@ class DatabaseAssignmentTests(TestCase):
         """
         """
         tmp_dir = get_qiime_temp_dir()
-        self.test_out = get_tmp_filename(tmp_dir=tmp_dir,
-                                         prefix='qiime_parallel_tests_',
-                                         suffix='',
-                                         result_constructor=str)
-        create_dir(self.test_out)
+        self.test_out = mkdtemp(dir=tmp_dir,
+                                prefix='qiime_parallel_tests_',
+                                suffix='')
         self.dirs_to_remove = [self.test_out]
 
         self.output_fp = join(self.test_out, 'fmap.txt')
@@ -48,32 +48,32 @@ class DatabaseAssignmentTests(TestCase):
         self.files_to_remove = [self.output_fp, self.failure_fp,
                                 self.usearch_fp, self.log_fp, self.bl6_fp]
 
-        self.refseqs1_fp = get_tmp_filename(tmp_dir=self.test_out,
-                                            prefix='qiime_refseqs',
-                                            suffix='.fasta')
+        _, self.refseqs1_fp = mkstemp(dir=self.test_out,
+                                      prefix='qiime_refseqs',
+                                      suffix='.fasta')
         refseqs1_f = open(self.refseqs1_fp, 'w')
         refseqs1_f.write(refseqs1)
         refseqs1_f.close()
         self.files_to_remove.append(self.refseqs1_fp)
 
-        self.refseqs2_fp = get_tmp_filename(tmp_dir=self.test_out,
-                                            prefix='qiime_refseqs',
-                                            suffix='.fasta')
+        _, self.refseqs2_fp = mkstemp(dir=self.test_out,
+                                      prefix='qiime_refseqs',
+                                      suffix='.fasta')
         refseqs2_f = open(self.refseqs2_fp, 'w')
         refseqs2_f.write(refseqs2)
         refseqs2_f.close()
         self.files_to_remove.append(self.refseqs2_fp)
 
-        self.inseqs1_fp = get_tmp_filename(tmp_dir=self.test_out,
-                                           prefix='qiime_inseqs',
-                                           suffix='.fasta')
+        _, self.inseqs1_fp = mkstemp(dir=self.test_out,
+                                     prefix='qiime_inseqs',
+                                     suffix='.fasta')
         inseqs1_f = open(self.inseqs1_fp, 'w')
         inseqs1_f.write(inseqs1)
         inseqs1_f.close()
         self.files_to_remove.append(self.inseqs1_fp)
-        self.inseqs2_fp = get_tmp_filename(tmp_dir=self.test_out,
-                                           prefix='qiime_inseqs',
-                                           suffix='.fasta')
+        _, self.inseqs2_fp = mkstemp(dir=self.test_out,
+                                     prefix='qiime_inseqs',
+                                     suffix='.fasta')
         inseqs2_f = open(self.inseqs2_fp, 'w')
         inseqs2_f.write(inseqs2)
         inseqs2_f.close()
