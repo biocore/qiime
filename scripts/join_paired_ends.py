afb4a70c96ce6c288d70566e46b8449990948b36
johnjohnchase <llcooljohn@gmail.com>
2014-03-17 14:12:44 -0600

    removed unused commit

--- scripts/join_paired_ends.py
+++ scripts/join_paired_ends.py
@@ -8,7 +8,6 @@ __version__ = "1.8.0-dev"
 __maintainer__ = "Mike Robeson"
 __email__ = "robesonms@ornl.gov"
 
-from skbio.parse.sequences import fastq_parse
 from qiime.join_paired_ends import (join_method_names,
                                     join_method_constructors,
                                     write_synced_barcodes_fastq)
