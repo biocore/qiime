85eb7af2efaa1f4538118801ae6318e46c321739
Jose Navas <josenavasmolina@gmail.com>
2014-05-02 10:06:44 -0600

    Fixing to_file argument

--- scripts/principal_coordinates.py
+++ scripts/principal_coordinates.py
@@ -92,14 +92,16 @@ def main():
             # Store the PCoA results on the output directory
             base_fname, ext = splitext(fname)
             out_file = join(output_path, 'pcoa_%s.txt' % base_fname)
-            pcoa_scores.to_file(out_file)
+            with open(out_file, 'w') as f:
+                pcoa_scores.to_file(f)
 
     else:
         # Run PCoA on the input distance matrix
         with open(input_path, 'U') as f:
             pcoa_scores = pcoa(f)
         # Store the results in the output file
-        pcoa_scores.to_file(output_path)
+        with open(output_path, 'w') as f:
+            pcoa_scores.to_file(f)
 
 
 if __name__ == "__main__":
