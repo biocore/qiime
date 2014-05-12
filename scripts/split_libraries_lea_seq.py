4465fe7ffe5e38371d7a5d66dc81d5086f5beae6
charudatta-navare <charudatta.navare@gmail.com>
2014-05-11 22:44:04 -0400

    Update split_libraries_lea_seq.py

--- scripts/split_libraries_lea_seq.py
+++ scripts/split_libraries_lea_seq.py
@@ -9,35 +9,47 @@ __version__ = "1.8.0-dev"
 __maintainer__ = "Charudatta Navare"
 __email__ = "charudatta.navare@gmail.com"
 
-from qcli import parse_command_line_parameters, make_option
-from qiime.split_libraries_lea_seq import (get_cluster_ratio, select_majority_sequence, get_consensus, read_input_file)
 import tempfile
+import os
+from qiime.util import create_dir
+from qcli import parse_command_line_parameters, make_option
+from qiime.split_libraries_lea_seq import (get_cluster_ratio, get_consensus,
+                                         read_input_file, extract_primer,
+                                         select_unique_rand_bcs)
 
-script_info={}
 
-script_info['brief_description'] = "Implements Low-Error Amplicon Sequencing (LEA-Seq)"
-
-script_info['script_description'] = """Implements Low-Error Amplicon Sequencing (LEA-Seq) method, \
+script_info = {}
+script_info[
+    'brief_description'] = "Implements Low-Error Amplicon Sequencing (LEA-Seq)"
+script_info[
+    'script_description'] = """Implements Low-Error Amplicon Sequencing (LEA-Seq) method, \
 described in: Faith, Jeremiah J., et al. \
-The long-term stability of the human gut microbiota. Science 341.6141 (2013).
-This method is based on redundant sequencing of a set of linear PCR template\
-extensions of 16S rRNA genes. The oligonucleotide primer that is used for PCR template extensions\
-is labeled with a random barcode 5' to the universal 16S rRNA primer sequence. This PCR pool is\
-then amplified with exponential PCR, using primers that specifically amplify only the linear\
-PCR molecules. An index primer is added to the amplicons along with a primer specific for each\
-sample. This exponential PCR pool is then sequenced redundantly (20x coverage). The resulting\
-sequences are separated by sample, using the index sequence. The amplicon sequences within each\
-sample are separated by the random barcodes. The large number of reads for each barcode helps\
-to create an error-corrected consensus sequence for the initial template molecule.
+The long-term stability of the human gut microbiota.\
+Science 341.6141 (2013).\
+This method is based on redundant sequencing of a set of linear PCR\
+template extensionsof 16S rRNA genes. The oligonucleotide primer\
+that is used for PCR template extensions is labeled with a random barcode\
+5' to the universal 16S rRNA primer sequence. This PCR pool is then\
+amplified with exponential PCR, using primers that specifically\
+amplify only the linear PCR molecules. An index primer is added to\
+the amplicons along with a primer specific for each sample.
+This exponential PCR pool is then sequenced redundantly (20x coverage).\
+The resulting sequences are separated by sample, using the index sequence.\
+The amplicon sequences within each sample are separated by the random\
+barcodes. The large number of reads for each barcode helps to\
+create an error-corrected consensus sequence for the\
+initial template molecule.
 """
-
-script_info['script_usage'] = """Example: %prog -i fwd.fq,rev.fq -m Mapping_file.txt -o output_dir --barcode_type=7
+script_info['script_usage'] = []
+script_info['script_usage'].append
+(("""
+    Example: %prog -i fwd.fq,rev.fq -m Mapping_file.txt -o output_dir""",
+    """""",
+    """"""))
+script_info['output_description'] = """The %prog generates:\
+A fasta file called seqs.fna which contains\
+error corrected consensus sequence for the template DNA\
 """
-
-script_info['output_description'] = """ The %prog generates:\
-A fasta file called seqs.fna which contains error corrected consensus sequence for the template DNA\
-"""
-
 script_info['required_options'] = [
     make_option('-i', '--sequence_read_fps', type='existing_filepaths',
                 help='the forward and reverse sequence read fastq files '
@@ -47,7 +59,6 @@ script_info['required_options'] = [
     make_option('-m', '--mapping_fp', type='existing_filepath',
                 help='metadata mapping file')
 ]
-
 script_info['optional_options'] = [
     make_option('--barcode_type', type='string',
                 help='the type of barcode used. This can be an integer, e.g. '
@@ -78,28 +89,27 @@ script_info['version'] = __version__
 
 
 def main():
-    option_parser, opts, args = \
-	parse_command_line_parameters(**script_info)
-	barcode_type = opts.barcode_type
-	max_barcode_errors = opts.max_barcode_errors
-	output_dir = opts.output_dir
-	mapping_fp = opts.mapping_fp
-	sequence_read_fps = opts.sequence_read_fps
-	min_consensus = opts.min_consensus
-	max_cluster_ratio = opts.threshold_for_cluster_ratio
-	random_bc_lookup = read_input_file(sequence_read_fps, mapping_fp,
-                                       output_dir, barcode_type,
-                                       max_barcode_errors)
-	consensus_outfile = open(os.path.join(output_dir, "seqs.fna"), "w")
-	for sample_id in random_bc_lookup:
-		for random_bc in random_bc_lookup[sample_id]:
-			seqs, counts = random_bc_lookup[sample_id][random_bc]
-			if cluster_ratio[sample_id][random_bc] < max_cluster_ratio:
-				consensus_seq = select_majority_sequence(seqs, counts)
-			else:
-				consensus_seq = get_consensus(seqs, counts, min_consensus)  
-			consensus_outfile.write(">"+ sample_id + random_bc 
-                                        + "\n" + consensus_seq+"\n")
+    option_parser, opts, args = parse_command_line_parameters(**script_info)
+    barcode_type = opts.barcode_type
+    max_barcode_errors = opts.max_barcode_errors
+    mapping_fp = opts.mapping_fp
+    sequence_read_fps = opts.sequence_read_fps
+    min_consensus = opts.min_consensus
+    max_cluster_ratio = opts.threshold_for_cluster_ratio
+    output_dir = opts.output_dir
+    create_dir(output_dir)
+    consensus_outfile = open(os.path.join(output_dir, "seqs.fna"), "w")
+
+    consensus_seq_lookup = read_input_file(sequence_read_fps, mapping_fp,
+                                           output_dir, barcode_type,
+                                           max_barcode_errors, min_consensus,
+                                           max_cluster_ratio, option_parser)
+
+    for sample_id in consensus_seq_lookup:
+        for random_bc in consensus_seq_lookup[sample_id]:
+            consensus_seq = consensus_seq_lookup[sample_id][random_bc]
+            consensus_outfile.write(">" + sample_id + random_bc
+                                    + "\n" + consensus_seq + "\n")
 
 if __name__ == "__main__":
     main()
