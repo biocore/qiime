275096a3807709d55aedaa14ed27d0d6d2ce05de
Greg Caporaso <gregcaporaso@gmail.com>
2014-05-14 19:28:50 -0700

    no cog yaaaay

--- tests/test_denoiser/test_flowgram_clustering.py
+++ tests/test_denoiser/test_flowgram_clustering.py
@@ -19,9 +19,8 @@ from tempfile import mkdtemp
 
 from unittest import TestCase, main
 from numpy.testing import assert_almost_equal
-from cogent.core.sequence import Sequence
 from brokit.denoiser import Flowgram, parse_sff, FlowgramCollection
-from cogent.app.util import ApplicationNotFoundError
+from skbio.app.util import ApplicationNotFoundError
 
 from qiime.denoiser.flowgram_clustering import *
 from qiime.denoiser.utils import FlowgramContainerArray
