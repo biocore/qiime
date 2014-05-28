b6569e5ead4e401ee155fba0b8fcc276fda854f5
Jose Navas <josenavasmolina@gmail.com>
2014-05-28 11:03:04 -0600

    Adding documentation

--- scripts/compare_trajectories.py
+++ scripts/compare_trajectories.py
@@ -15,27 +15,66 @@ from os.path import join, exists
 
 import pandas as pd
 
-from skbio.maths.stats.ordination import OrdinationResults
+from skbio.math.stats.ordination import OrdinationResults
 
 from qiime.util import parse_command_line_parameters, make_option
-from qiime.trajectory_analysis import (run_trajectory_analysis,
-                                       TRAJECTORY_ALGORITHMS)
+from qiime.compare_trajectories import (run_trajectory_analysis,
+                                        TRAJECTORY_ALGORITHMS)
 
 
 script_info = {}
-script_info['brief_description'] = """"""
-script_info['script_description'] = """"""
+script_info['brief_description'] = ("Run analysis of volatility using a "
+                                    "variety of algorithms")
+script_info['script_description'] = (
+    "This script mainly allows performing analysis of volatility on time "
+    "series data, but they can be applied to any data that contains a gradient"
+    ". The methods available are RMS (either using 'avg' or 'trajectory'); or "
+    "the first difference (using 'diff'), or 'wdiff' for a modified first "
+    "difference algorithm. The trajectories are computed as follows. For `avg`"
+    " it calculates the average point within a group and then computes the "
+    "norm of the distance of each sample from the averaged value. For "
+    "'trajectory' each component of the result trajectory is computed as "
+    "taking the sorted list of samples in the group and taking the norm of the"
+    " coordinates of the 2nd samples minus the 1st sample, 3rd sample minus "
+    "2nd sample and so on. For `diff` it calculates the norm for all the "
+    "time-points and then calculates the first difference for each resulting "
+    "point. For `wdiff`, it calculates the norm for all the time-points and "
+    "substracts the mean of the next number of elements, specified using the "
+    "`--window_size` parameters, and the current element.")
 script_info['script_usage'] = [
-    ("Example", "", "%prog")
+    ("Average method",
+     "Execute the analysis of volatility using the average method, grouping "
+     "the samples using the 'Treatment' category",
+     "%prog -i pcoa_res.txt -m map.txt -c 'Treatment' -o avg_output"),
+    ("Trajectory method",
+     "Execute the analysis of volatility using the trajectory method, grouping"
+     " the samples using the 'Treatment' category and sorting them using the "
+     "'time' category",
+     "%prog -i pcoa_res.txt -m map.txt -c 'Treatment' --algorithm trajectory "
+     "-o trajectory_output -s time"),
+    ("First difference method",
+     "Execute the analysis of volatility using the first difference method, "
+     "grouping the samples using the 'Treatment' category, sorting them using "
+     "the 'time' category and calculating the trajectory using the first "
+     "four axes",
+     "%prog -i pcoa_res.txt -m map.txt -c 'Treatment' --algorithm diff "
+     "-o diff_output -s time --axes 4"),
+    ("Window difference method",
+     "Execute the analysis of volatility using the window difference method, "
+     "grouping the samples using the 'Treatment' category, sorting them using "
+     "the 'time' category, weighting the output by the space between "
+     "samples in the 'time' category and using a window size of three.",
+     "%prog -i pcoa_res.txt -m map.txt -c 'Treatment' --algorithm wdiff "
+     "-o wdiff_output -s time --window_size 3 -w")
 ]
 script_info['required_options'] = [
-    make_option('-i', '--input_ordination_fp', type='existing_filepath',
+    make_option('-i', '--input_fp', type='existing_filepath',
                 help="Input ordination results filepath"),
     make_option('-m', '--map_fp', type='existing_filepath',
                 help="Input metadata mapping filepath"),
-    make_option('-c', '--category', type='string',
-                help="Category name of the mapping file to use to create the "
-                     "vectors"),
+    make_option('-c', '--categories', type='string',
+                help="Comma-separated list of category names of the mapping "
+                     "file to use to create the trajectories"),
     make_option('-o', '--output_dir', type='new_dirpath',
                 help="Name of the output directory to save the results")
 ]
@@ -44,31 +83,13 @@ script_info['optional_options'] = [
                 help="Category name of the mapping file to use to sort"),
     make_option('--algorithm', type='choice', default='avg',
                 choices=TRAJECTORY_ALGORITHMS,
-                help="The algorithm used to create the vectors. The method "
-                     "used can be RMS (either using 'avg' or 'trajectory'); or"
-                     " the first difference (using 'diff'), or 'wdiff' for a "
-                     "modified first difference algorithm (see --window_size) "
-                     "the aforementioned use all the dimensions and weights "
-                     "them using their percentage explained; returns the norm "
-                     "of the created vectors; and their confidence using ANOVA"
-                     ". The Vectors are created as follows: for 'avg' it "
-                     "calculates the average at each timepoint (averaging "
-                     "within a group), then calculates the norm of each point;"
-                     " for 'trajectory' calculates the norm for the 1st-2nd, "
-                     "2nd-3rd, etc.; for 'diff', it calculates the norm for "
-                     "all the time-points and then calculates the first "
-                     "difference for each resulting point; for 'wdiff' it uses"
-                     " the same procedure as the previous method but the "
-                     "subtraction will be between the mean of the next number "
-                     "of elements specified in --window_size and the current "
-                     "element, both methods ('wdiff' and 'diff') will also "
-                     "include the mean and the standard deviation of the "
-                     "calculations [defautl: %default]"),
-    make_option('--vectors_axes', dest='vectors_axes', type='int', default=3,
-                help="The number of axes to account while doing the vector "
-                     "specific calculations. We suggest using 3 because those "
-                     "are the ones being displayed in the plots but you could "
-                     "use any number between 1 and number of samples- 1. To "
+                help="The algorithm to use. Available methods: "
+                     + str(TRAJECTORY_ALGORITHMS) + ". [Default: %default]"),
+    make_option('--axes', type='int', default=3,
+                help="The number of axes to account while doing the trajectory"
+                     " specific calculations. We suggest using 3 because those"
+                     " are the ones being displayed in the plots but you could"
+                     " use any number between 1 and number of samples - 1. To "
                      "use all of them pass 0. [default: %default]"),
     make_option('-w', '--weight_by_vector', default=False, action='store_true',
                 help="Use -w when you want the output to be weighted by the "
@@ -76,9 +97,9 @@ script_info['optional_options'] = [
                      "days between samples [default: %default]"),
     make_option('--window_size', type='int', default=None,
                 help="Use --window_size, when selecting the modified first "
-                     "difference (\'wdiff\') option for --algorithm. This "
+                     "difference ('wdiff') option for --algorithm. This "
                      "integer determines the number of elements to be averaged"
-                     " per element subtraction, the resulting vector. "
+                     " per element subtraction, the resulting trajectory. "
                      "[default: %default]")
 ]
 script_info['version'] = __version__
@@ -88,11 +109,11 @@ if __name__ == '__main__':
 
     ord_fp = opts.input_ordination_fp
     mapping_fp = opts.map_fp
-    vector_category = opts.category
+    categories = opts.categories
     output_dir = opts.output_dir
     sort_by = opts.sort_by
     algorithm = opts.algorithm
-    vectors_axes = opts.vectors_axes
+    axes = opts.axes
     weighted = opts.weight_by_vector
     window_size = opts.window_size
 
@@ -105,9 +126,10 @@ if __name__ == '__main__':
     # if all its column values are NA, which should occur only with in comments
     metamap = pd.read_csv(mapping_fp, sep='\t', index_col=0).dropna(how='all')
 
-    if vector_category not in metamap.keys():
-        option_parser.error("Vector category %s does not exist in the mapping "
-                            "file" % vector_category)
+    for category in categories:
+        if category not in metamap.keys():
+            option_parser.error("Category %s does not exist in the mapping "
+                                "file" % categories)
 
     if sort_by:
         if sort_by == 'SampleID':
@@ -118,13 +140,13 @@ if __name__ == '__main__':
         else:
             sort_category = sort_by
 
-    if vectors_axes < 0 or vectors_axes > len(ord_res.eigvals):
-        option_parser.error("--vectors_axes should be between 0 and the max "
+    if axes < 0 or axes > len(ord_res.eigvals):
+        option_parser.error("--axes should be between 0 and the max "
                             "number of axes available (%d), found: %d "
-                            % (len(ord_res.eigvals), vectors_axes))
-    elif vectors_axes == 0:
-        # If vector_axes = 0, we generate all of them
-        vectors_axes = len(ord_res.eigvals)
+                            % (len(ord_res.eigvals), axes))
+    elif axes == 0:
+        # If axes = 0, we generate all of them
+        axes = len(ord_res.eigvals)
 
     if weighted:
         if sort_by == 'SampleID':
@@ -143,16 +165,16 @@ if __name__ == '__main__':
             option_parser.error("--window_size should be a positive integer, "
                                 "%d found" % window_size)
 
-    res = run_trajectory_analysis(ord_res, metamap, vector_category,
-                                  sort_category, algorithm, vectors_axes,
+    res = run_trajectory_analysis(ord_res, metamap, categories,
+                                  sort_category, algorithm, axes,
                                   weighted, window_size)
 
     if not exists(output_dir):
         mkdir(output_dir)
 
-    vectors_fp = join(output_dir, 'vectors.txt')
-    vectors_raw_fp = join(output_dir, 'vectors_raw_values.txt')
-    with open(vectors_fp, 'w') as f:
-        f.write('\n'.join(res))
-    with open(vectors_raw_fp, 'w') as f:
-        f.write('\n'.join(res))
+    trajectories_fp = join(output_dir, 'trajectories.txt')
+    trajectories_raw_fp = join(output_dir, 'trajectories_raw_values.txt')
+
+    with open(trajectories_fp, 'w') as out_f:
+        with open(trajectories_raw_fp, 'w') as raw_f:
+            res.to_files(out_f, raw_f)
