b8c5aa98ed6e99ce5800e2090b81f2f031865500
Antonio Gonzalez <antgonza@gmail.com>
2014-06-10 17:08:41 -0600

    updating failing tests

--- tests/failing_tests.sh
+++ tests/failing_tests.sh
@@ -1,22 +1,18 @@
-# python /home/qiime/qiime/tests/test_alpha_diversity.py
-# python /home/qiime/qiime/tests/test_beta_diversity.py
-# python /home/qiime/qiime/tests/test_core_microbiome.py
-# python /home/qiime/qiime/tests/test_filter.py
-# python /home/qiime/qiime/tests/test_format.py
- python /home/qiime/qiime/tests/test_make_otu_heatmap.py
-# python /home/qiime/qiime/tests/test_make_otu_heatmap_html.py
-# python /home/qiime/qiime/tests/test_make_otu_network.py
-# python /home/qiime/qiime/tests/test_make_otu_table.py
-# python /home/qiime/qiime/tests/test_map_reads_to_reference.py
-# python /home/qiime/qiime/tests/test_parallel/test_map_reads_to_reference.py
-# python /home/qiime/qiime/tests/test_parallel/test_multiple_rarefactions.py
-# python /home/qiime/qiime/tests/test_rarefaction.py
-# python /home/qiime/qiime/tests/test_simsam.py
-# python /home/qiime/qiime/tests/test_split.py
-# python /home/qiime/qiime/tests/test_stats.py
-# python /home/qiime/qiime/tests/test_summarize_taxa.py
-# python /home/qiime/qiime/tests/test_util.py
-# python /home/qiime/qiime/tests/test_workflow/test_core_diversity_analyses.py
-# python /home/qiime/qiime/tests/test_workflow/test_downstream.py
-# python /home/qiime/qiime/tests/test_workflow/test_pick_open_reference_otus.py
-# python /home/qiime/qiime/tests/test_workflow/test_upstream.py
+# python test_compare_categories.py
+# python test_core_microbiome.py
+# python test_make_otu_heatmap.py
+# python test_make_otu_heatmap_html.py
+# python test_make_otu_network.py
+# python test_parallel/test_multiple_rarefactions.py
+# python test_split.py
+# python test_stats.py
+# python test_util.py
+# python test_workflow/test_core_diversity_analyses.py
+# python test_workflow/test_downstream.py
+# python test_workflow/test_pick_open_reference_otus.py
+# python test_workflow/test_upstream.py
+
+
+# Ran 295 commands to test 122 scripts. 28 of these commands failed and 2 scripts could not be tested due to errors.
+# Failed scripts were: beta_diversity_through_plots.py beta_significance.py compute_core_microbiome.py core_diversity_analyses.py filter_fasta.py filter_otus_from_otu_table.py filter_taxa_from_otu_table.py identify_paired_differences.py jackknifed_beta_diversity.py make_otu_heatmap_html.py make_otu_network.py merge_otu_tables.py pick_open_reference_otus.py split_otu_table_by_taxonomy.py summarize_otu_by_cat.py summarize_taxa_through_plots.py
+# 2 scripts could not be imported. Scripts were: compare_categories.py trflp_file_to_otu_table.p
