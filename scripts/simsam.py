68200bca46513f53aa2c89bd8b71d5ed1d84403a
Adam Robbins-Pianka <adam.robbinspianka@colorado.edu>
2014-06-11 23:10:01 -0600

    use biom_open with simsam
    Also, pep8 changes

--- scripts/simsam.py
+++ scripts/simsam.py
@@ -11,33 +11,45 @@ __maintainer__ = "Justin Kuczynski"
 __email__ = "justinak@gmail.com"
 
 from os.path import join, split, splitext
+
 from cogent.parse.tree import DndParser
+from biom.parse import parse_biom_table
+from biom.util import biom_open
+
 from qiime.util import (add_filename_suffix, create_dir, get_options_lookup,
                         parse_command_line_parameters, make_option)
 from qiime.simsam import simsam_range_to_files
-from biom.parse import parse_biom_table
 
 options_lookup = get_options_lookup()
 
 script_info = {}
 script_info[
-    'brief_description'] = "Simulate samples for each sample in an OTU table, using a phylogenetic tree."
-script_info['script_description'] = """ This script makes n samples related to each sample in an input otu table
-
-An input OTU table with 3 samples and n=2 will result in an output OTU table with 6 samples total: 3 clusters of 2 related samples.
-
-To simulate each of the new samples, this script uses a sample in the input OTU table, and for each OTU in that sample the script
-traverses rootward on the tree a distance specified by '-d' to a point x. It then randomly selects a tip that decends from x,
-(call that new tip 'o2'), and reassigns all observations of the original OTU to the tip/OTU 'o2'.
-"""
+    'brief_description'] = ("Simulate samples for each sample in an OTU "
+                            "table, using a phylogenetic tree.")
+script_info['script_description'] = (
+    "This script makes n samples related to each sample in an input otu table "
+    "\n"
+    "An input OTU table with 3 samples and n=2 will result in an output OTU "
+    "table with 6 samples total: 3 clusters of 2 related samples."
+    "\n"
+    "To simulate each of the new samples, this script uses a sample in the "
+    "input OTU table, and for each OTU in that sample the script traverses "
+    "rootward on the tree a distance specified by '-d' to a point x. It then "
+    "randomly selects a tip that decends from x, (call that new tip 'o2'), "
+    "and reassigns all observations of the original OTU to the tip/OTU 'o2'.")
 script_info['script_usage'] = [(
     "",
-    "Create an OTU table with 3 related samples for each sample in otu_table.biom with dissimilarities of 0.001.",
+    "Create an OTU table with 3 related samples for each sample in "
+    "otu_table.biom with dissimilarities of 0.001.",
     "%prog -i otu_table.biom -t rep_set.tre -o simsam_out1 -d .001 -n 3")]
 script_info['script_usage'].append(
     ("",
-     "Create OTU tables with 2, 3 and 4 related samples for each sample in otu_table.biom with dissimilarities of 0.001 and 0.01. Additionally create new mapping files with metadata for each of the new samples for use in downstream analyses.",
-     "%prog -i otu_table.biom -t rep_set.tre -o simsam_out2 -d .001,.01 -n 2,3,4 -m map.txt"))
+     "Create OTU tables with 2, 3 and 4 related samples for each sample in "
+     "otu_table.biom with dissimilarities of 0.001 and 0.01. Additionally "
+     "create new mapping files with metadata for each of the new samples for "
+     "use in downstream analyses.",
+     "%prog -i otu_table.biom -t rep_set.tre -o simsam_out2 -d .001,.01 -n "
+     "2,3,4 -m map.txt"))
 script_info['output_description'] = """
 The output directory will contain an OTU table with samples named:
 'original_sample_0, original_sample_1 ...'
@@ -61,17 +73,18 @@ script_info['required_options'] = [
     make_option(
         '-d',
         '--dissim',
-        help='dissimilarity between nodes up the tree, as a single value or comma-separated list of values'),
-    make_option('-n', '--num',
-                help='number of simulated samples per input sample, as a single value or comma-separated list of values')
+        help='dissimilarity between nodes up the tree, as a single value or '
+        'comma-separated list of values'),
+    make_option('-n', '--num', help='number of simulated samples per input '
+                'sample, as a single value or comma-separated list of values')
 
 ]
 script_info['optional_options'] = [
     make_option('-m', '--mapping_fp', type='existing_filepath',
-                help='the mapping filepath. If provided, an output mapping file '
-                'containing the replicated sample IDs (with all other metadata '
-                'columns copied over) will also be created [default: %default]',
-                default=None)
+                help=('the mapping filepath. If provided, an output mapping '
+                      'file containing the replicated sample IDs (with all '
+                      'other metadata columns copied over) will also be '
+                      'created [default: %default]'), default=None)
 ]
 script_info['version'] = __version__
 
@@ -83,9 +96,8 @@ def main():
     create_dir(output_dir)
 
     otu_table_fp = opts.otu_table
-    otu_table_fh = open(otu_table_fp, 'U')
-    otu_table = parse_biom_table(otu_table_fh)
-    otu_table_fh.close()
+    with biom_open(otu_table_fp, 'U') as biom_file:
+        otu_table = parse_biom_table(biom_file)
 
     tree_fh = open(opts.tree_file, 'U')
     tree = DndParser(tree_fh)
