95e00bbb3636112c242622f0601dc5c578a6175a
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2014-06-12 03:11:00 -0600

    MAINT: Use load_table instead of context manager

--- scripts/filter_distance_matrix.py
+++ scripts/filter_distance_matrix.py
@@ -11,8 +11,7 @@ __maintainer__ = "Greg Caporaso"
 __email__ = "gregcaporaso@gmail.com"
 
 
-from biom.parse import parse_biom_table
-from biom.util import biom_open
+from biom import load_table
 
 from qiime.util import (parse_command_line_parameters,
                         get_options_lookup, make_option)
@@ -71,8 +70,7 @@ def main():
 
     output_f = open(opts.output_distance_matrix, 'w')
     if opts.otu_table_fp:
-        with biom_open(opts.otu_table_fp) as biom_file:
-            otu_table = parse_biom_table(biom_file)
+        otu_table = load_table(opts.otu_table_fp)
         samples_to_keep = otu_table.sample_ids
         # samples_to_keep = \
         # sample_ids_from_otu_table(open(opts.otu_table_fp,'U'))
