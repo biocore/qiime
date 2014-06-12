5edb990f46b11d0a1f6f2f4b81b8955f72d4f108
Adam Robbins-Pianka <adam.robbinspianka@colorado.edu>
2014-06-12 02:43:24 -0600

    Use shorter form load_table

--- scripts/sort_otu_table.py
+++ scripts/sort_otu_table.py
@@ -12,7 +12,7 @@ __maintainer__ = "Greg Caporaso"
 __email__ = "gregcaporaso@gmail.com"
 
 from biom.parse import parse_biom_table
-from biom.util import biom_open
+from biom import load_table
 
 from qiime.parse import parse_mapping_file
 from qiime.util import (parse_command_line_parameters, get_options_lookup,
@@ -77,8 +77,7 @@ def sample_ids_from_f(lines):
 def main():
     option_parser, opts, args = parse_command_line_parameters(**script_info)
 
-    with biom_open(opts.input_otu_table, 'U') as biom_file:
-        otu_table_data = parse_biom_table(biom_file)
+    otu_table_data = load_table(opts.input_otu_table)
     sort_field = opts.sort_field
     mapping_fp = opts.mapping_fp
     sorted_sample_ids_fp = opts.sorted_sample_ids_fp
