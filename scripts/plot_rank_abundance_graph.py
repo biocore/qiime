aa6b36c3063e93bed66f585ef258d8154f31c37d
Adam Robbins-Pianka <adam.robbinspianka@colorado.edu>
2014-06-12 00:46:24 -0600

    use biom_open, also pep8 fixes

--- scripts/plot_rank_abundance_graph.py
+++ scripts/plot_rank_abundance_graph.py
@@ -4,36 +4,44 @@ from __future__ import division
 
 __author__ = "Jens Reeder"
 __copyright__ = "Copyright 2011, The QIIME Project"
-__credits__ = ["Jens Reeder"]
+__credits__ = ["Jens Reeder", "Adam Robbins-Pianka"]
 __license__ = "GPL"
 __version__ = "1.8.0-dev"
 __maintainer__ = "Justin Kuczynski"
 __email__ = "justinak@gmail.com"
 
+from biom.parse import parse_biom_table
+from biom.util import biom_open
 
 from qiime.util import make_option
-
-
 from qiime.plot_rank_abundance_graph import plot_rank_abundance_graphs
-from qiime.util import parse_command_line_parameters, get_options_lookup, \
-    create_dir
-from biom.parse import parse_biom_table
+from qiime.util import (parse_command_line_parameters, get_options_lookup,
+                        create_dir)
 
 options_lookup = get_options_lookup()
 
 script_info = {}
 script_info['brief_description'] = "plot rank-abundance curve"
-script_info[
-    'script_description'] = "Plot a set of rank-abundance graphs from an OTU table and a set of sample names. Multiple graphs will be plotted into the same figure, in order to allow for an easy comparison across samples."
+script_info['script_description'] = (
+    "Plot a set of rank-abundance graphs from an OTU table and a set of "
+    "sample names. Multiple graphs will be plotted into the same figure, "
+    "in order to allow for an easy comparison across samples.")
 script_info['script_usage'] = [("Single graph example",
-                                "Plot the rank-abundance curve of one sample using a linear scale for the x_axis:",
-                                "%prog -i otu_table.biom  -s 'PC.354' -x -v -o single_plot.pdf"),
+                                "Plot the rank-abundance curve of one sample "
+                                "using a linear scale for the x_axis:",
+                                "%prog -i otu_table.biom  -s 'PC.354' -x -v "
+                                "-o single_plot.pdf"),
                                ("multiple graph example",
-                                "Plot the rank-abundance curve of several samples:",
-                                "%prog -i otu_table.biom  -s 'PC.354,PC.481,PC.636' -x -v -o multi_plot.pdf"),
+                                "Plot the rank-abundance curve of several "
+                                "samples:",
+                                "%prog -i otu_table.biom  -s "
+                                "'PC.354,PC.481,PC.636' -x -v -o "
+                                "multi_plot.pdf"),
                                ("multiple graph example",
-                                "Plot the rank-abundance curve of all samples in an OTU table:",
-                                "%prog -i otu_table.biom  -s '*' -x -f eps -v -o all_plot.eps"),
+                                "Plot the rank-abundance curve of all samples "
+                                "in an OTU table:",
+                                "%prog -i otu_table.biom  -s '*' -x -f eps "
+                                "-v -o all_plot.eps"),
                                ]
 
 script_info['output_description'] = ""
@@ -49,39 +57,33 @@ script_info['required_options'] = [
         '-o',
         '--result_fp',
         type='new_filepath',
-        help='Path to store resulting figure file. File extension will be appended if not supplied (e.g.: rankfig -> rankfig.pdf). Additionally, a log file rankfig_log.txt will be created'),
+        help='Path to store resulting figure file. File extension will be '
+        'appended if not supplied (e.g.: rankfig -> rankfig.pdf). '
+        'Additionally, a log file rankfig_log.txt will be created')
 ]
 
 # could basically allow all of matplotlib format here
 file_types = ['pdf', 'svg', 'png', 'eps']
 
 script_info['optional_options'] = [
-
-    make_option('-a', '--absolute_counts', help='plot absolute abundance values instead of '
-                + 'relative [default: %default]', action='store_true', default=False),
-
+    make_option('-a', '--absolute_counts', help='plot absolute abundance '
+                'values instead of relative [default: %default]',
+                action='store_true', default=False),
     make_option('-n', '--no-legend', action='store_true', default=False,
                 help='do not draw a legend [default: %default]'),
-
     make_option('-x', '--x_linear_scale', help='draw x axis in linear scale '
-                + '[default: %default]',
-                action='store_true', default=False),
-
+                '[default: %default]', action='store_true', default=False),
     make_option('-y', '--y_linear_scale', help='draw y axis in linear scale '
-                + '[default: %default]',
-                action='store_true', default=False),
-
-    make_option('-f', '--file_type', help='save plot using this image type. Choice of ' +
-                ', '.join(file_types) + ' [default: %default]', type='choice', choices=file_types,
-                default='pdf')
-
+                '[default: %default]', action='store_true', default=False),
+    make_option('-f', '--file_type', help='save plot using this image type. '
+                'Choice of ' + ', '.join(file_types) + ' [default: %default]',
+                type='choice', choices=file_types, default='pdf')
 ]
 script_info['version'] = __version__
 
 
 def main():
-    option_parser, opts, args =\
-        parse_command_line_parameters(**script_info)
+    option_parser, opts, args = parse_command_line_parameters(**script_info)
 
     otu_table_fp = opts.otu_table_fp
     sample_name = opts.sample_name
@@ -91,7 +93,8 @@ def main():
     log_fh.write("OTU table file: %s\n" % otu_table_fp)
     log_fh.write("sample names: %s\n" % sample_name)
 
-    otu_table = parse_biom_table(open(opts.otu_table_fp, "U"))
+    with biom_open(opts.otu_table_fp, 'U') as biom_file:
+        otu_table = parse_biom_table(biom_file)
 
     plot_rank_abundance_graphs(opts.result_fp, opts.sample_name, otu_table,
                                opts.file_type,
