39d7eeddbc20fc98717db85447d7fc81d02277c5
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2014-06-13 13:39:40 -0600

    STY: PEP8 import statements

--- tests/test_parallel/test_map_reads_to_reference.py
+++ tests/test_parallel/test_map_reads_to_reference.py
@@ -23,8 +23,8 @@ from biom import load_table
 from qiime.test import initiate_timeout, disable_timeout
 from qiime.util import get_qiime_temp_dir
 from qiime.parse import parse_otu_map
-from qiime.parallel.map_reads_to_reference import (ParallelDatabaseMapperUsearch, ParallelDatabaseMapperBlat,
-    ParallelDatabaseMapperBwaShort)
+from qiime.parallel.map_reads_to_reference import (ParallelDatabaseMapperBlat,
+    ParallelDatabaseMapperUsearch, ParallelDatabaseMapperBwaShort)
 
 
 class ParallelDatabaseMapperTests(TestCase):
