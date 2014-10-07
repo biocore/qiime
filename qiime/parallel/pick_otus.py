1955e1492808ceadbba097b30ea08d3b1d6d8efd
Jose Navas <josenavasmolina@gmail.com>
2014-10-07 11:10:08 -0600

    Adding documentation to parallel/pick_otus.py

--- qiime/parallel/pick_otus.py
+++ qiime/parallel/pick_otus.py
@@ -28,7 +28,15 @@ from qiime.workflow.util import generate_log_fp, WorkflowLogger
 
 
 def merge_otu_maps(output_fp, otu_maps):
-    """"""
+    """Merges all the OTU maps listed on otu_maps
+
+    Parameters
+    ----------
+    otu_maps : list of str
+        Paths to the otu map files
+    output_fp : str
+        Path to the output otu map
+    """
     unique_otu_map = {}
     for fp in otu_maps:
         with open(fp, 'U') as otu_map:
@@ -51,6 +59,22 @@ def merge_otu_maps(output_fp, otu_maps):
 class ParallelPickOtus(ParallelWrapper):
     def _construct_job_graph(self, input_fp, output_dir, params,
                              jobs_to_start=None):
+        """Creates the job workflow graph to pick otus in parallel.
+
+        Parameters
+        ----------
+        input_fp : str
+            Path to the input fasta file
+        output_dir : str
+            Path to the output directory. It will be created if it does
+            not exists
+        params : dict
+            Parameters to use when calling pick_otus.py, in the
+            form of {param_name: value}
+        jobs_to_start : int, optional
+            Number of jobs to start. Default: None = start as many jobs as
+            workers in the cluster
+        """
         # Create the output directory if it does not exists
         output_dir = abspath(output_dir)
         if not exists(output_dir):
@@ -126,12 +150,92 @@ class ParallelPickOtus(ParallelWrapper):
             self._job_graph.add_edge(node, "MERGE_FAILURES")
             self._job_graph.add_edge(node, "MERGE_OTU_MAPS")
 
+    def _picker_specific_nodes(self, params, working_dir):
+        """Adds the nodes to the job workflow graph specific to the tool to be
+        used.
+
+        Parameters
+        ----------
+        params : dict
+            Parameters to use when calling pick_otus.py, in the
+            form of {param_name: value}
+        working_dir : str
+            Path to the working directory
+
+        Returns
+        -------
+        tuple of (list of str, dict of {str: func})
+            The list contains the name of all the nodes added to the job graph.
+            The dict contains the handler function needed to process the
+            result of each node added (if needed).
+
+        Raises
+        ------
+        NotImplementedError
+            If called from the base class
+        """
+        raise NotImplementedError("This method should be overwritten in the "
+                                  "subclasses")
+
+    def _get_specific_params_str(self, params):
+        """Builds the parameter string for pick_otus.py specific to the tool
+        to be used
+
+        Parameters
+        ----------
+        params : dict
+            Parameters to use when calling pick_otus.py, in the
+            form of {param_name: value}
+
+        Returns
+        -------
+        str
+            A string with all the parameters to pass to pick_otus.py
+
+        Raises
+        ------
+        NotImplementedError
+            If called from the base class
+        """
+        raise NotImplementedError("This method should be overwritten in the "
+                                  "subclasses")
+
 
 class ParallelPickOtusUclustRef(ParallelPickOtus):
     def _picker_specific_nodes(self, params, working_dir):
+        """Adds the nodes to the job workflow graph specific to uclust ref.
+
+        Parameters
+        ----------
+        params : dict
+            Parameters to use when calling pick_otus.py, in the
+            form of {param_name: value}
+        working_dir : str
+            Path to the working directory
+
+        Returns
+        -------
+        tuple of (list of str, dict of {str: func})
+            The list contains the name of all the nodes added to the job graph.
+            The dict contains the handler function needed to process the
+            result of each node added (if needed).
+        """
         return [], {}
 
     def _get_specific_params_str(self, params):
+        """Builds the parameter string for pick_otus.py specific to uclust ref
+
+        Parameters
+        ----------
+        params : dict
+            Parameters to use when calling pick_otus.py, in the
+            form of {param_name: value}
+
+        Returns
+        -------
+        str
+            A string with all the parameters to pass to pick_otus.py
+        """
         enable_rev_strand_match_str = (
             '-z' if params['enable_rev_strand_match'] else '')
         optimal_uclust_str = '-A' if params['optimal_uclust'] else ''
@@ -152,9 +256,40 @@ class ParallelPickOtusUclustRef(ParallelPickOtus):
 
 class ParallelPickOtusUsearch61Ref(ParallelPickOtus):
     def _picker_specific_nodes(self, params, working_dir):
+        """Adds the nodes to the job workflow graph specific to usearch61 ref.
+
+        Parameters
+        ----------
+        params : dict
+            Parameters to use when calling pick_otus.py, in the
+            form of {param_name: value}
+        working_dir : str
+            Path to the working directory
+
+        Returns
+        -------
+        tuple of (list of str, dict of {str: func})
+            The list contains the name of all the nodes added to the job graph.
+            The dict contains the handler function needed to process the
+            result of each node added (if needed).
+        """
         return [], {}
 
     def _get_specific_params_str(self, params):
+        """Builds the parameter string for pick_otus.py specific to usearch61
+        ref
+
+        Parameters
+        ----------
+        params : dict
+            Parameters to use when calling pick_otus.py, in the
+            form of {param_name: value}
+
+        Returns
+        -------
+        str
+            A string with all the parameters to pass to pick_otus.py
+        """
         # Generate the parameters to pass to pick_otus.py. This must exclude
         # parameters that get passed only to the parallel version
         # (e.g jobs_to_start) and values that get overwritten (e.g.,
@@ -177,6 +312,23 @@ class ParallelPickOtusUsearch61Ref(ParallelPickOtus):
 
 class ParallelPickOtusBlast(ParallelPickOtus):
     def _picker_specific_nodes(self, params, working_dir):
+        """Adds the nodes to the job workflow graph specific to blast.
+
+        Parameters
+        ----------
+        params : dict
+            Parameters to use when calling pick_otus.py, in the
+            form of {param_name: value}
+        working_dir : str
+            Path to the working directory
+
+        Returns
+        -------
+        tuple of (list of str, dict of {str: func})
+            The list contains the name of all the nodes added to the job graph.
+            The dict contains the handler function needed to process the
+            result of each node added (if needed).
+        """
         node = "BUILD_BLAST_DB"
         if not params['blast_db']:
             # Build the blast database from the refseqs_fp -- all procs
@@ -189,6 +341,19 @@ class ParallelPickOtusBlast(ParallelPickOtus):
         return [node], {node: blast_db_builder_handler}
 
     def _get_specific_params_str(self, params):
+        """Builds the parameter string for pick_otus.py specific to blast
+
+        Parameters
+        ----------
+        params : dict
+            Parameters to use when calling pick_otus.py, in the
+            form of {param_name: value}
+
+        Returns
+        -------
+        str
+            A string with all the parameters to pass to pick_otus.py
+        """
         return (
             "-m blast -e %s -s %s --min_aligned_percent %s %s"
             % (params['max_e_value'], params['similarity'],
@@ -209,6 +374,20 @@ class ParallelPickOtusTrie(ParallelWrapper):
     algorithms, we are not extending the ParallelPickOtus class
     """
     def _construct_job_graph(self, input_fp, output_dir, params):
+        """Creates the job workflow graph to pick otus in parallel using the
+        trie algorithm.
+
+        Parameters
+        ----------
+        input_fp : str
+            Path to the input fasta file
+        output_dir : str
+            Path to the output directory. It will be created if it does
+            not exists
+        params : dict
+            Parameters to use when calling pick_otus.py, in the
+            form of {param_name: value}
+        """
         # Create the output directory if it does not exists
         output_dir = abspath(output_dir)
         if not exists(output_dir):
@@ -271,6 +450,11 @@ def trie_input_splitter(fasta_fp, output_dir, prefix_length):
         Path to the output directory
     prefix_length : int
         The length of the prefix
+
+    Returns
+    -------
+    list of str
+        Paths to the output files
     """
     out_files = []
     buffered_handles = {}
@@ -298,7 +482,15 @@ def trie_input_splitter(fasta_fp, output_dir, prefix_length):
 
 
 def merge_otu_maps_trie(output_fp, otu_maps):
-    """"""
+    """Merge the OTU maps of the parallel trie approach
+
+    Parameters
+    ----------
+    output_fp : str
+        Path to the output otu map
+    otu_maps : list of str
+        Paths to the otu maps
+    """
     otu_id = 0
     with open(output_fp, 'w') as outf:
         for fp in otu_maps:
