1ab7e7dc0250b770ad93db03dbee4ef6da397fb7
Greg Caporaso <gregcaporaso@gmail.com>
2015-01-06 12:58:12 -0700

    BUG/MAINT/TST: fixed #1824, and general code clean-up

--- scripts/filter_alignment.py
+++ scripts/filter_alignment.py
@@ -18,9 +18,9 @@ import numpy as np
 from qiime_default_reference import get_template_alignment_column_mask
 
 from qiime.util import (load_qiime_config, parse_command_line_parameters,
-                        make_option)
+                         make_option)
 from qiime.filter_alignment import (apply_lane_mask_and_gap_filter,
-                                    remove_outliers, generate_lane_mask)
+                                     remove_outliers, generate_lane_mask)
 
 script_info = {}
 script_info[
@@ -29,12 +29,17 @@ script_info[
     'script_description'] = """This script should be applied to generate a useful tree when aligning against a template alignment (e.g., with PyNAST). This script will remove positions which are gaps in every sequence (common for PyNAST, as typical sequences cover only 200-400 bases, and they are being aligned against the full 16S gene). Additionally, the user can supply a lanemask file, that defines which positions should included when building the tree, and which should be ignored. Typically, this will differentiate between non-conserved positions, which are uninformative for tree building, and conserved positions which are informative for tree building. FILTERING ALIGNMENTS WHICH WERE BUILT WITH PYNAST AGAINST THE GREENGENES CORE SET ALIGNMENT SHOULD BE CONSIDERED AN ESSENTIAL STEP."""
 script_info['script_usage'] = []
 script_info['script_usage'].append(
-    ("""Examples:""",
+    ("""Example 1:""",
      """As a simple example of this script, the user can use the following command, which consists of an input FASTA file (i.e. resulting file from align_seqs.py) and the output directory "filtered_alignment/":""",
      """%prog -i seqs_rep_set_aligned.fasta -o filtered_alignment/"""))
 
 script_info['script_usage'].append(
-    ("",
+     ("""Example 2:""",
+     """Apply the same filtering as above, but additionally remove sequences whose distance from the majority consensus sequence is more than 3 (can be changed by passing --threshold) standard deviations above the mean:""",
+     """%prog -i seqs_rep_set_aligned.fasta -o filtered_alignment/ --remove_outliers"""))
+
+script_info['script_usage'].append(
+    ("""Example 3:""",
      """Alternatively, if the user would like to use a different gap fraction threshold ("-g"), they can use the following command:""",
      """%prog -i seqs_rep_set_aligned.fasta -o filtered_alignment/ -g 0.95"""))
 
@@ -71,7 +76,7 @@ script_info['optional_options'] = [
                 default=False),
     make_option('-t', '--threshold', action='store',
                 type='float', help='with -r, remove seqs whose dissimilarity to the ' +
-                'consensus sequence is approximately > x standard devaitions above ' +
+                'consensus sequence is approximately > x standard deviations above ' +
                 'the mean of the sequences [default: %default]',
                 default=3.0),
     make_option('-e', '--entropy_threshold', action='store',
@@ -127,18 +132,18 @@ def main():
         # apply the lanemask/gap removal, then remove outliers
 
         seq_gen = apply_lane_mask_and_gap_filter(infile, lane_mask,
-                                                 opts.allowed_gap_frac, verbose=opts.verbose,
+                                                 opts.allowed_gap_frac,
                                                  entropy_threshold=opts.entropy_threshold)
 
         filtered_aln = remove_outliers(seq_gen, opts.threshold)
-        for seq in filtered_aln.Seqs:
-            outfile.write(seq.toFasta())
+        for seq in filtered_aln:
+            outfile.write(seq.to_fasta())
             outfile.write('\n')
 
     else:
         # just apply the lanemask/gap removal
         for result in apply_lane_mask_and_gap_filter(infile, lane_mask,
-                                                     opts.allowed_gap_frac, verbose=opts.verbose,
+                                                     opts.allowed_gap_frac,
                                                      entropy_threshold=opts.entropy_threshold):
             outfile.write(result)
     infile.close()
