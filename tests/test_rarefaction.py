811bef6f2c92a69ce7c81e594ddf5b6528c37cf3
Greg Caporaso <gregcaporaso@gmail.com>
2015-01-30 10:06:23 -0700

    TST: fixes #1918

--- tests/test_rarefaction.py
+++ tests/test_rarefaction.py
@@ -46,9 +46,7 @@ class FunctionTests(TestCase):
 
         self.otu_table = Table(self.otu_table_data,
                                self.taxon_names,
-                               self.sample_names,
-                               observation_metadata=[{}, {}, {}, {}],
-                               sample_metadata=[{}, {}, {}])
+                               self.sample_names)
 
         self.otu_table_meta = Table(self.otu_table_data,
                                     self.taxon_names, self.sample_names,
